f519bfff7ee7be72d4795793a6842abab1c81e5a
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-18 19:46:34 -0700

    Update tryCatch.java

--- Java/ExceptionHandling/tryCatch.java
+++ Java/ExceptionHandling/tryCatch.java
@@ -1,5 +1,29 @@
 package Java.ExceptionHandling;
 
 public class tryCatch {
-    
+    /**
+     * Flow Control of a Try-Cattch
+     * 1. If Try Block Succeeds, no exception is thrown, then
+     * control goes to the finally block if it is present. 
+     * Catch Blocks are skipped. In absence of finally block, 
+     * any code below catch block is executed
+     * 
+     * 2. If Try Block Fails
+     * @return
+     */
+    public static String flowControl(){
+
+        try{
+            System.out.println("Executing Try Block");
+            return "Returning from Try Block";
+        } catch(Exception e){
+            return "Returning from Catch Block";
+        } finally {
+            System.out.println("Print from finally");
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(flowControl());
+    }
 }
