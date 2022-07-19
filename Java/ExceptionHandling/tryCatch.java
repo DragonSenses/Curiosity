1e82b74983184984265c3e0380a29f2879e475b1
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-18 19:48:17 -0700

    Update tryCatch.java

--- Java/ExceptionHandling/tryCatch.java
+++ Java/ExceptionHandling/tryCatch.java
@@ -8,7 +8,15 @@ public class tryCatch {
      * Catch Blocks are skipped. In absence of finally block, 
      * any code below catch block is executed
      * 
-     * 2. If Try Block Fails
+     * 2. If Try Block Fails (Exception occurs), control transfers
+     * to the catch block where the exception is handled. The 
+     * remaining code in try-block is never executed. When a finally
+     * block is present, then that is run after the catch block's 
+     * execution completes
+     * 
+     * 3. If try/catch blocks have a return statement, even then the
+     * finally block still executes! Flow control jumps to the finally
+     * block and then goes back to the return statement
      * @return
      */
     public static String flowControl(){
