c521d17965082fea01642b7327825ebbadc05c7e
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-18 20:17:42 -0700

    Update tryCatch.java

--- Java/ExceptionHandling/tryCatch.java
+++ Java/ExceptionHandling/tryCatch.java
@@ -53,4 +53,57 @@ public class tryCatch {
         System.out.println("================= ");
         System.out.println(flowControl2());
     }
+
+    /** BELOW IS BAD PRACTICE, NEVER PUT A RETURN STATEMENT IN FINALLY */
+
+    String somethingThatThrewAnException(){
+        return "";
+    }
+
+    void cleanUp(){
+    }
+
+    /**
+     * ISSUE: Exception is not being propagated passed this method,
+     * because of the return statement in finally. The exception 
+     * was thrown down into another method, caught, logged, and 
+     * rethrown in somethingThatThrewAnException(). Return in 
+     * finally stops the exception that happens in the try block
+     * from propagating up even though it was not caught. 
+     * 
+     * Remember a try statement with a finally block is executed by
+     * first executing the try block. Then there is a choice depending
+     * on whether 1. execution of try block completes normally, 2. if
+     * execution of try block completes abruptly because of a throw, 
+     * 3. or if exeuction of try block completes abruptly for any other
+     * reason R then the finally block is executed and makes a choice
+     * on whether it completes normally or abruptly. In the latter case,
+     * if it completes abruptly due to reason S, then try statement 
+     * completes abruptly for reason S whereas reason R is discarded.
+     * 
+     * To put it simply, a return statement within the finally block 
+     * could hide any Throwable. AS a return statement in finally 
+     * block will trump any other reason from the regular block
+     * 
+     * @return
+     */
+    public String neverPutReturnInFinally(){
+        Object obj = null;
+        try{
+            return somethingThatThrewAnException();
+        } catch(Exception e){
+            obj = (char)'c';
+            obj.toString();
+            return "Returning from Catch Block";
+        } finally {
+            System.out.println("Print from finally");
+            cleanUp();
+            // return obj.toString();
+        }
+
+        // Just put your return here, after then finally
+        // Anything that affects return of function should lie in try block
+        // Finally block only should be where one cleans up after oneself
+        // Such as releasing file pointers, database connections, etc.
+    }
 }
