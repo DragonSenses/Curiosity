29531938c22442f077404374d8171e068e13fbf7
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-20 04:24:56 -0700

    Update OperationPrecedence.java

--- Java/Operators/OperationPrecedence.java
+++ Java/Operators/OperationPrecedence.java
@@ -6,38 +6,59 @@ package Java.Operators;
  * ======================================================================
  * Precedence   Operator    Type                            Associativity
  * ======================================================================
- * 15           ()          Parentheses
- *              []          Array subscript                 Left to Right
+ *              ()          Parentheses
+ * 15           []          Array subscript                 Left to Right
  *              .           Member selection
  * ----------------------------------------------------------------------
  * 14           ++          Unary post-increment            Right to left
  *              --          Unary post-decrement                        
  * ----------------------------------------------------------------------
- * 13
+ *              ++          Unary pre-increment         
+ *              --          Unary pre-decrement
+ *              +           Unary plus
+ * 13           -           Unary minus                     Right to left
+ *              !           Unary logical negation
+ *              ~           Unary bitwise complement
+ *              (type)      Unary type cast
  * ----------------------------------------------------------------------
- * 12
+ *              *           Multiplication
+ * 12           /           Division                        Left to right
+ *              %           Modulus
  * ----------------------------------------------------------------------
- * 11
+ * 11           +           Addition                        Left to right
+ *              -           Subtraction
  * ----------------------------------------------------------------------
- * 10
+ *              <<          Bitwise left shift              Left to right
+ * 10           >>          Bitwise right shift with sign extension
+ *              >>>         Bitwise right shift with zero extension
  * ----------------------------------------------------------------------
- * 9
+ *              <           Relational less than
+ *              <=          Relational less than or equal
+ * 9            >           Relational greater than         Left to right
+ *              >=          Relational greater than or equal
+ *              instanceof  Type comparison (objects only) 
  * ----------------------------------------------------------------------
- * 8
+ * 8            ==          Relational is equal to          Left to right
+ *              !=          Relational is not equal to   
  * ----------------------------------------------------------------------
- * 7
+ * 7            &           Bitwise AND                     Left to right
  * ----------------------------------------------------------------------
- * 6
+ * 6            ^           Bitwise exclusive OR            Left to right
  * ----------------------------------------------------------------------
- * 5
+ * 5            |           Bitwise inclusive OR            Left to right
  * ----------------------------------------------------------------------
- * 4
+ * 4            &&          Logical AND                     Left to right
  * ----------------------------------------------------------------------
- * 3
+ * 3            ||          Logical OR                      Left to right
  * ----------------------------------------------------------------------
- * 2
+ * 2            ?:          Ternary conditional             Right to left
  * ----------------------------------------------------------------------
- * 1
+ *              =           Assignment
+ *              +=          Addition assignment
+ * 1            -=          Subtraction assignment          Right to left
+ *              *=          Multiplication assignment
+ *              /=          Division assignment
+ *              %=          Modulus assignment
  * ----------------------------------------------------------------------
  */
 public class OperationPrecedence {
