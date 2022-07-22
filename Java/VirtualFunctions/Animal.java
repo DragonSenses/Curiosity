7bc66204f8bbd5908e2ae3e40cd4e85b37d18aff
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-21 21:52:02 -0700

    Update Animal.java

--- Java/VirtualFunctions/Animal.java
+++ Java/VirtualFunctions/Animal.java
@@ -14,11 +14,27 @@ package Java.VirtualFunctions;
  * Some languages provide modifiers to prevent methods from being 
  * overidden by derived classes (such as using final keyword in Java).
  * 
- * In Java, all methods other than private and static methods, are by
- * default virtual methods. 
+ * In Java, all methods other than final, private, and static methods
+ * are by default virtual methods. 
+ * 
+ * That is because by definition of of virtual methods, final methods
+ * can never be overridden and private methods are never inherited. 
+ * 
+ * Static methods are accessible and therefore inherited, but it cannot
+ * be overriden. If one were to make a new static method with the same 
+ * signature in a subclass, the old static method is hidden, not overriden.
+ * 
+ * A key distinction is that the version of the overriden method gets 
+ * invoked is the one in the subclass. The version of the hidden method 
+ * that gets invoked depends on whether it is invoked from the superclass
+ * or the subclass.
  * 
  * Virtual methods is any method which is defined again in the derived
- * class. 
+ * class. So the prerequisities of a virtual function are Inheritance
+ * and Polymorphism.
+ * 
+ * Runtime Polymorphism is a process in which a call to an overidden
+ * method is resolved at runtime rather than compile time. 
  */
 public class Animal {
 
