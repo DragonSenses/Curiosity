899c71f8a1b972de9ac23f72be04f0499947d9e6
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-22 21:26:18 -0700

    Update FileRenamer.java

--- Java/File/FileRenamer.java
+++ Java/File/FileRenamer.java
@@ -1,5 +1,11 @@
 package Java.File;
 import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+// import java.util.LinkedList;
+// import java.util.Stack;
+import java.util.List;
 
 public class FileRenamer {
     
@@ -8,16 +14,31 @@ public class FileRenamer {
     }
 
     /**
-     * Gets the directories rooted at the incoming parameter pathname.
-     * @param pathname
+     * Gets the directories at the incoming parameter pathname.
+     * @param pathname  The name of the directory to look in
      */
     public static File[] getDirectories(String pathname) {
         File[] directories = new File(pathname).listFiles(File::isDirectory);
         return directories;
     }
 
+    /**
+     * Gets all the directories recursively, rooted at the directory pathname
+     * @param pathname Name of directory to look in
+     * @return A Stack of Directories
+     */
+    public static Deque<File> getAllDirectories(String pathname){
+        Deque<File> folders = new ArrayDeque<File>(); 
+        File[] directory = getDirectories(pathname);
+        List<File> list = Arrays.asList(directory);
+        if(list.isEmpty()) { 
+            
+        }
+        return folders;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Here are the list of directories:");
+        System.out.println("#####################################");
         File[] directories = getDirectories(".");
 
         System.out.println("Here are the list of directories:");
