456901e57a5316c8969d6aaf6d193866e7933a39
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-24 00:17:02 -0700

    Create ConstructorReferences.java

---
+++ Java/Java8/ConstructorReferences.java
@@ -0,0 +1,11 @@
+package Java.Java8;
+
+/**
+ * Syntax: ClassName::new
+ * 
+ * Can create a reference to an existing constructor using its name
+ * and the keyword new
+ */
+public class ConstructorReferences {
+    
+}
