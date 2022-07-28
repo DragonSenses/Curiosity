c7d71fefc1f5473cca6784e66d084f29a9a78067
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-27 22:09:41 -0700

    Update StringProcess.java

--- Java/String/StringProcess.java
+++ Java/String/StringProcess.java
@@ -9,5 +9,17 @@ public class StringProcess {
         }
     }
 
-    
+    public static void lowercase(String s) {
+        s.toLowerCase();
+    }
+
+    public static void uppercase(String s) {
+        s.toUpperCase();
+    }
+
+    public static void main(String[] args){
+        String s = "Overlord";
+        rename(s,"Over");
+        System.out.println(s);
+    }
 }
