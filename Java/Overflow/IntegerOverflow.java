0ce359796725425ea909a914f7fc7fec6a4c669d
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-27 21:44:09 -0700

    Update IntegerOverflow.java

--- Java/Overflow/IntegerOverflow.java
+++ Java/Overflow/IntegerOverflow.java
@@ -3,6 +3,11 @@ package Java.Overflow;
 /*
  * A study on the MAX INT value with 2^31 = 2147483648
  * 
+ * Signed
+ * byte  =   8 bits
+ * short =   16 bits
+ * int   =   32 bits
+ * long  =   64 bits
  */
 public class IntegerOverflow {
 
@@ -18,11 +23,13 @@ public class IntegerOverflow {
      */
     public static void main(String[] args) {
 
-        System.out.println(Integer.MIN_VALUE); // -2,147,483,648
-        System.out.println(Integer.MAX_VALUE); // 2,147,483,647
-
-        System.out.println(Integer.MIN_VALUE - 1); // 2,147,483,647, UNDERFLOW
-        System.out.println(Integer.MAX_VALUE + 1); // -2,147,483,648, OVERFLOW
-
+        System.out.println("Minimum value of int [-2^31]: " + Integer.MIN_VALUE); // -2,147,483,648
+        System.out.println("Maximum value of int [2^31 - 1]: " + Integer.MAX_VALUE); // 2,147,483,647
+        System.out.println("========= Overflow of Minimum and Maximum int ========");
+        
+        System.out.println("Minimum - 1 value is: " 
+            + (Integer.MIN_VALUE - 1)); // 2,147,483,647 UNDERFLOW
+        System.out.println("Maximum + 1 value is: " 
+            + Integer.MAX_VALUE + 1); // -2,147,483,648 OVERFLOW
     }
 }
