3064d602aa0262ef595c516499ee04ec437353da
DragonSenses <kendrickfrancisco@gmail.com>
2022-07-28 21:28:28 -0700

    Update StringProcess.java

--- Java/String/StringProcess.java
+++ Java/String/StringProcess.java
@@ -17,9 +17,29 @@ public class StringProcess {
         s.toUpperCase();
     }
 
+    /**
+     * Extracts and Prints out every word split by space within a
+     * String s
+     * @param s the String to extract from
+     * @return string array containing every word split by space within s
+     */
+    public String[] extractWordsSplitBySpace(String s){
+        String[] result = s.split(" ");
+        for(int i = 0; i!=s.length(); i++) {
+            System.out.println(result[i]);
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         String s = "Overlord";
         rename(s,"Over");
         System.out.println(s);
+        System.out.println(s.replace("Over",""));
+        System.out.println(s);
+
+        String st = "       Overlord        ";
+        System.out.println(st);
+        System.out.println(st.trim());
     }
 }
