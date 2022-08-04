f245bb4b681845480d597f4c39fc28411b089ee5
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-03 20:44:16 -0700

    Create ArrayMethods.java

---
+++ Java/Arrays/ArrayMethods.java
@@ -0,0 +1,15 @@
+package Java.Arrays;
+
+import java.util.Arrays;
+
+/**
+ * A cluster of top methods used for Arrays.
+ */
+public class ArrayMethods {
+    
+    public static void main(String[] args){
+        /* Printing an Array */
+        int[] intArray = {1,2,3,4,5,6,7};
+        System.out.println(Arrays.toString(intArray));
+    }
+}
