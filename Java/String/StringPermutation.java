2a60f4014d6c7a8439c0fe3f5d708c2078679e32
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-07 01:10:59 -0700

    Create StringPermutation.java

---
+++ Java/String/StringPermutation.java
@@ -0,0 +1,15 @@
+package Java.String;
+
+/**
+ * Problem Statement: A string contain N characters can be have any
+ * of the characters lowercase or uppercase. For a four letter word,
+ * find every possible permutation (lower/uppercase) of that word. 
+ * A permutation does not change the order the characters were in,
+ * but rather whether each character is capitalized or not. 
+ * 
+ * Example: ping can be written as Ping or pinG. There are 16 
+ * possible ways to permute the word.
+ */
+public class StringPermutation {
+    
+}
