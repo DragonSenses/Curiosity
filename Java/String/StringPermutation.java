57670b2ea2862bebbf6e366fa5f2e99baae4cdc0
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-07 01:19:30 -0700

    Update StringPermutation.java

--- Java/String/StringPermutation.java
+++ Java/String/StringPermutation.java
@@ -12,4 +12,30 @@ package Java.String;
  */
 public class StringPermutation {
     
+    /**
+     * Finds every possible case where each character can
+     * be capitalized or lowercase, and prints out every 
+     * possible combination.
+     * @param word The word to find all permutations of
+     */
+    public void printPermutations(String word){
+        int permutations = 0;
+        // 1. Convert to char array
+        char[] arr = word.toCharArray();
+        
+        // Create a array of Strings that contain all the possibilities
+        int possibilities = arr.length * arr.length; // n^2 possible permutations
+        String[] words = new String[possibilities];
+        // 2. Start with all lower case
+        words[0] = word.toLowerCase();
+        // 3. Then all uppercase
+        words[1] = word.toLowerCase();
+        // 4. Then first char uppercase
+        
+
+    }
+
+    public static void main(String[] args){
+
+    }
 }
