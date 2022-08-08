998c884f8d2d869b78a17d36ced137f8ce00c7f6
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-07 20:03:06 -0700

    Update BitwiseOperations.java

--- Java/Operators/BitwiseOperations.java
+++ Java/Operators/BitwiseOperations.java
@@ -8,25 +8,31 @@ public class BitwiseOperations {
     
     /**
      * Private utility function that pads the passed in binary number, to
-     * a specified length.
+     * a specified length. 
      * 
-     * Bitwise logic: Let num = 1, length = 4, the result of toBinaryString()
+     * Bitwise logic: Let n = 1, length = 4, the result of toBinaryString()
      * is just 1. To get the padded zeros to the left, we manipulate it by:
      * 
      * 1) Shift the binary value of 1 to the left to the length wanted (length times)
      * This provides the padded left zeros.
      *      Result = 10000
-     * 2) Bitwise OR it with the target integer (num = 1) to get close to the value we need
+     * 2) Bitwise OR it with the target integer (n = 1) to get close to the value we need
      *      Result = 10001
      * 3) Substring the result as we only want the significant bits and the padded zeros, up
      * to the length that we want. So an operation of substring(1) would yield
      *      Result =  0001
-     * @param num The number to convert to binary and pad to the left
+     * 
+     * Special Cases:
+     * 1) Negative Numbers
+     * 2) When number in binaryString exceeds the length 
+     *    ex. n = 16, but length = 4, binary representation of 16 is 10000, a length of 5
+     * 
+     * @param n The number to convert to binary and pad to the left
      * @param length the length of the binary string to pad to
      * @return The padded binary number to a specified length
      */
-    public static String paddedBinaryString(int num, int length){
-        return Integer.toBinaryString ((1 << length) | num).substring(1);
+    public static String paddedBinaryString(int n, int length){
+        return Integer.toBinaryString ((1 << length) | n).substring(1);
     }
 
     // Performance-wise fastest way to find if a number is even
@@ -56,6 +62,7 @@ public class BitwiseOperations {
     }
 
     public static void main(String[] args){
+        System.out.println(Integer.toBinaryString(16));
         System.out.println(paddedBinaryString(1,4));
     }
 }
