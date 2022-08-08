210a274d1810b777fd7f4f0c12a29f5ff8cd42b5
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-07 19:59:20 -0700

    Update BitwiseOperations.java

--- Java/Operators/BitwiseOperations.java
+++ Java/Operators/BitwiseOperations.java
@@ -6,6 +6,29 @@ package Java.Operators;
  */
 public class BitwiseOperations {
     
+    /**
+     * Private utility function that pads the passed in binary number, to
+     * a specified length.
+     * 
+     * Bitwise logic: Let num = 1, length = 4, the result of toBinaryString()
+     * is just 1. To get the padded zeros to the left, we manipulate it by:
+     * 
+     * 1) Shift the binary value of 1 to the left to the length wanted (length times)
+     * This provides the padded left zeros.
+     *      Result = 10000
+     * 2) Bitwise OR it with the target integer (num = 1) to get close to the value we need
+     *      Result = 10001
+     * 3) Substring the result as we only want the significant bits and the padded zeros, up
+     * to the length that we want. So an operation of substring(1) would yield
+     *      Result =  0001
+     * @param num The number to convert to binary and pad to the left
+     * @param length the length of the binary string to pad to
+     * @return The padded binary number to a specified length
+     */
+    public static String paddedBinaryString(int num, int length){
+        return Integer.toBinaryString ((1 << length) | num).substring(1);
+    }
+
     // Performance-wise fastest way to find if a number is even
     public static boolean isEvenBitAnd(int i) {
         return (i & 1) == 0;
@@ -30,5 +53,9 @@ public class BitwiseOperations {
     // Has extra instructions to check for when i < 0
     public static boolean isEvenMod(int i) {
         return i % 2 == 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(paddedBinaryString(1,4));
     }
 }
