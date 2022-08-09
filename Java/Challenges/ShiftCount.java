abe86207e1d20a82977e1cac84b82857894961db
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-09 03:06:16 -0700

    Update ShiftCount.java

--- Java/Challenges/ShiftCount.java
+++ Java/Challenges/ShiftCount.java
@@ -19,13 +19,13 @@ public class ShiftCount {
     // >> operator - Arithmetic (signed) right shift operator
 
     public static int countShifts(int x, int y, int z){
-        int bitX, bitY, bitZ, shifts = 0;
+        int bitX, bitY, bitZ;
+        int shifts = 0;
         // Loop through the range of 0-31
         for(int i = 0; i < 32; i++){
-
-
             // 1. Shift each number by right by the iteration count
-            // then use & operator by 1 to attain the bit
+            // then use & operator by 1 to only attain the least
+            // significant bit (as all other values are 0 except LSB of 1)
             bitX = ((x >> i) & 1);
             bitY = ((y >> i) & 1);
             bitZ = ((z >> i) & 1);
@@ -35,18 +35,24 @@ public class ShiftCount {
                 System.out.println(bitY);
                 System.out.println(bitZ);
             }
+
+            // 2. Check if bitX | bitY = bitC
+
+            // 3. If true check bitC 
+            // 4. If bitA and bitB are both 1 
         }
 
         return shifts;
     }
     
     public static void main(String[] args){
+        int x = 2; // 0010
+        int y = 4; // 0100
+        int z = 6; // 0110
 
-        int x = 2;
-        int y = 4;
-        int z = 6;
-        int i = 0;
+        int i = 1;
 
+        System.out.println(Integer.toBinaryString(6));
         System.out.println(((x >> i) & 1));
         System.out.println(((y >> i) & 1));
         System.out.println(((z >> i) & 1));
