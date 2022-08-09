d2a2ad29147e55f4d61c5299b19627208141d36f
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-09 02:11:33 -0700

    Create ShiftCount.java

---
+++ Java/Challenges/ShiftCount.java
@@ -0,0 +1,12 @@
+package Java.Challenges;
+
+/**
+ * Problem Statement: Take 3 integers, and take the minimum amount of
+ * shifts required to make the sum of the first two numbers equal the
+ * third number. This program will return the least amount of shifts
+ * required to do so.
+ */
+public class ShiftCount {
+    // Using Bitwise Operations
+    
+}
