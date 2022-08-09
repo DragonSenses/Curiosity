ae503d3b676fc04b4d1cb5cdd91656792b5925f3
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-09 02:49:32 -0700

    Update ShiftCount.java

--- Java/Challenges/ShiftCount.java
+++ Java/Challenges/ShiftCount.java
@@ -7,33 +7,50 @@ package Java.Challenges;
  * required to do so.
  */
 public class ShiftCount {
+    private static boolean debug = true;
+
     // Using Bitwise Operations
+    // & operator - if two bits are 1, output is 1, else all 0's
+    // ex. 0101 & 0011 = 0001 , 5 & 3 = 1
+
+    // | operator - if two bits are 0, output is 0, else all 1's
+    // ex. 1100 | 1010 = 1110 , 12 | 10 = 14
+
+    // >> operator - Arithmetic (signed) right shift operator
 
     public static int countShifts(int x, int y, int z){
         int bitX, bitY, bitZ, shifts = 0;
         // Loop through the range of 0-31
         for(int i = 0; i < 32; i++){
-            // & operator - if two bits are 1, output is 1, else all 0's
-            // ex. 0101 & 0011 = 0001 , 5 & 3 = 1
-            // | operator - if two bits are 0, output is 0, else all 1's
-            // ex. 1100 | 1010 = 1110 , 12 | 10 = 14
+
 
             // 1. Shift each number by right by the iteration count
-            // then use & operator to determine which bits are equal
-            
+            // then use & operator by 1 to attain the bit
+            bitX = ((x >> i) & 1);
+            bitY = ((y >> i) & 1);
+            bitZ = ((z >> i) & 1);
+
+            if(debug) {
+                System.out.println(bitX);
+                System.out.println(bitY);
+                System.out.println(bitZ);
+            }
         }
 
         return shifts;
     }
-
     
     public static void main(String[] args){
 
         int x = 2;
         int y = 4;
         int z = 6;
+        int i = 0;
 
-        System.out.println(countShifts(x,y,z));
+        System.out.println(((x >> i) & 1));
+        System.out.println(((y >> i) & 1));
+        System.out.println(((z >> i) & 1));
+        // System.out.println(countShifts(x,y,z));
 
     }
 }
