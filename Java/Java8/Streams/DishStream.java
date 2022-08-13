a45e5947e38d78c70940c4e5ff0604b23a114ee9
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-13 08:04:58 -0700

    DishStream Summary added

--- Java/Java8/Streams/DishStream.java
+++ Java/Java8/Streams/DishStream.java
@@ -15,6 +15,17 @@ import static java.util.stream.Collectors.toList;
  * 
  * Usually database-like operations such as grouping a list, finding a certain item.
  * 
+ * - A Stream is a sequence of elements from a source that supports data-processing 
+ * operations.
+ * - Streams make use of internal iteration: the iteratio nis abstracted away throgh
+ * operations such as filter, map, and sorted
+ * - Two types of Stream Operations: Intermediate and Terminal Operations
+ * - Intermediate operations (such as filter and map) return a stream and can be 
+ * chained together. They're used to set up a pipeline of operations but don't 
+ * produce any result
+ * - Terminal Operations such as forEach and count return a non-stream value and 
+ * process a stream pipeline to return a result
+ * - Elements of a stream are computed on demand ("lazily")
  * 
  */
 public class DishStream {
