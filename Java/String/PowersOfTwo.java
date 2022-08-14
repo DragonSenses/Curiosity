e2473f99d071bdcd8aaaad92668daa9b0dfcf85c
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-14 02:00:28 -0700

    Create PowersOfTwo.java

---
+++ Java/String/PowersOfTwo.java
@@ -0,0 +1,23 @@
+package Java.String;
+
+import java.util.stream.IntStream;
+
+/**
+ * Was always interested in powers of 2, so wanted to: 
+ * To Do: Create a power of 2 table with n left column, 2^n right column.
+ * 
+ * Next step: split the pairs of columns n/2^n at 16 values, so 0-15 for
+ * the first then 16-31 for the 2nd 
+ * 
+ * Next step: calculate powers of 1024 and its deviation
+ */
+public class PowersOfTwo {
+    // Java 8 practice, breaking lines every 5 elements using a lambda and ternary
+    // statement.
+    // print the values, if it isn't a multiple of 4 tab, otherwise a newline
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1,25) // Generate a range of 1 to 25
+                .map(i -> i << 1) // Multiply each value by 2
+                .forEachOrdered(i -> System.out.print(i + ((i % 4 != 0) ? "\t" : "\n")));
+    }
+}
