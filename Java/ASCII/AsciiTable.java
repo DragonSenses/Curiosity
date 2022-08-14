17fae8b2c0c09e62bdbf87fcd89b820f942f412a
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-14 02:02:45 -0700

    Update AsciiTable.java

--- Java/ASCII/AsciiTable.java
+++ Java/ASCII/AsciiTable.java
@@ -1,10 +1,41 @@
 package Java.ASCII;
 
+/**
+ * Prints out a String Representation of the ASCII Table, 2^7, values 0-127
+ * 
+ * Note: [0-9] is 48-57, [A-Z] is 65-90, [a-z] is 97-122
+ * 
+
+ */
 public class AsciiTable {
 
-    public static void main(String[] args) {
-        for (int c = 32; c < 128; c++) {
-            System.out.println(c + ": " + (char) c);
+    StringBuilder table = new StringBuilder(
+            String.format("%1$-8s %2$-10s %3$s\n", "Decimal", "ASCII", "Hex"));
+
+    public StringBuilder getTable(){
+        return table;
+    }
+
+
+
+    public static void buildTable(StringBuilder table){
+        char c;
+        // Loop from 0 to 127
+        for (int i = 0; i < 128; i++){
+            // Typecast i into char
+            c = (char) i;
+
         }
+    }
+
+    public static void main(String[] args) {
+        // for (int c = 32; c < 128; c++) {
+        //     System.out.println(c + ": " + (char) c);
+        // }
+        
+        AsciiTable a = new AsciiTable();
+        StringBuilder table = a.getTable();
+        buildTable(table);
+        System.out.println(table.toString());
     }
 }
