513392488077a7259c0accdff033b9ca6f06682e
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-17 04:51:37 -0700

    Update PowersOfTwo.java

--- Java/String/PowersOfTwo.java
+++ Java/String/PowersOfTwo.java
@@ -12,6 +12,24 @@ import java.util.stream.IntStream;
  * Next step: calculate powers of 1024 and its deviation
  */
 public class PowersOfTwo {
+    // To Do 
+    // Want a Table such that every integer is a power of two 
+    // print tab with each until 5th value print new line
+    public static boolean skipLine(int i){
+        switch(i){
+            case(16):   //2^4
+                return true; 
+            case(512):  // 2^9
+                return true;    
+            case(16384):    // 2^14    
+                return true;    
+            case(524288):   // 2^19
+                return true;
+            default:
+                return false;
+        }
+    }
+
     // Java 8 practice, breaking lines every 5 elements using a lambda and ternary
     // statement.
     // print the values, if it isn't a multiple of 4 tab, otherwise a newline
@@ -20,12 +38,18 @@ public class PowersOfTwo {
         //         .map(i -> (i << 1)) // Multiply each value by 2
         //         .forEachOrdered(i -> System.out.print(i + ((i % 5 != 0) ? "\t" : "\n")));
 
-        IntStream.rangeClosed(1,70)
-                .map(i -> i)
-                .forEachOrdered(i -> System.out.print( ((i & i-1) == 0) ? i : " "));
+        // IntStream.rangeClosed(1,70)
+        //         .filter(i -> (i & i-1) == 0)    
+        //         .forEachOrdered(i -> System.out.println(i));
+
+        // IntStream.rangeClosed(1,70)
+        //         .filter(i -> (i & i-1) == 0)    
+        //         .forEachOrdered(i -> System.out.print(i + ((i % 16 != 0) ? "\t" : "\n")));
+
+        IntStream.rangeClosed(1,524288)
+                .filter(i -> (i & i-1) == 0)    
+                .forEachOrdered(i -> System.out.print(i + ((skipLine(i)) ? "\n" : "\t")));
     }
 
-    // To Do 
-    // Want a Table such that every integer is a power of two 
-    // print tab with each until 5th value print new line
+
 }
