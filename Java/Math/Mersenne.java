aca2dbf700dc5f68b09a87d3275996f5a095b837
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-18 15:36:29 -0700

    Create Mersenne.java

---
+++ Java/Math/Mersenne.java
@@ -0,0 +1,20 @@
+package Java.Math;
+
+/**
+ * A Mersenne number is of the form Mn = 2^n - 1, where n is an integer.
+ * - Binary Repunits: consists of all 1's in base-2
+ * - For Mersenne number to be a prime number, then n must be prime, so
+ * should be denoted as Mp = 2^p - 1. 
+ * - Mersennes primes is one of the most computationally intensive
+ * 
+ * To Do: 
+ * - Make an array of mersenne
+ * - Stream of Number of digits of mersenne number
+ * - sequence of numbers representing the of digits of each mersenne 
+ */
+public class Mersenne {
+    
+    public static void main(String[] args){
+        
+    }
+}
