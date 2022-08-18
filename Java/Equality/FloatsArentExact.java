e30dbae7a473bb4d0f625ff7fb4f3d7d647635b2
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-18 16:07:30 -0700

    Create FloatsArentExact.java

---
+++ Java/Equality/FloatsArentExact.java
@@ -0,0 +1,25 @@
+package Java.Equality;
+
+/**
+ * Avoid float and double if exact answers are required. They perform binary
+ * floating-point arithmetic which was carefully designed to furnish accurate
+ * approximations quickly over a broad range of magnitudes. 
+ * 
+ * Due to their storage method in memory, floats and doubles are ill-suited
+ * for monetary calculations, because it is impossible to represent 0.1 (or
+ * any other negative power of ten) as a float or double exactly.
+ * 
+ * Naive workaround is to merely round the results prior to printing, but 
+ * this does not always work. 
+ * 
+ * Solutions:
+ * 1) BigDecimal 
+ * - Two disadvantages is that its a lot less convenient than using a 
+ * primitive arithmetic type; and its slower. 
+ * 2) Use int or long, and track decimal point yourself 
+ * 
+ * Problem to demonstrate:
+ */
+public class FloatsArentExact {
+    
+}
