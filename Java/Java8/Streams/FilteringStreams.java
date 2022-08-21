466045403df10e9c98054fb34d92c40eba34314a
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-21 12:47:00 -0700

    Create FilteringStreams.java

---
+++ Java/Java8/Streams/FilteringStreams.java
@@ -0,0 +1,12 @@
+package Java.Java8.Streams;
+
+/**
+ * Demonstrate ways to Filter a Stream
+ */
+public class FilteringStreams {
+    
+    public static void main(String[] args) {
+        
+
+    } // end of main
+} // end of Class
