0d9070d3be3442f6237da634af8ea76fd6972c0a
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-22 02:45:57 -0700

    Update MappingStreams.java

--- Java/Java8/Streams/MappingStreams.java
+++ Java/Java8/Streams/MappingStreams.java
@@ -1,5 +1,8 @@
 package Java.Java8.Streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Demonstrate ways to Map a stream. 
  * Mapping is selecting information from certain objects.
@@ -7,5 +10,21 @@ package Java.Java8.Streams;
  * version of rather than modifying.  
  */
 public class MappingStreams {
+    public static final List<Dish> menu = Arrays.asList(
+        new Dish("pork", false, 800, Dish.Type.MEAT),
+        new Dish("beef", false, 700, Dish.Type.MEAT),
+        new Dish("chicken", false, 400, Dish.Type.MEAT),
+        new Dish("french fries", true, 530, Dish.Type.OTHER),
+        new Dish("rice", true, 350, Dish.Type.OTHER),
+        new Dish("season fruit", true, 120, Dish.Type.OTHER),
+        new Dish("pizza", true, 550, Dish.Type.OTHER),
+        new Dish("prawns", false, 400, Dish.Type.FISH),
+        new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
     
+    public static void main(String[] args){
+        System.out.println("======== Mapping by Name ========");
+        
+    }
+   
 }
