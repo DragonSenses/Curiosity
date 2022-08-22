f5027a74e4342d0fff62d627e5b0a3f01c23ffff
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-22 02:43:26 -0700

    Create MappingStreams.java

---
+++ Java/Java8/Streams/MappingStreams.java
@@ -0,0 +1,11 @@
+package Java.Java8.Streams;
+
+/**
+ * Demonstrate ways to Map a stream. 
+ * Mapping is selecting information from certain objects.
+ * Mapping is similar to "transforming" with the nuance of creating a new
+ * version of rather than modifying.  
+ */
+public class MappingStreams {
+    
+}
