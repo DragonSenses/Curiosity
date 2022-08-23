7ef1c9b4e3dc287b030a6f39095611159aa656e7
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-23 16:18:57 -0700

    Create FindingStreams.java

---
+++ Java/Java8/Streams/FindingStreams.java
@@ -0,0 +1,10 @@
+package Java.Java8.Streams;
+
+/**
+ * Demonstrates whether some elements in a set of data match a given property
+ * 
+ * methods: allMatch(), anyMatch(), noneMatch(), findFirst(), findAny()
+ */
+public class FindingStreams {
+    
+}
