7d2b2fb10d69606f4957bad5bfd367bc4a60d037
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-24 16:47:06 -0700

    Update ReducingStreams.java

--- Java/Java8/Streams/ReducingStreams.java
+++ Java/Java8/Streams/ReducingStreams.java
@@ -1,8 +1,16 @@
 package Java.Java8.Streams;
 
 /**
-
+ * Demonstrate ways to Reduce a Stream. This combines elements of a stream to
+ * express more complicated queries; classified as reduction operations as a 
+ * stream is reduced to a value. 
+ * 
+ * ================================= Methods =================================
+ * - reduce() - takes two parameters 1) initial value, 2) a lambda to combine
+ * two stream elements and produce a new value (or the operation to combine
+ * all the elements of the list)
  */
 public class ReducingStreams {
+    
     
 }
