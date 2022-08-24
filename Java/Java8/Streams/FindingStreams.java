09c944ea75cc02dc8dd2a241b0aef0c670bd4801
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-23 17:33:53 -0700

    Summary of Finding Stream methods

--- Java/Java8/Streams/FindingStreams.java
+++ Java/Java8/Streams/FindingStreams.java
@@ -1,10 +1,24 @@
 package Java.Java8.Streams;
 
 /**
- * Demonstrates whether some elements in a set of data match a given property
+ * Finding whether some elements in a set of data match a given property.
  * 
- * methods: allMatch(), anyMatch(), noneMatch(), findFirst(), findAny()
+ * ================================= Methods =================================
+ * -anyMatch() - checks if a predicate matches at least one element, a terminal
+ * operation that returns a boolean
+ * 
+ * -allMatch() - check if all elements of a stream match a given predicate
+ * 
+ * -noneMatch() - ensures that no elements in the stream match the given predicate
+ * 
+ * -findAny() - returns an arbitrary element of the current stream
+ * 
+ * -findFirst() - finds the first element given a stream that has an encounter
+ * order that specifies the order in which items logically appear in the stream
  */
 public class FindingStreams {
     
+    public static void main(String[] args){
+
+    }
 }
