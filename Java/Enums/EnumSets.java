f13dc69d0b735ea45c5e053937513d03e089be18
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-25 20:17:43 -0700

    Update EnumSets.java

--- Java/Enums/EnumSets.java
+++ Java/Enums/EnumSets.java
@@ -1,56 +1,59 @@
 package Java.Enums;
 
+import java.util.Arrays;
+import java.util.EnumSet;
+
 /**
  * EnumSets are another way to represent bit flags, in a more concise and
  * type safe manner. See BitFlags.java to see a traditional way. 
  */
 public class EnumSets {
-    public enum Flag {
+    public enum Flag { 
         UPPERCASE, REVERSE, PREFIX, SUFFIX;
+
+        // Must defined EnumSet<> consisting of all constants in Flag type
+        public static final EnumSet<Flag> ALL_OPTIONS = EnumSet.allOf(Flag.class);
     }
     
-     // Each bit flag represents a power of 2 in binary
-     public static final int UPPERCASE = 1;      // 0001
-     public static final int REVERSE = 2;        // 0010
-     public static final int PREFIX = 4;         // 0100
-     public static final int SUFFIX = 8;         // 1000
- 
-     // A binary repunit will represent multiple/all options selected
-     public static final int ALL_OPTIONS = 15;   // 1111
- 
      private static final String prefix = "The ";
      private static final String suffix = " ====";
  
-     public static String format(String val, int flags){
-         if((flags & UPPERCASE) == UPPERCASE) val = val.toUpperCase();
+     public static String format(String val, EnumSet<Flag> flags){
+         if(flags.contains(Flag.UPPERCASE)) val = val.toUpperCase();
  
-         if((flags & REVERSE) == REVERSE) {
+         if(flags.contains(Flag.REVERSE)) {
              val = new StringBuffer(val).reverse().toString();
          }
  
-         if((flags & PREFIX) == PREFIX) { 
+         if(flags.contains(Flag.PREFIX)) { 
              val = prefix.concat(val);
          }
  
-         if((flags & SUFFIX) == SUFFIX) { 
+         if(flags.contains(Flag.SUFFIX)) { 
              val = val.concat(suffix);
          }
  
          return val;
      }
  
-     // applies the following flags and returns an array of Strings
+     // applies the EnumSet flags and returns an array of Strings
      public static String[] apply(String val){
          String[] arr = new String[6]; 
          arr[0] = val;
-         arr[1] = format(val, UPPERCASE);
-         arr[2] = format(val, REVERSE);
-         arr[3] = format(val, PREFIX);
-         arr[4] = format(val, SUFFIX);
-         arr[5] = format(val, ALL_OPTIONS);
+         arr[1] = format(val, EnumSet.of(Flag.UPPERCASE));
+         arr[2] = format(val, EnumSet.of(Flag.REVERSE));
+         arr[3] = format(val, EnumSet.of(Flag.PREFIX));
+         arr[4] = format(val, EnumSet.of(Flag.SUFFIX));
+         arr[5] = format(val, Flag.ALL_OPTIONS);
          return arr;
      }
  
+     // Prints all values of a string array for more cleaner output
+     public static void print(String[] arr){
+        Arrays.stream(arr).forEach(System.out::println);
+        System.out.println();
+     }
+
      public static void main(String[] args){
          String index = "Index Librorum Prohibitorum";
          String codeName = "Dedicatus545";
@@ -58,7 +61,7 @@ public class EnumSets {
          String[] indexes = apply(index);
          String[] codeNames = apply(codeName);
  
-         System.out.println(Arrays.toString(indexes));
-         System.out.println(Arrays.toString(codeNames));
+         print(indexes);
+         print(codeNames);
      }
 }
