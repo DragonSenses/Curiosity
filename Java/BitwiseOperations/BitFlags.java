530655ddf15426c9f8522435e11e2aefaa93b96f
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-25 19:26:38 -0700

    Create BitFlags.java

---
+++ Java/BitwiseOperations/BitFlags.java
@@ -0,0 +1,10 @@
+package Java.BitwiseOperations;
+
+/**
+ * Bit Flags, or bit fields, are efficient ways of storing boolean values in a
+ * primitive type. A integer would be able to provide 31 flags (the most
+ * significant bit is reserved for signed value +/- sign). 
+ */
+public class BitFlags {
+    
+}
