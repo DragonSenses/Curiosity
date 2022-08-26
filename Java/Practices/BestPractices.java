e6fc804f1083d4065311af6ed5bcd5f20634c673
DragonSenses <kendrickfrancisco@gmail.com>
2022-08-26 16:29:53 -0700

    Create BestPractices.java

---
+++ Java/Practices/BestPractices.java
@@ -0,0 +1,12 @@
+package Java.Practices;
+
+/**
+ * List of Top Coding standards in Java 
+ * 
+ * 1. Proper Naming Conventions
+ * 2. Class members access privately
+ * 3. Underscores in lengthy Numeric literals
+ */
+public class BestPractices {
+    
+}
