f0588de9d6fcb32dfd61942a019f3eb328541d66
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-10 15:13:05 -0700

    Create Reducing.java

---
+++ Java/Java8/Collectors/Reducing.java
@@ -0,0 +1,20 @@
+package Java.Java8.Collectors;
+
+/**
+ * Collectors are parameters to the stream method collect(), and are used when
+ * it's necesssary to reorganize the stream's items into a collection. But it 
+ * can be used to combine all the items in the stream into a single result. 
+ * 
+ * 1. Count the Number of Dishes in the Menu, using counting()
+ * 2. Find Maximum in a Stream of Values
+ * 3. Find Minimum in a Stream of Values
+ */
+public class Reducing {
+    
+
+    public static void main(String... args) {
+
+    }
+
+
+}
