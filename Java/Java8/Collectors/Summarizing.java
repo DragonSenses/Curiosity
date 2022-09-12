1855f660a3e96140e6c19dc17f1dd29564bdf9a0
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-11 21:17:32 -0700

    Update Summarizing.java

--- Java/Java8/Collectors/Summarizing.java
+++ Java/Java8/Collectors/Summarizing.java
@@ -1,5 +1,25 @@
 package Java.Java8.Collectors;
 
+/**
+ * Collectors provide specific factory method for summing, which accepts a 
+ * function that maps an object into the int that has to be summed and returns
+ * a collector that, when passed to collect method, performs the requested 
+ * summarization. 
+ * 
+ * For Instance, we can:
+ * 1. Find Total Number of Calories in the Menu
+ * 2. Find Average Number of Calories in the Menu
+ * 3. Find count, sum, minimum, average, and maximum all in one operation
+ * 
+ * ================================= Methods =================================
+ * -collect() - a terminal stream operation that combines all elements of a 
+ * stream into a List, a reduction operation that takes an argumenty various
+ * recipes for accumulating the elements of a stream into a summary result
+ * 
+ */
 public class Summarizing {
     
+    public static void main(String[] args) {
+
+    }
 }
