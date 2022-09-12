77f69bdfe03d24af1946ee8670847a9aa4c155d0
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-12 15:36:33 -0700

    Update Grouping.java

--- Java/Java8/Collectors/Grouping.java
+++ Java/Java8/Collectors/Grouping.java
@@ -1,30 +1,48 @@
 package Java.Java8.Collectors;
 
 import static Java.Java8.Collectors.Dish.menu; // Use the Menu from Dish.java
+import static Java.Java8.Collectors.Dish.dishTags;
 
 import java.util.List;
-import static java.util.stream.Collectors.toList;
+import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * Grouping is a common databse operation where items in a set are grouped
  * based on one or more properties. 
  * 
  * For Instance, we can:
- * 1. Classify the dishes in the menu accordifing to their type 
+ * 1. Group the dishes in the menu according to their type 
  * 
  * ================================= Methods =================================
  * -collect() - a terminal stream operation that combines all elements of a 
  * stream into a List, a reduction operation that takes an argumenty various
  * recipes for accumulating the elements of a stream into a summary result
  * 
- * -Collectors.
+ * -Collectors.groupingBy() - a factory method that takes in a Function, and
+ * returns a Map having as map key the value returned by classification 
+ * function and corresponding map value a list of all items in the stream
+ * that have thhat classified value. 
+ * (Ex. Menu-Classification: {Key = Dish.Type, Value = List of Dishes of that Type})
  */
 public class Grouping {
     
+    /**
+     * 1. We pass in a classification Function (a method reference) which 
+     * extracts the Dish.Type for each Dish in the Stream. This returns a
+     * map whose keys are Dish.Type and values are List of all dishes of
+     * that Type
+     */
+    private static Map<Dish.Type, List<Dish>> groupDishesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType));
+    }
+
     public static void main(String[] args) {
         showMenu();
         System.out.println("\n======== Group Dishes based on Type ========");
+        System.out.println("[Dishes grouped by type]\n" + groupDishesByType());
     }
 
     // Prints the available menu and respective calories
