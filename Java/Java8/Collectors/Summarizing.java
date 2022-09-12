551e96a412ba5165c6bb14dc0eaf9505d844b2f0
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-11 21:21:07 -0700

    Update Summarizing.java

--- Java/Java8/Collectors/Summarizing.java
+++ Java/Java8/Collectors/Summarizing.java
@@ -16,6 +16,21 @@ package Java.Java8.Collectors;
  * stream into a List, a reduction operation that takes an argumenty various
  * recipes for accumulating the elements of a stream into a summary result
  * 
+ * -Collectors.summingInt() - accepts a function that maps an object into the
+ * int that has to be summed and returns the collector that performs the 
+ * requested summarization (when passed to the usual collect() method)
+ * 
+ * -Collectors.summingLong - long variant
+ * -Collectors.summingDouble - double variant
+ * 
+ * -Collectors.averagingInt - calculates the average of the same set of numeric
+ * values
+ * 
+ * -Collectors.averagingDouble - double variant of average
+ * -Collectors.averagingLong - long variant of average
+ * 
+ * - 
+ * 
  */
 public class Summarizing {
     
