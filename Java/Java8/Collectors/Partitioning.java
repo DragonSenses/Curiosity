fbca95589326b39b40630352e5d0ca43224d8091
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-13 20:39:09 -0700

    Create Partitioning.java

---
+++ Java/Java8/Collectors/Partitioning.java
@@ -0,0 +1,18 @@
+package Java.Java8.Collectors;
+
+/**
+ * Partitioning is a special case of grouping: having a predicate called a
+ * partitioning function as a classfication function 
+ * 
+ * 1. 
+ * 
+ * ================================= Methods =================================
+ * -collect() - a terminal stream operation that combines all elements of a 
+ * stream into a List, a reduction operation that takes an argumenty various
+ * recipes for accumulating the elements of a stream into a summary result
+ * 
+
+ */
+public class Partitioning {
+    
+}
