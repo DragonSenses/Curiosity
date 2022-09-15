3b653a85908c0b18ae11627dcc94f9a4a808ac91
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-14 20:51:07 -0700

    Create Prime.java

---
+++ Java/Math/Prime.java
@@ -0,0 +1,16 @@
+package Java.Math;
+
+/**
+ * Prime numbers are evenly divisible only by itself and one (e.g., 2, 3, 5, 7, 11).
+ * A prime number is a natural number greater than 1 that is not a product of two
+ * smaller natural numbers.
+ * 
+ * Primality. 
+ */
+public class Prime {
+    
+
+    public static void main(String[] args){
+        
+    }
+}
