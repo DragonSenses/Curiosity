ec578ca7c040251e7c74fc88aa73d246c7104747
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-14 20:43:08 -0700

    Update Partitioning.java

--- Java/Java8/Collectors/Partitioning.java
+++ Java/Java8/Collectors/Partitioning.java
@@ -1,16 +1,19 @@
 package Java.Java8.Collectors;
-// Use file within same package
-import static Java.Java8.Collectors.Dish.menu; // Use the Menu from Dish.java
+// Use file Dish.java within same package
+import static Java.Java8.Collectors.Dish.menu; 
 
 // Import Data Structures
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 // Import java.util.stream.Collector static methods
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.groupingBy;
-
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.Comparator.comparingInt;
 
 /**
  * Partitioning is a special case of grouping: having a predicate called a
@@ -32,7 +35,8 @@ import static java.util.stream.Collectors.groupingBy;
  * nonvegetarian dishes
  * 3. Using overloaded partioningBy() method, pass a second collector to also
  * group vegetarian and nonvegetarian dishes by type, producing a two-level Map
- * 4. 
+ * 4. Reuse earlier code that finds most caloric dish, and find the most 
+ * caloric dish between vegetarian and nonvegetarian dishes
  * 
  * ================================= Methods =================================
  * -collect() - a terminal stream operation that combines all elements of a 
@@ -80,6 +84,20 @@ public class Partitioning {
         return menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
     }
 
+    /**
+     * 4. Reuse earlier code that finds most caloric dish, and find the most 
+     * caloric dish between vegetarian and nonvegetarian dishes
+     * 
+     * @return Finds the most caloric dish between vegetarian and nonvegetarian dishes
+     */
+    private static Map<Boolean,Dish> mostCaloricPartitionedByVegetarian() {
+        return menu.stream().collect(
+            partitioningBy(Dish::isVegetarian,
+                collectingAndThen(
+                    maxBy(comparingInt(Dish::getCalories)),
+                    Optional::get)));
+    }
+
     public static void main(String[] args) {
         showMenu();
         System.out.println("\n======== Partition Menu by Vegetarian Dishes ========");
@@ -91,7 +109,8 @@ public class Partitioning {
         System.out.println("\n======== Partition by Predicate and Group by Type ========");
         System.out.println("[Vegetarian Dishes by type]\n " + vegetarianDishesByType()); // Two-Level Map
   
-        System.out.println("\n======== Partition by Predicate and Group by Type ========");
+        System.out.println("\n======== Find most Caloric Dish between Partitioned Menu ========");
+        System.out.println("[Most caloric dishes by vegetarian]\n " + mostCaloricPartitionedByVegetarian());
     }
 
     // Prints the available menu and respective calories
