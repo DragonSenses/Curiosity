473dd33db1e4f524501533cb00733bab5449820a
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-14 20:55:40 -0700

    Update Prime.java

--- Java/Math/Prime.java
+++ Java/Math/Prime.java
@@ -16,12 +16,26 @@ public class Prime {
      * @param candidate number to check Primality
      * @return true if number is prime, false otherwise
      */
-    public boolean isPrime(int candidate) {
+    public boolean isPrimeStart(int candidate) {
         // Generate a range of natural numbers starting from and including 2,
         // up to but excluding the candidate
         return IntStream.range(2, candidate)
                         .noneMatch(i -> candidate % i == 0); 
     } // Returns true if candidate isn't divisible for any of the numbers in the stream
+
+    /**
+     * Given a candidate number, checks whether it is prime or not. This method 
+     * has the simple optimization in that it tests only for factors less than 
+     * or equal to the square root of the candidate.
+     * 
+     * @param candidate number to check Primality
+     * @return true if number is prime, false otherwise
+     */
+    public boolean isPrime(int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                        .noneMatch(i -> candidate % i == 0);
+    }
 
     public static void main(String[] args){
 
