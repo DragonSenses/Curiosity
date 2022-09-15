af299037fa9ccc9e07a0d2fdac19efc4fa72b6d2
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-14 20:53:28 -0700

    Prime Test

--- Java/Math/Prime.java
+++ Java/Math/Prime.java
@@ -1,5 +1,7 @@
 package Java.Math;
 
+import java.util.stream.IntStream;
+
 /**
  * Prime numbers are evenly divisible only by itself and one (e.g., 2, 3, 5, 7, 11).
  * A prime number is a natural number greater than 1 that is not a product of two
@@ -9,8 +11,19 @@ package Java.Math;
  */
 public class Prime {
     
+    /**
+     * Given a candidate number, checks whether it is prime or not
+     * @param candidate number to check Primality
+     * @return true if number is prime, false otherwise
+     */
+    public boolean isPrime(int candidate) {
+        // Generate a range of natural numbers starting from and including 2,
+        // up to but excluding the candidate
+        return IntStream.range(2, candidate)
+                        .noneMatch(i -> candidate % i == 0); 
+    } // Returns true if candidate isn't divisible for any of the numbers in the stream
 
     public static void main(String[] args){
-        
+
     }
 }
