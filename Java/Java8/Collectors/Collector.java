8e8ef46325721ee0b7338a21b6cca4f0b77ca634
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-15 19:39:02 -0700

    Update Collector.java

--- Java/Java8/Collectors/Collector.java
+++ Java/Java8/Collectors/Collector.java
@@ -1,5 +1,6 @@
 package Java.Java8.Collectors;
 
+import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
 /**
@@ -13,9 +14,7 @@ import java.util.function.Supplier;
  * This implies that you're free to create customized reduction operations by
  * providing your own implementation of the Collector interface. 
  * 
- * Signature Breakdown: 
- * 
- * public interface Collector<T, A, R> 
+ * Signature Breakdown: "public interface Collector<T, A, R>" 
  * 
  * T - the generic type of the items in the Stream to be collected
  * A - the type of Accumulator, the object on which the partial result will be
@@ -28,7 +27,9 @@ import java.util.function.Supplier;
  * 
  * public class ToListCollector<T> implements Collector<T, List<T>, List<T>>
  * 
- * ================================= Methods =================================
+ * 
+ * ======= Making sense of the Methods declared by Collector interface =======
+ * 
  * - Notice how the first four methods returns a function that will be invoked 
  * by the collect() method
  * - The fifth method, characteristics, provides a set of characteristics 
@@ -36,6 +37,11 @@ import java.util.function.Supplier;
  * optimizations(i.e. parallelization) it's allowed to employ while performing
  * the reduction operation.
  * 
+ * 1) supplier()
+ * 2) accumulator()
+ * 3) finisher()
+ * 4) combiner()
+ * 5) characteristics()
  */
 // public interface Collector<T, A, R> {
 //     Supplier<A> supplier();
@@ -56,12 +62,32 @@ public interface Collector<T, A, R> {
      * result of the collection process when performed on an empty stream. 
      * 
      * In ToListCollector, thhe supplier will return an empty List.
+     * One can also pass a constructor reference.
      * 
      * @return a Supplier of an empty accumulator
      */
     Supplier<A> supplier();
     
+    /**
+     * 2. Adding an Element to a Result Container: The Accumulator Method
+     * Returns the function that performs the reduction operation. While 
+     * traversing the nth element in the stream, this function is applied
+     * with two arguments:
+     * 1) the accumulator being the result of the reduction 
+     * (after hhaving collected the first n-1 items of the stream) 
+     * 2) and the nth element itself. 
+     * 
+     * The function returns void because the accumulator is modified in place,
+     * meaning that its internal state is changed by the function application 
+     * to reflect the effect of the traversed element. 
+     * 
+     * For ToListCollector, this function merely has to add the current item to
+     * the list containg the already traversed ones. 
+     * @return the function that performs the reduction operation
+     */
     BiConsumer<A, T> accumulator();
+
+
     Function<A, R> finisher();
     BinaryOperator<A> combiner();
     Set<Characteristics> characteristics();
