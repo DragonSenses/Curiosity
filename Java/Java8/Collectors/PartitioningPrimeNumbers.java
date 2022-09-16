57aabf7bd8549cc2d3c80fe4aadfdaba56035bcf
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-15 17:54:55 -0700

    Create PartitioningPrimeNumbers.java

---
+++ Java/Java8/Collectors/PartitioningPrimeNumbers.java
@@ -0,0 +1,20 @@
+package Java.Java8.Collectors;
+
+/**
+ * Partitioning is a special case of grouping: having a predicate called a
+ * partitioning function as a classfication function.
+ * 
+ * ================================= Methods =================================
+ * -collect() - a terminal stream operation that combines all elements of a 
+ * stream into a List, a reduction operation that takes an argumenty various
+ * recipes for accumulating the elements of a stream into a summary result
+ * 
+ * -partitioningBy(f) - takes a partitioning function and splits the elements
+ * into a Map with two different groups, True or False as keys
+ */
+public class PartitioningPrimeNumbers {
+    
+    public static void main(String[] args) {
+    
+    }
+}
