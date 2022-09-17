15110d92831be300c5d9fa1b48273f8ee21c49bb
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-16 21:10:11 -0700

    Update ToListCollector.java

--- Java/Java8/Collectors/ToListCollector.java
+++ Java/Java8/Collectors/ToListCollector.java
@@ -2,15 +2,26 @@ package Java.Java8.Collectors;
 
 import java.util.*;
 import java.util.function.*;
-
+import java.util.stream.Collector;
+import static java.util.stream.Collector.Characteristics.*;
 
 /**
  * Developing a ToListCollector. 
  */
 public class ToListCollector<T> implements Collector<T, List<T>, List<T>>{
+    
+    @Override
     Supplier<A> supplier();
+
+    @Override
     BiConsumer<A, T> accumulator();
+    
+    @Override
     Function<A, R> finisher();
+    
+    @Override
     BinaryOperator<A> combiner();
+    
+    @Override
     Set<Characteristics> characteristics();
 }
