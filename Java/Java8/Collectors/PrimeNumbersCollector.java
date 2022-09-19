ee0e04113110a88ff302553048382cc24eb5f113
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-18 20:54:16 -0700

    Update PrimeNumbersCollector.java

--- Java/Java8/Collectors/PrimeNumbersCollector.java
+++ Java/Java8/Collectors/PrimeNumbersCollector.java
@@ -4,7 +4,12 @@ package Java.Java8.Collectors;
  * Custom Collector that implements the Collector interface. 
  * 
  * public interface Collector<T, A, R>
+ * 
+ * T - Type of Elements in the Stream                    -> Streams of Integers
+ * A - Type of Object used to Accumulate Partial results -> Map<Boolean, List<Integer>>
+ * R - Type of the final result of the collect operation -> Map<Boolean, List<Integer>>
  */
-public class PrimeNumbersCollector {
+public class PrimeNumbersCollector 
+    implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>>  {
     
 }
