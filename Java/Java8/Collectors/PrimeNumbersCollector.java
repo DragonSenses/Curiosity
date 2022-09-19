b30fd57d96d694654fd6d219933f1d1badbc113a
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-18 20:18:57 -0700

    Create PrimeNumbersCollector.java

---
+++ Java/Java8/Collectors/PrimeNumbersCollector.java
@@ -0,0 +1,10 @@
+package Java.Java8.Collectors;
+
+/**
+ * Custom Collector that implements the Collector interface. 
+ * 
+ * public interface Collector<T, A, R>
+ */
+public class PrimeNumbersCollector {
+    
+}
