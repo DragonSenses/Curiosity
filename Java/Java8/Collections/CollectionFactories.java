e69f1a04564a37fc02a0fe80d9f3f266923f11bc
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-21 18:51:05 -0700

    Update CollectionFactories.java

--- Java/Java8/Collections/CollectionFactories.java
+++ Java/Java8/Collections/CollectionFactories.java
@@ -1,8 +1,24 @@
 package Java.Java8.Collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Java 9 introduced a few convenient ways to create small collection objects
+ */
 public class CollectionFactories {
     
-    public static void main(String[] args){
+    public List<String> makeFriends(){
+        // One way to create a small collection of objects, manually
+        List<String> friends = new ArrayList<>();
+        friends.add("Ellen");
+        friends.add("Daedalus");
+        friends.add("Olivia");
         
+        return friends;
+    }
+
+    public static void main(String[] args){
+
     }
 }
