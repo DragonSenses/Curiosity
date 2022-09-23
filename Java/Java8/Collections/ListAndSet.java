d4862513b665b7c7aa21e4ef9fe25a90079e6aee
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-22 21:52:23 -0700

    Update ListAndSet.java

--- Java/Java8/Collections/ListAndSet.java
+++ Java/Java8/Collections/ListAndSet.java
@@ -3,7 +3,7 @@ package Java.Java8.Collections;
 // import Java.Java8.Collectors.GroupingTransactions;
 // import static Java.Java8.Collectors.GroupingTransactions.transactions;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 // import java.util.Arrays;
 
 // import java.util.Map;
@@ -29,16 +29,35 @@ import java.util.List;
  */
 public class ListAndSet {
     
+
+    // Build the List of Strings
+    public static List<String> build(){
+        List<String> list = new ArrayList<>();
+        list.add("Ami");
+        list.add("Ouka");
+        list.add("Akane");
+        list.add("Riho");
+        list.add("Shiragiku");
+        return list;
+    }
+    
     /**
      * Consider a List of family names. We can remove members from the list
-     * if their names start with the letter A. 
+     * if their names start with the letter A.  
+     * 
+     * Cannot remove using a for each loop as it uses an Iterator object and
+     * the Collection object itself, which is removing the element by calling
+     * remove(). Iterator is no longer synced with the state of the collection,
+     * so must use Iterator object explicitly to call its remove() method. This
+     * is the substitution to this fairly verbose way of removing elements 
+     * within collection and without getting a ConcurrentModificationException.
      */
     public static void useRemoveIf(){
-        List<String> family = 
-            List.of("Ami", "Ouka", "Akane", "Riho", "Shiragiku");
+        List<String> family = build();
+        System.out.println("------- Before Removal -------");
         family.stream().forEach(System.out::println);
-
-        System.out.println("After Removal");
+        
+        System.out.println("------- After Removal -------");
         family.removeIf(member -> member.charAt(0) == 'A');
         family.stream().forEach(System.out::println);
     }
