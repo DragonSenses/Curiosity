d195a204b5805b6f9166bbf06f526f37eb623124
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-22 19:31:22 -0700

    Create ListAndSet.java

---
+++ Java/Java8/Collections/ListAndSet.java
@@ -0,0 +1,19 @@
+package Java.Java8.Collections;
+
+/**
+ * Java 8 introduced new methods into the List and Set interfaces
+ * 
+ * ================================= Methods =================================
+ * -collect() - a terminal stream operation that combines all elements of a 
+ * stream into a List, a reduction operation that takes an argumenty various
+ * recipes for accumulating the elements of a stream into a summary result
+ * 
+ * -partitioningBy(f) - takes a partitioning function and splits the elements
+ * into a Map with two different groups, True or False as keys
+ * 
+ * -partioningBy(f,collector) - overloaded method that takes partitioning 
+ * function and a second collector to produce a multi-leveled Map. 
+ */
+public class ListAndSet {
+    
+}
