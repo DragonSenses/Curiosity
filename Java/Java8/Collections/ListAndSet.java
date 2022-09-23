1cfada0322425a004c0de16a80e701c8b04be78e
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-22 20:01:23 -0700

    Class Comment update

--- Java/Java8/Collections/ListAndSet.java
+++ Java/Java8/Collections/ListAndSet.java
@@ -1,18 +1,17 @@
 package Java.Java8.Collections;
 
 /**
- * Java 8 introduced new methods into the List and Set interfaces
+ * Java 8 introduced new methods into the List and Set interfaces.
+ * 
+ * All these methods mutate the collections on which they're invoked. They 
+ * change the collection itself, unlike stream operations, which produce a new
+ * (copied) result. Since modifying collections can be error-prone and verbose, 
+ * these methods were added.
  * 
  * ================================= Methods =================================
- * -collect() - a terminal stream operation that combines all elements of a 
- * stream into a List, a reduction operation that takes an argumenty various
- * recipes for accumulating the elements of a stream into a summary result
- * 
- * -partitioningBy(f) - takes a partitioning function and splits the elements
- * into a Map with two different groups, True or False as keys
- * 
- * -partioningBy(f,collector) - overloaded method that takes partitioning 
- * function and a second collector to produce a multi-leveled Map. 
+ * -removeIf() -  
+ * -replaceAll() -
+ * -sort() - 
  */
 public class ListAndSet {
     
