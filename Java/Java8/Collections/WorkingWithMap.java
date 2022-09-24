1531c3d3fb56fe1afb25967defebfffea41177f6
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-23 17:41:44 -0700

    Create WorkingWithMap.java

---
+++ Java/Java8/Collections/WorkingWithMap.java
@@ -0,0 +1,20 @@
+package Java.Java8.Collections;
+
+/**
+ * Java 8 introduced several default methods supported by the Map interface. 
+ * The purpose of these new operations is to help you write more concise code
+ * by using a readily available idiomatic pattern instead of implementing it 
+ * yourself. 
+ * 
+ * ================================= Methods =================================
+ * -forEach
+ * 
+ * Sorting
+ * -replaceAll() - replaces elements using a (UnaryOperator) function. 
+ * Available on List.
+ * 
+ * -sort() - sorts the list itself, available on list. 
+ */
+public class WorkingWithMap {
+    
+}
