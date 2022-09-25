35dbc6d82cc5ab80c2967c4c99b69187218081b4
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-24 17:39:37 -0700

    Update WorkingWithMap.java

--- Java/Java8/Collections/WorkingWithMap.java
+++ Java/Java8/Collections/WorkingWithMap.java
@@ -101,6 +101,18 @@ public class WorkingWithMap {
      * of MessageDigest to calculate SHA-256 hases. 
      */
 
+    // Two Functions that will be later used to transform the values of age
+    public static double toDogAge(double n){
+        // human_age = 16ln(dog_age) + 31
+        // e^(human_age - 31)/(16) = dog age
+         return Math.exp((double)(n-31)/16);
+    }
+
+    public static double toHumanAge(double n){
+        // human_age = 16ln(dog_age) + 31
+        return 16*Math.log(n) + 31;
+    }
+
     public static void main(String[] args){
         System.out.println("Map:\n----");
         Map<String, Integer> familyMap = build();
