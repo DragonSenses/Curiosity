b7114ef1c86f977f1580ead2a2666b895b159454
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-26 18:43:53 -0700

    Create ConcurrentHashMap.java

---
+++ Java/Java8/Collections/ConcurrentHashMap.java
@@ -0,0 +1,16 @@
+package Java.Java8.Collections;
+
+/**
+ * ConcurrentHashMap class provides a more modern HashMap which is also 
+ * concurrency friendly. Allows concurrent add and update operations that lock
+ * only certain parts of the internal data structure. Thus, read and write 
+ * operations have improved performance compared with the synchronized HashTable
+ * 
+ * ================================= Methods =================================
+ * -forEach()
+ * -reduce()
+ * -search()
+ */
+public class ConcurrentHashMap {
+    
+}
