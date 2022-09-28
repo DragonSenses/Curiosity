c8623833d968e4e3e59fba81318f80dc41d6e9a2
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-27 20:21:11 -0700

    Create Null.java

---
+++ Java/Java8/Optionals/Null.java
@@ -0,0 +1,15 @@
+package Java.Java8.Optionals;
+
+/**
+ * NullPointerException is commonly thrown when we examine a field of an object
+ * perhaps to determine whether its value is one of two expected forms, only to
+ * find that what we're examining is not an object but a null pointer. 
+ * 
+ * null references are convenient ways to model the absence of a value.
+ * 
+ * Everytime one doubts a variable could be null, one is obliged to do 
+ * defensive checking.  
+ */
+public class Null {
+    
+}
