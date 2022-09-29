c4c3840434eade07ef2ec0cbe46e19313ad4a003
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-28 18:28:52 -0700

    Update Person.java

--- Java/Java8/Optionals/Person.java
+++ Java/Java8/Optionals/Person.java
@@ -10,4 +10,14 @@ import java.util.Optional;
 public class Person {     
     // Member Fields      
     private Optional<Car> car; // A person may not own a car
+
+    public class Car {
+        private Optional<Insurance> insurance;  // A car may not be insured
+        public Optional<Insurance> getInsurance() { return insurance; }
+    }
+
+    public class Insurance {
+        private String name;    // An Insurance company must have a name
+        public String getName() { return name; }
+    }
 }
