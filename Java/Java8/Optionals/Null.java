25a679a95761785692dc04a69da1ad33fa0a4098
DragonSenses <kendrickfrancisco@gmail.com>
2022-09-28 19:34:28 -0700

    Moved relevant comment

--- Java/Java8/Optionals/Null.java
+++ Java/Java8/Optionals/Null.java
@@ -1,24 +1,6 @@
 package Java.Java8.Optionals;
 
 /**
- * Optional class, inspired by Haskell and Skala, encapsulates an optional value.
- * Checking is enforced by the type system. 
- * 
- * When a value is present, the Optional class wraps it. 
- * The absense of a value is modeled with an empty optional returned by method
- * Optional.empty(), which returns a special singleton instance of Optional class.
- * 
- * ---- Optional.empty() vs. Null Reference ----
- * Trying to dereference a null invariably causes NullPointerException, whereas
- * Optional.empty() is a valid, workable object of type Optional.
- * 
- * Declaring Optional<Object> instead of just Object signals that a missing value
- * is permitted there. 
- *  
- * Consistenly using Optional values creates a clear distinction
- * between a missing value that's planned for and a value that's absent only
- * because of a bug in the alogirthm or problem in data. 
- * 
  * NullPointerException is commonly thrown when we examine a field of an object
  * perhaps to determine whether its value is one of two expected forms, only to
  * find that what we're examining is not an object but a null pointer. 
--- Java/Java8/Optionals/OptionalsExample.java
+++ Java/Java8/Optionals/OptionalsExample.java
@@ -1,5 +1,23 @@
 package Java.Java8.Optionals;
-
+/**
+ * Optional class, inspired by Haskell and Skala, encapsulates an optional value.
+ * Checking is enforced by the type system. 
+ * 
+ * When a value is present, the Optional class wraps it. 
+ * The absense of a value is modeled with an empty optional returned by method
+ * Optional.empty(), which returns a special singleton instance of Optional class.
+ * 
+ * ---- Optional.empty() vs. Null Reference ----
+ * Trying to dereference a null invariably causes NullPointerException, whereas
+ * Optional.empty() is a valid, workable object of type Optional.
+ * 
+ * Declaring Optional<Object> instead of just Object signals that a missing value
+ * is permitted there. 
+ *  
+ * Consistenly using Optional values creates a clear distinction
+ * between a missing value that's planned for and a value that's absent only
+ * because of a bug in the alogirthm or problem in data. 
+ */
 public class OptionalsExample {
     
 }
