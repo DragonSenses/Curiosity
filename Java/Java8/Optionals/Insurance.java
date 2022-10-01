57f95ea61461428891054eb4eb1697dee35b1139
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-01 15:34:17 -0700

    Update Insurance.java

--- Java/Java8/Optionals/Insurance.java
+++ Java/Java8/Optionals/Insurance.java
@@ -2,5 +2,11 @@ package Java.Java8.Optionals;
 
 public class Insurance {
     private String name;    // An Insurance company must have a name
+
+    // Constructor
+    public Insurance(String name){
+        this.name = name;
+    }
+
     public String getName() { return name; }
 }
