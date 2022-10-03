41c016f5aa5ebe21929b301dd3aab08fe8812805
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-02 17:53:20 -0700

    Create OperationsWithOptional.java

---
+++ Java/Java8/Optionals/OperationsWithOptional.java
@@ -0,0 +1,12 @@
+package Java.Java8.Optionals;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+import java.util.Optional;
+
+public class OperationsWithOptional {
+
+
+
+}
