63a081ee0877d6ae827bcf4b38e616b1cbe42117
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-02 17:28:00 -0700

    Create OptionalUtility.java

---
+++ Java/Java8/Optionals/OptionalUtility.java
@@ -0,0 +1,12 @@
+package Java.Java8.Optionals;
+
+/**
+ * Optionals allows us to rethink how we deal with potentially missing values
+ * and also how to interact with native Java APIs. 
+ * 
+ * This class will showcase ways we can adapt null values and Optional with
+ * existing Java API.
+ */
+public class OptionalUtility {
+    
+}
