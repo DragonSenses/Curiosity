65ff5674e884351f55939bca3577124f94388c92
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-04 19:31:53 -0700

    Update DateTime.java

--- Java/Java8/DateAndTime/DateTime.java
+++ Java/Java8/DateAndTime/DateTime.java
@@ -5,11 +5,13 @@ import java.time.LocalDate;
 import java.time.Month;
 
 /**
- * Experimenting with the new Date and Time API. 
+ * Experimenting with the new Java 8's Date and Time API. 
+ * 
  * ================================= Summary =================================
  * 1. Creating a LocalDate object
  * 2. Creating a LocalTime object
  * ================================= Methods =================================
+ * 
  * ----------------------- LocalDate Methods ---------------------------------
  * -LocalDate.of() - static factory method that produces a LocalDate object 
  * with year, month, day as parameters
@@ -20,17 +22,30 @@ import java.time.Month;
  */
 public class DateTime {
 
+    /**
+     * Creating a LocalDate and reading its values. 
+     */
     public static void makeLocalDate(){
+        // Represent -> 2017-09-21
         LocalDate date = LocalDate.of(2017, 9, 21);
-        int year = date.getYear();
-        Month month = date.getMonth();
-        int day = date.getDayOfMonth();
-        DayOfWeek dow = date.getDayOfWeek();
-        int len = date.lengthOfMonth();
-        boolean leap = date.isLeapYear();
+        int year = date.getYear();           // 2017  
+        Month month = date.getMonth();       // SEPTEMBER
+        int day = date.getDayOfMonth();      // 21
+        DayOfWeek dow = date.getDayOfWeek(); // THURSDAY
+        int len = date.lengthOfMonth();      // 30
+        boolean leap = date.isLeapYear();    // false (not a leap year)
+
+        System.out.println("Local Date:\t" + date);
+        System.out.println("Year:\t" + year);
+        System.out.println("Month:\t" + month);
+        System.out.println("Day:\t" + day);
+        System.out.println("Day of the Week:\t" + dow);
+        System.out.println("Length of the Month:\t" + len);
+        System.out.println("Is Leap Year?\t" + leap);
     }
 
     public static void main(String[] args) {
         System.out.println("======== Working with LocalDate and LocalTime ========");
+        makeLocalDate();
     }
 }
