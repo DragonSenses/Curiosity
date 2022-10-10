7471db949545ac44ce9688739945845d49c6ef2b
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-10 16:01:19 -0700

    Create CollectionsExample.java

---
+++ Java/Java8/DefaultMethods/CollectionsExample.java
@@ -0,0 +1,30 @@
+package Java.Java8.DefaultMethods;
+
+/**
+ * Default Methods in a Nutshell
+ * An interface can contain method signatures for which an implementing class 
+ * doesn't provide an implementation. The missing method bodies are given as 
+ * part of the interface (hence, default implementations) rather than in the 
+ * implementing class. 
+ * 
+ * To recognize a default method it starts with a default modifier and contains
+ * a body like a method declared in a class. 
+ * 
+ * A functional interface contains only one abstract method; default methods are
+ * nonabstract methods. 
+ * 
+ * Both abstract class and an interface can contain abstract methods and methods
+ * with a body. 
+ * - A class can extend only from one abstract class, but a class 
+ * can implement multiple interfaces
+ * - An abstract class can enforce a common state through instance variables
+ * (fields). An interface can't have instance variables. 
+ * 
+ * 
+ */
+public interface CollectionsExample {
+    int size();
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+}
