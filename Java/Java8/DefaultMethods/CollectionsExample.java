2247866afa6007e71962ea211cb589bd06e52886
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-10 16:38:59 -0700

    Complete Default Method implementations

--- Java/Java8/DefaultMethods/CollectionsExample.java
+++ Java/Java8/DefaultMethods/CollectionsExample.java
@@ -10,11 +10,32 @@ import java.util.function.Predicate;
  * part of the interface (hence, default implementations) rather than in the 
  * implementing class. 
  * 
+ * Default methods allow you to add implementations inside an interface in a
+ * source-compatible way.
+ * 
  * To recognize a default method it starts with a default modifier and contains
  * a body like a method declared in a class. 
  * 
  * A functional interface contains only one abstract method; default methods are
  * nonabstract methods. 
+ * 
+ * You may want to create interfaces with default methods for two use cases:
+ * 1) Optional Methods
+ * 2) Multiple Inheritance of Behavior
+ * 
+ * ----------------------------  Optional Methods  ----------------------------
+ * Some classes that implement an interface but leave empty some method 
+ * implementations. For instance, the Interator interface defines hasNext(),
+ * next(), but also remove(). Remove was ignored often, so was left with empty
+ * implementations for most classes, which is unnecessary boilerplate code.
+ * 
+ * With default methods, you can provide a default implementation for methods, 
+ * so concrete classes don't need to explicitly provide an empty implementation.
+ * The Iterator interface in Java 8 provides a default implementation for remove.
+ *  
+ * This reduces boilerplate code, any class the implements Iterator interface
+ * no longer needs to declare an empty remove method to ignore it because it 
+ * now has a default implementation. 
  * 
  * ----------------------- Abstract Classes vs Interfaces ---------------------
  * Both abstract class and an interface can contain abstract methods and methods
@@ -26,6 +47,7 @@ import java.util.function.Predicate;
  * 
  * ================================= Summary ==================================
  * (1) Implement removeIf() as a default method 
+ * (2) Examine Iterator default implementation for remove (optional method)
  * ================================= Methods ================================== 
  */
 public interface CollectionsExample <E> {
@@ -42,7 +64,7 @@ public interface CollectionsExample <E> {
      * @return true when an element that fulfills the condition is removed,
      * false otherwise
      */
-    default boolean removeIf(Predicate<Boolean> test){
+    default boolean removeIf(Predicate<? super E> test){
         boolean removed = false;
         Iterator<E> each = iterator();
         while(each.hasNext()) {
@@ -54,4 +76,16 @@ public interface CollectionsExample <E> {
         return removed;
     }
 
+    /**
+     * (2) Examine Iterator default implementation for remove (optional method)
+     * hasNext() and next() both need implementations by implementing classes.
+     * But remove() is an Optional Method, and no longer needs to be declared
+     */
+    interface Iterator<T> {
+        boolean hasNext();
+        T next();
+        default void remove() { 
+            throw new UnsupportedOperationException();
+        }
+    }
 } // end of Class
