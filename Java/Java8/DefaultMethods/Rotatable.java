1e6054e32b368408fd8aa7041143e1d9054310a5
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-11 21:22:33 -0700

    Renamed Rotatable Interface

--- Java/Java8/DefaultMethods/Rotatable.java
+++ Java/Java8/DefaultMethods/Rotatable.java
@@ -4,7 +4,7 @@ public interface Rotatable {
     void setRotationAngle(int angleInDegrees);
     int getRotationAngle();
 
-    
+    // A default implementation for rotateBy() 
     default void rotateBy(int angleInDegrees){
         setRotationAngle((getRotationAngle () + angleInDegrees) % 360);
     }
