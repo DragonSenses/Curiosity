701e315687dc2da343f28b2afd3c2c8949919c3e
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-11 21:31:18 -0700

    Create Resizable.java

---
+++ Java/Java8/DefaultMethods/Resizable.java
@@ -0,0 +1,13 @@
+package Java.Java8.DefaultMethods;
+
+public interface Resizable {
+    int getWidth();
+    int getHeight();
+    void setWidth(int width);
+    void setHeight(int height);
+    void setAbsoluteSize(int width, int height);
+
+    default void setRelativeSize(int wFactor, int hFactor){
+        setAbsoluteSize(getWidth() / wFactor, getHeight() / hFactor);
+    }
+}
