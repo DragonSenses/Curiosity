33c05dc3dd0745d292a0c3a020b68fd8dc754079
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-11 21:14:57 -0700

    Create MultipleInheritanceExample.java

---
+++ Java/Java8/DefaultMethods/MultipleInheritanceExample.java
@@ -0,0 +1,9 @@
+package Java.Java8.DefaultMethods;
+
+public class MultipleInheritanceExample {
+    
+
+    // public class ArrayList<E> extends AbstractList<E>
+    //     implements List<E>, RandomAccess, Cloneable,
+    //     Serializable { }
+}
