f78bdf945dac1b43b7fc59f78818a5417f221cbf
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-11 21:32:58 -0700

    Header Comment for Resizable added

--- Java/Java8/DefaultMethods/Resizable.java
+++ Java/Java8/DefaultMethods/Resizable.java
@@ -1,5 +1,27 @@
 package Java.Java8.DefaultMethods;
 
+/**
+ * Suppose one needs to define several shapes with different characteristics
+ * for the game you're creating. Some shapes are resizable but not rotatable,
+ * some should be rotatable and movable but not resizable. To achieve great
+ * code reuse we create a minimal interface with orthogonal functionalities.
+ * 
+ * We use a technique that is somewhat related to Template Design Pattern, in 
+ * which a skeleton algorithm is defined in terms of other methods that need
+ * to be implemented. 
+ * 
+ * This stand-alone interface has five abstract methods: 
+ * -getWidth(), getHeight()
+ * -setWidth(), setHeight()
+ * -setAbsoluteSize()
+ * 
+ * And one default method, that is implemented by using abstract methods:
+ * -setRelativeSize() 
+ * 
+ * Any class that implements Resizable will need to provide an implementation
+ * for the five abstract methods, but will inherit the default implementation
+ * of setRelativeSize() for free.
+ */
 public interface Resizable {
     int getWidth();
     int getHeight();
