d78e59a0e2ed333292236cbbffc552b3b35759c2
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-12 17:23:42 -0700

    Update Box.java

--- Java/Java8/DefaultMethods/Box.java
+++ Java/Java8/DefaultMethods/Box.java
@@ -10,20 +10,46 @@ package Java.Java8.DefaultMethods;
  * and setRelativeSize().
  */
 public class Box implements Moveable, Resizable, Rotatable {
+    private int x;
+    private int y;
+
     // Moveable Methods
-    int getX();
-    int getY();
-    void setX(int x);
-    void setY(int y);
+    public int getX(){ return this.x; }
+
+    public int getY(){ return this.y; }
+
+    public void setX(int x) {
+
+    }
+
+    public void setY(int y) {
+
+    }
     
     // Resizable methods
-    int getWidth();
-    int getHeight();
-    void setWidth(int width);
-    void setHeight(int height);
-    void setAbsoluteSize(int width, int height);
+    public int getWidth(){
+
+    }
+
+    public int getHeight() {
+
+    }
+
+    public void setWidth(int width) {
+
+    }
+    public void setHeight(int height) {
+
+    }
+    public void setAbsoluteSize(int width, int height){
+
+    }
 
     // Rotatable Methods
-    void setRotationAngle(int angleInDegrees);
-    int getRotationAngle();
+    public void setRotationAngle(int angleInDegrees) {
+
+    }
+    public int getRotationAngle() {
+
+    }
 }
