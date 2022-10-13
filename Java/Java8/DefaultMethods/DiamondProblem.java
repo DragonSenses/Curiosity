b8e01ae5dbe37512bfedef1ca289156fbaf7ce7a
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-12 18:13:19 -0700

    Attempt to Emulate Diamond Problem

--- Java/Java8/DefaultMethods/DiamondProblem.java
+++ Java/Java8/DefaultMethods/DiamondProblem.java
@@ -1,5 +1,21 @@
 package Java.Java8.DefaultMethods;
 
 public class DiamondProblem {
+    public interface A {
+        default void hello() {
+            System.out.println("Hello from A");
+        }
+    }
+
+    public interface B extends A {
+        default void hello() {
+            System.out.println("Hello from B");
+        }
+    }
     
+    public class C implements B, A {
+        public static void main(String... args) {
+            new C().hello();
+        }
+    }
 }
