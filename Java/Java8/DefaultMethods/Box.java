7ac7f03069c79fc7b563a8be7ec7964afb1fb646
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-12 17:18:23 -0700

    Create Box.java

---
+++ Java/Java8/DefaultMethods/Box.java
@@ -0,0 +1,9 @@
+package Java.Java8.DefaultMethods;
+
+/**
+ * Composing Interfaces. This class showcases a concrete class that is Movable,
+ * Resizeable, Rotatable. 
+ */
+public class Box implements Moveable, Resizable, Rotatable {
+    
+}
