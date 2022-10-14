d0a627cc985c114fde90db22fb4a10c1b737b762
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-13 18:26:09 -0700

    Diamond Problem Explored

--- Java/Java8/DefaultMethods/Diamond.java
+++ Java/Java8/DefaultMethods/Diamond.java
@@ -1,26 +1,10 @@
 package Java.Java8.DefaultMethods;
 
-// public class DiamondProblem {
-//     public interface A {
-//         default void hello() {
-//             System.out.println("Hello from A");
-//         }
-//     }
-
-//     public interface B extends A {
-//         default void hello() {
-//             System.out.println("Hello from B");
-//         }
-//     }
-    
-//     public class C implements B, A {
-//         public static void main(String... args) {
-//             new C().hello();
-//         }
-//     }
-// }
-
-
+/**
+ * Classic Diamond Problem in C++. In which a class can inherit two methods
+ * with the same signature, which gets chosen? Here we attempt it in Java
+ * through default methods. 
+ */
 public class Diamond {
 
     public static void main(String... args) {
@@ -42,12 +26,35 @@ public class Diamond {
     }
   
     static interface C extends A {
-      default void hello() {
-        System.out.println("Hello from C");
+      // default void hello() {
+      //   System.out.println("Hello from C");
+      // }
+    }
+  
+    static class D implements B, C {
+      public static void main(String[] args){
+        D d = new D();
+        d.hello();
       }
     }
   
-    static class D implements B, C {}
-  
   }
-  
+  // public class DiamondProblem {
+//     public interface A {
+//         default void hello() {
+//             System.out.println("Hello from A");
+//         }
+//     }
+
+//     public interface B extends A {
+//         default void hello() {
+//             System.out.println("Hello from B");
+//         }
+//     }
+    
+//     public class C implements B, A {
+//         public static void main(String... args) {
+//             new C().hello();
+//         }
+//     }
+// }
