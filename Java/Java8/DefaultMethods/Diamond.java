6d82013a5a6fce0bdf6578e8b7bc283ff4088c15
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-13 18:16:23 -0700

    Renamed to Diamond

---
+++ Java/Java8/DefaultMethods/Diamond.java
@@ -0,0 +1,45 @@
+package Java.Java8.DefaultMethods;
+
+// public class DiamondProblem {
+//     public interface A {
+//         default void hello() {
+//             System.out.println("Hello from A");
+//         }
+//     }
+
+//     public interface B extends A {
+//         default void hello() {
+//             System.out.println("Hello from B");
+//         }
+//     }
+    
+//     public class C implements B, A {
+//         public static void main(String... args) {
+//             new C().hello();
+//         }
+//     }
+// }
+
+
+public class Diamond {
+
+    public static void main(String... args) {
+      new D().hello();
+    }
+  
+    static interface A {
+  
+      public default void hello() {
+        System.out.println("Hello from A");
+      }
+  
+    }
+  
+    static interface B extends A {}
+  
+    static interface C extends A {}
+  
+    static class D implements B, C {}
+  
+  }
+  
