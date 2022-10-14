8ab4298166f395c7194b3c15ec6be58739b541b6
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-13 18:33:53 -0700

    Update ResolutionRules.java

--- Java/Java8/DefaultMethods/ResolutionRules.java
+++ Java/Java8/DefaultMethods/ResolutionRules.java
@@ -22,5 +22,53 @@ package Java.Java8.DefaultMethods;
  * explicitly.
  */
 public class ResolutionRules {
+    // public class Diamond {
+
+    //     public static void main(String... args) {
+    //       new D().hello();
+    //     }
+      
+    //     static interface A {
+      
+    //       public default void hello() {
+    //         System.out.println("Hello from A");
+    //       }
+      
+    //     }
+      
+    //     static interface B extends A {
+    //         default void hello() {
+    //           System.out.println("Hello from B");
+    //         }
+    //     }
+      
+    //     static interface C extends A {
+    //       default void hello() {
+    //         System.out.println("Hello from C");
+    //       }
+    //     }
+      
+    //     static class D implements B, C {
+    //     }
+      
+    //   }
+
+    public static void main(String... args) {
+        new D().hello();
+      }
+    
+      static interface A {
+    
+        public default void hello() {
+          System.out.println("Hello from A");
+        }
+    
+      }
+    
+      static interface B extends A {}
+    
+      static interface C extends A {}
+    
+      static class D implements B, C {}
     
 }
