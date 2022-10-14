20b2a784ba9ead6d6e03c806e4ab90943ca2b939
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-13 18:28:06 -0700

    Create ResolutionRules.java

---
+++ Java/Java8/DefaultMethods/ResolutionRules.java
@@ -0,0 +1,13 @@
+package Java.Java8.DefaultMethods;
+
+/**
+ * With the advent of Default Methods, there's the possibility of a class 
+ * inheriting more than one method with the same signature. Which version
+ * should be used? Such conflicts are rare in practice, but when they do 
+ * occur, there must be rules that specify hhow to deal with the conflict. 
+ * 
+ * Here we explore the Resolution Rules. 
+ */
+public class ResolutionRules {
+    
+}
