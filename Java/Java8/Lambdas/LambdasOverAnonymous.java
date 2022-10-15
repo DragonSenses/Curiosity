b4d0e84b6dfe61eb621352957c9368e28bed57f1
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-14 21:06:34 -0700

    Update LambdasOverAnonymous.java

--- Java/Java8/Lambdas/LambdasOverAnonymous.java
+++ Java/Java8/Lambdas/LambdasOverAnonymous.java
@@ -7,7 +7,18 @@ public class LambdasOverAnonymous {
     
     private LambdasOverAnonymous() {} // prevent instantiation
 
+    /* Sort a list of Strings in order of length */
+
+    public static sortWithAnonymousClass(List<String> words){
+        // Anonymous class instance as a function object - obsolete!
+        Collections.sort(words, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return Integer.compare(s1.length(), s2.length());
+            }
+        });
+    } 
+    
     public static void main(String[] args){
-        
+
     }
 }
