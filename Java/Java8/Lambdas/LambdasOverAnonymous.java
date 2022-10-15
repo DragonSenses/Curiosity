51eadadb67c52c7f037e5936f0d45d8fdb211f3f
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-14 21:19:38 -0700

    Update Method Comments

--- Java/Java8/Lambdas/LambdasOverAnonymous.java
+++ Java/Java8/Lambdas/LambdasOverAnonymous.java
@@ -1,5 +1,6 @@
 package Java.Java8.Lambdas;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -18,10 +19,22 @@ import java.util.List;
  * instances of these interfaces using lambda expressions, or lambdas for short.
  * 
  * Lambdas are similar in function to anonymous classes, far more concise. 
+ * 
+ * Omit the types of all lambda parameters unless their presence makes your
+ * program clearer. If the compiler generates an error telling you it can’t infer
+ * the type of a lambda parameter, then specify it. Sometimes you may have to cast
+ * the return value or the entire lambda expression.
  */
 public class LambdasOverAnonymous {
     
-    private LambdasOverAnonymous() {} // prevent instantiation
+    public static List<String> makeList(){
+        List<String> list = new ArrayList<>();
+        list.add("abc");
+        list.add("1234567");
+        list.add("ab");
+        list.add("1234");
+        list.add("The secret of getting ahead is getting started. ~Mark Twain");
+    }
 
     /* Sort a list of Strings in order of length */
 
@@ -40,6 +53,11 @@ public class LambdasOverAnonymous {
 
     /**
      * Sort a list of Strings in order of length, using a Lambda Expression.
+     * 
+     * Note that the types of the lambda (Comparator<String>), of its parameters
+     * (s1 and s2, both String), and of its return value (int) are not present
+     * in the code. The compiler deduces these types from context, using a
+     * process known as type inference.
      * @param words list of Strings to sort
      */
     public static void sortWithLambda(List<String> words){
@@ -49,6 +67,7 @@ public class LambdasOverAnonymous {
     } 
     
     public static void main(String[] args){
-        List<String> list = List.of("1234567", "12345", "123", "1234");
+        List<String> list = makeList();
+        
     }   
 }
