93b320d528e96f8814c58172cfe2cc4bdc14412d
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-14 21:04:22 -0700

    Create LambdasOverAnonymous.java

---
+++ Java/Java8/Lambdas/LambdasOverAnonymous.java
@@ -0,0 +1,13 @@
+package Java.Java8.Lambdas;
+
+/**
+ * Prefer Lambdas to Anonymous Classes. 
+ */
+public class LambdasOverAnonymous {
+    
+    private LambdasOverAnonymous() {} // prevent instantiation
+
+    public static void main(String[] args){
+        
+    }
+}
