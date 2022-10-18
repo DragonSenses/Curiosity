142d4593676cdfdd96f1dbe445589f4f3fe15907
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-17 20:24:36 -0700

    first attempt for anagrams

--- Java/Challenges/Anagrams.java
+++ Java/Challenges/Anagrams.java
@@ -2,6 +2,11 @@ package Java.Challenges;
 
 import java.io.IOException;
 
+// import data structures
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Class that prints all the anagram groups whose size meets a user-specified
  * minimum. Two words are anagrams if they consist of the same letters in a 
@@ -9,7 +14,7 @@ import java.io.IOException;
  */
 public class Anagrams {
     // pseudo code 
-    // use data structure to store anagrams
+    // use data structure to store anagrams --> Map
     // read from collection of strings
     // make anagrams 
     private static String getAnagram(String s){
@@ -18,5 +23,12 @@ public class Anagrams {
 
     // Later will update to read words from a file 
     public static void main(String[] args) throws IOException {
+        // Create string words from the source
+        String[] source = new String[12]; 
+
+        // Store anagrams in data structure, in this case a Map
+        // Key are the String words, Value is the Set of anagrams formed 
+        // since each combination is unique
+        Map<String, Set<String>> groups = new HashMap(); 
     }
 }
