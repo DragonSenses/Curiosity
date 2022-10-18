9750355e19164a73231ce31e52386ac1c5acbd27
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-17 20:34:42 -0700

    Implemented alphabetize method

--- Java/Challenges/Anagrams.java
+++ Java/Challenges/Anagrams.java
@@ -1,7 +1,7 @@
 package Java.Challenges;
 
 import java.io.IOException;
-
+import java.util.Arrays;
 // import data structures
 import java.util.HashMap;
 import java.util.Map;
@@ -23,8 +23,16 @@ public class Anagrams {
     // use data structure to store anagrams --> Map
     // read from collection of strings
     // make anagrams 
-    private static String getAnagram(String s){
 
+    /**
+     * Alphabetizes a given String and returns it. 
+     * @param s String to alphabetize
+     * @return the String sorted in alphabetical order
+     */
+    private static String alphabetize(String s){
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
     }
 
     /**
