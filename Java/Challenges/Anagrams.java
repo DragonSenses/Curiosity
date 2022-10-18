038d200329883bce91565fa346bf5b02affd31f2
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-17 20:21:14 -0700

    create pseudocode

--- Java/Challenges/Anagrams.java
+++ Java/Challenges/Anagrams.java
@@ -1,5 +1,22 @@
 package Java.Challenges;
 
-public class Anagrams {
+import java.io.IOException;
 
+/**
+ * Class that prints all the anagram groups whose size meets a user-specified
+ * minimum. Two words are anagrams if they consist of the same letters in a 
+ * different order. 
+ */
+public class Anagrams {
+    // pseudo code 
+    // use data structure to store anagrams
+    // read from collection of strings
+    // make anagrams 
+    private static String getAnagram(String s){
+
+    }
+
+    // Later will update to read words from a file 
+    public static void main(String[] args) throws IOException {
+    }
 }
