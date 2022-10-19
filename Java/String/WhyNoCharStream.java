e444e6f3852d897997ca46820c60f5c574bc3800
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-19 15:40:16 -0700

    Create WhyNoCharStream.java

---
+++ Java/String/WhyNoCharStream.java
@@ -0,0 +1,15 @@
+package Java.String;
+
+/**
+ * Discovered that Java does not support a char stream. Here we find ways to be
+ * able to use the API in spite of this. 
+ * 
+ * AS we know, the method String.chars() returns a stream of ints or IntStream
+ * that represent character codes. 
+ */
+public class WhyNoCharStream {
+    
+    public static void main(String[] args){
+        
+    }
+}
