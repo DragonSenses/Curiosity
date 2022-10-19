974a8cfed2ec71cd1cadc15bbb8bc8c9274187a8
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-18 21:18:59 -0700

    Show ills of processing char values with Streams

--- Java/Java8/Streams/Alphabetize.java
+++ Java/Java8/Streams/Alphabetize.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 
 /**
  * Alphabetizes, or sorts, a String. Experiments ways to do so and how it 
- * is affected by Java's lack of support for primitive char streams.  
+ * is affected by Java's lack of support for primitive char streams. 
+ * 
+ * We showcase how alphabetize a String should not be reimplemented to use
+ * streams as it would have been less clear, more difficult to write correctly,
+ * and probably slower. 
  */
 public class Alphabetize {
     
@@ -20,8 +24,33 @@ public class Alphabetize {
         return new String(a);
     }
 
+    /**
+     * Demonstrates the hazards of processing char values with streams.
+     * 
+     * Expected to print Hellow world! but instead prints 721011081081113211911111410810033
+     * This happens because the elements of the stream returned by "Hello world!".chars()
+     * are not char values but int values, so the int overloading of print is invoked. 
+     * Confusing that a method named chars returns a stream of int values. 
+     */
+    private static void example(){
+        "Hello world!".chars().forEach(System.out::print);
+    }
+
+    /**
+     * Fixes the example above of processing char values with Streams. 
+     * The program usies a cast to force the invocation of the correct overloading.
+     * Ideally, you should refrain from using streams to process chhar values. 
+     */
+    private static void example2(){
+        "Hello world!".chars().forEach(x -> System.out.print((char) x));
+    }
+
     public static void main(String[] args) {
-        String s = "Listen"; // Anagram of Silent
+        System.out.println(alphabetize("listen"));
+        System.out.println(alphabetize("silent"));
+
+        example();
         System.out.println();
+        example2();
     }
 }
