41a86d4f964646037b2f60836d30db67d2f25c4c
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-19 16:54:57 -0700

    Finalize Class, Reverted Previous Change

--- Java/String/WhyNoCharStream.java
+++ Java/String/WhyNoCharStream.java
@@ -1,13 +1,28 @@
 package Java.String;
 
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 /**
- * Discovered that Java does not support a char stream. Here we find ways to be
- * able to use the API in spite of this. 
+ * Discovered that Java does not support a char stream. Java is an "impure" 
+ * Object-Oriented language, since the primitives are not objects. Primitives
+ * was a pragmatic decision to improve performance at the expense of object
+ * -oriented purity. When Streams API was introduced, only some primitives 
+ * were supported (int, long, double ...) to reduce the amount of clutter to
+ * the API. The designers did not want to support all of the primitives, 
+ * "All" or "none" are comfortable places for a design to be, yet neither was
+ * acceptable. So they had to find a reasonable value of "some", which ended up
+ * with primitive specializations for int, long, and double. 
  * 
- * AS we know, the method String.chars() returns a stream of ints or IntStream
+ * A CharStream primitive specialization was considered but rejected since its
+ * use would be quite narrow compared to the amount of builk it added to the API.
+ * 
+ * So the designers chose the pragmatic choice of providing a subset of primitive
+ * specializations, giving a moderately sized, high performing API that imposes a
+ * relatively small burden on callers in a fairly narrow range of use cases (char processing).
+ * 
+ * Source: https://stackoverflow.com/questions/22435833/why-is-string-chars-a-stream-of-ints-in-java-8
+ * 
+ * Here we explore a way to be able to use the API in spite of this. 
+ * 
+ * As we know, the method String.chars() returns a stream of ints or IntStream
  * that represent character codes. 
  * 
  * To be able to process chars using Streams in Java 8, such as printing, first
@@ -20,8 +35,6 @@ import java.util.stream.Stream;
  * 
  * ================================= Summary =================================
  * (1) Use mapToObj(i -> (char)i) rather than map() to obtain Stream<Character>
- * (2) Converting String to Stream, and Stream to String
- * (3) 
  */
 public class WhyNoCharStream {
     
@@ -43,15 +56,6 @@ public class WhyNoCharStream {
          .forEach(System.out::println);
     }
 
-    private static IntStream stringToIntStream(String string){
-        return  string.chars().mapToObj(i -> (char)i);
-    }
-
-    private static String intStreamToString(Stream<Object> stream){
-        stream.reduce(StringBuilder::new, (sb, c) -> sb.append((char)c)),
-                      StringBuilder::append).toString();
-    }
-
     public static void main(String[] args){
         String s = "hello";
 
@@ -63,5 +67,6 @@ public class WhyNoCharStream {
 
         System.out.printf("\n\t----- Printing %s the Wrong Way in Java 8 -----\n", s);
         printJava8WrongWay(s);
-    }
-}
+ 
+    } // end of Main
+} // end of Class
