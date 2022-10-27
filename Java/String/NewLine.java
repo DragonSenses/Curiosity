c20cb1b76cbe6b5acd3b825e750256ad99d1a9c3
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-26 17:24:55 -0700

    Create NewLine.java

---
+++ Java/String/NewLine.java
@@ -0,0 +1,46 @@
+package Java.String;
+
+import java.util.EnumSet;
+
+/**
+ * 
+ * This file serves as a Template for future classes that will implement EnumSet
+ * which is designed specifically to replace bit flags. 
+ * 
+ * ================================= Summary =================================
+ * (1) 
+ * (2) 
+ * (3) 
+ * (4) 
+ * ================================= Methods ==================================
+ * - 
+ */
+public class NewLine {
+
+    enum Flags {
+        One, Two, Three, Four
+    }
+
+    public static void execute(EnumSet<Flags> flags){
+        if(flags.contains(Flags.One)) {
+            System.out.println("One");
+        }
+        if(flags.contains(Flags.Two)) {
+            System.out.println("Two");
+        }
+        if(flags.contains(Flags.Three)) {
+            System.out.println("Three");
+        }
+        if(flags.contains(Flags.Four)) {
+            System.out.println("Four");
+        }
+    }
+
+    public static void main(String[] args) {
+        EnumSet<Flags> allOptions = EnumSet.allOf(Flags.class); 
+        execute(allOptions);
+
+        EnumSet<Flags> currentOpt = EnumSet.of(Flags.Two);
+        execute(currentOpt);
+    }
+}
