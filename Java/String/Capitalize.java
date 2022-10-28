7d6a9cb6c33979f66c09d771d2ff1ba99b357fad
DragonSenses <kendrickfrancisco@gmail.com>
2022-10-27 17:38:14 -0700

    Create Capitalize.java

---
+++ Java/String/Capitalize.java
@@ -0,0 +1,23 @@
+package Java.String;
+
+/**
+ * Goal: Process a String so that the first character of each word
+ * is Capitalized. 
+ */
+public class Capitalize {
+
+    /**
+     * Converts all the letters into upper/lowercase depending on their
+     * position near a space or other special characters
+     * @param s String to process
+     * @return String with first character of every word capitalized
+     */
+    public static String capitalizeString(String s)[
+        
+    ]
+
+    public static void main(String[] args){
+        String s = "demon prince goes to academy";
+
+    }
+}
