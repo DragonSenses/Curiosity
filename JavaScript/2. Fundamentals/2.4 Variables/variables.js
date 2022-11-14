f51b3cda0710b50f1fadb7c518a713d94f19dc2d
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-14 14:33:18 -0800

    Create variables.js

---
+++ JavaScript/2. Fundamentals/2.4 Variables/variables.js
@@ -0,0 +1,12 @@
+/**
+ * A variable is a named storage for data. To create a variable use "let" 
+ * keyword. 
+ * - let is a modern variable declaration
+ * - var is an older variable declaration
+ * - const is a variable declaration where the value of the variable 
+ * can't be changed.
+ */
+
+let variable;
+variable = 'Hi';
+
