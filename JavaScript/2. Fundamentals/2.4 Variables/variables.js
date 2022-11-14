5f112ae47fc446dc66b5e326989ca70d5824f61a
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-14 15:32:46 -0800

    Update variables.js

--- JavaScript/2. Fundamentals/2.4 Variables/variables.js
+++ JavaScript/2. Fundamentals/2.4 Variables/variables.js
@@ -7,6 +7,22 @@
  * can't be changed.
  */
 
-let variable;
-variable = 'Hi';
+let variable;       // Declares a variable
+variable = 'Hi';    // Assign data into variable
+alert(variable);    // Access variable
 
+// Declaring and assigning into one line
+let message = 'Hello';
+alert(message);
+
+// Declaring multiple variables, multiline style
+let user = 'user', 
+    age = 20, 
+    msg = 'Hey';
+
+/* Variable Naming
+1. Name must contain only letters, digits, or symbols $ and _
+2. First character must not be a digit
+When the name contains multiple words, camelCase is used (i.e., words go one 
+after another, each word except first starting with a capital letter: myVeryLongName)
+*/
