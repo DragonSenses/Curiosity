f836b799e14b3458db9f449aab58d275168cff5d
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-15 15:18:56 -0800

    Create EnumIsUseful.java

---
+++ Java/Enums/EnumIsUseful.java
@@ -0,0 +1,36 @@
+package Java.Enums;
+
+/**
+ * Enumerations serve the purpose of representing a group of named constants in
+ * a programming language.
+ * 
+ * Enum constants are implicitly static and final and you can not change their
+ * value once created.
+ * 
+ * Benefits:
+ * 1. Enum is Type-Safe, cannot assign anything else other than predefined Enum
+ * constants to an Enum variable. 
+ * 2. Enum has its own namespace.
+ * 3. Can use Java Enum inside Switch statements like int or char primitive 
+ * data types. 
+ * 4. Maintainability - adding new constants on Enum is easy, and does not break
+ * existing code
+ * 5. Enum in Java are reference types like class or interface and you can define
+ * constructor, methods, and variables inside java Enum which makes it more 
+ * powerful than Enum in C/C++
+ * 6. Can specify values of enum constants at creation time 
+ * 
+ * A Java enumeration is a class type. Although we don’t need need to instantiate
+ * an enum using new, it has the same capabilities as other classes. Just like 
+ * classes, you can: 
+ * - Give them constructors
+ * - Add instance variables and methods
+ * - Implement interfaces.
+ * 
+ * One thing to keep in mind is that, unlike classes, enumerations neither inherit
+ * other classes nor can get extended(i.e become superclass). 
+ */
+public class EnumIsUseful {
+    
+
+}
