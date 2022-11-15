52693a22bafc526c9d54ce4a644935f6248d6bcb
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-14 17:20:48 -0800

    Create variablesExercise.js

---
+++ JavaScript/2. Fundamentals/2.4 Variables/variablesExercise.js
@@ -0,0 +1,25 @@
+/** Working with Variables
+ * 1. Declare two variables: admin and name.
+ * 2. Assign the value "John" to name. 
+ * 3. Copy the value from name to admin.
+ * 4. Show the value of admin using alert (must output “John”).
+ */
+
+/** Right Naming
+ * 1. Create a variable with the name of our planet. How would you name such a variable?
+ * 2. Create a variable to store the name of a current visitor to a website. 
+ * How would you name that variable?
+ */
+
+/**
+ * The age constant.
+ *  
+ * The age is calculated from birthday using someCode(), some function call 
+ * whose implementation is hidden but calculates the age based on birthday.
+ * 
+ * Would it be right to use upper case for birthday? For age? Or even for both?
+ */
+
+ const birthday = '18.04.1982';
+
+ const age = someCode(birthday);
