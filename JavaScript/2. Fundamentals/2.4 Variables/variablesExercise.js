b5ddf1453df6c833269625318ee196d621e75d80
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-14 17:29:59 -0800

    Update variablesExercise.js

--- JavaScript/2. Fundamentals/2.4 Variables/variablesExercise.js
+++ JavaScript/2. Fundamentals/2.4 Variables/variablesExercise.js
@@ -5,11 +5,20 @@
  * 4. Show the value of admin using alert (must output “John”).
  */
 
+let admin, name;
+
+name = "John";
+admin = name; 
+alert(admin);
+
 /** Right Naming
  * 1. Create a variable with the name of our planet. How would you name such a variable?
  * 2. Create a variable to store the name of a current visitor to a website. 
  * How would you name that variable?
  */
+
+let ourPlanetName = "Earth";
+let currentUserName = "John Doe";
 
 /**
  * The age constant.
@@ -20,6 +29,9 @@
  * Would it be right to use upper case for birthday? For age? Or even for both?
  */
 
- const birthday = '18.04.1982';
+// birthday is a hard-coded constant whose value is known prior to execution.
+ const BIRTHDAY = '18.04.1982'; // so we name it as an uppercase constant
 
- const age = someCode(birthday);
+ // age is evaluated in run-time, constant in a sense that it does not change
+ // through the code execution. Keep lower case for it. 
+ const age = someCode(BIRTHDAY);
