8f261aaec202150e2da41dfbac2c575c799db09a
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-16 19:51:53 -0800

    Update operators.js

--- JavaScript/2. Fundamentals/2.8 Basic Operators/operators.js
+++ JavaScript/2. Fundamentals/2.8 Basic Operators/operators.js
@@ -36,3 +36,64 @@ done first and then "=" is evaluated storing the result
     ○ Each of the expressions are evaluated but only the result of the last one is returned
 */
 
+/* Terminology - unary, binary, operand */
+let x = 1;
+x = -x;     
+alert( x );     // -1, unary negation was applied
+
+x = 1, y = 3;
+alert( y - x ); // 2, binary minus subtracts values
+
+/* % - Remainder Operator */
+alert( 5 % 2 ); // 1, the remainder of 5 divided by 2
+alert( 8 % 3 ); // 2, the remainder of 8 divided by 3
+alert( 8 % 4 ); // 0, the remainder of 8 divided by 4
+
+// *** - Exponentiation Operator 
+alert( 2 ** 2 ); // 2² = 4
+alert( 2 ** 3 ); // 2³ = 8
+alert( 2 ** 4 ); // 2⁴ = 16
+alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
+
+/* String Concatenation with binary + */
+let s = "my" + "string";
+alert(s);   // mystring
+// If any operands is a string, then the other one is converted to a string too
+alert( '1' + 2 ); // "12"
+alert( 2 + '1' ); // "21"
+// Operators work one after another, so it first sums, returns 4, then concatenates
+alert(2 + 2 + '1' ); // "41" and not "221"
+
+// Only binary + supports strings in such a way, other operators always converts
+// their operands to numbers. e.g., subtraction (-) and division (/)
+alert( 6 - '2' ); // 4, converts '2' to a number
+alert( '6' / '2' ); // 3, converts both operands to numbers
+
+/* Numeric Conversion, Unary "+" 
+When applied to a single value, does nothing to numbers. But if operand is NOT 
+a number, the unary plus (+) converts it into a number. 
+Does the same thing as Number(value) function! (see Type Conversion)     */
+
+// No effect on numbers
+x = 1;
+alert( +x ); // 1
+
+let y = -2;
+alert( +y ); // -2
+
+// Converts non-numbers
+alert( +true ); // 1
+alert( +"" );   // 0
+
+// Unary plus (+) is useful, e.g., getting values from HTML form fields
+let apples = "2";
+let oranges = "3";
+
+alert( apples + oranges ); // "23", the binary plus concatenates strings
+
+// both values converted to numbers before the binary plus
+alert( +apples + +oranges ); // 5
+
+// the longer variant
+// alert( Number(apples) + Number(oranges) ); // 5
