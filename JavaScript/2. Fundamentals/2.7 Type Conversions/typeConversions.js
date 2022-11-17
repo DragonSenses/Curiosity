3ab43ddb3a6c781cd969539b6ce2c5af18042a42
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-16 18:52:50 -0800

    Update typeConversions.js

--- JavaScript/2. Fundamentals/2.7 Type Conversions/typeConversions.js
+++ JavaScript/2. Fundamentals/2.7 Type Conversions/typeConversions.js
@@ -8,21 +8,36 @@ String Conversion
     - false becomes "false", null becomes "null"
 
 Numeric Conversion
-    Use Number(value) functiton to explicitly convert a value to a number. 
+    Use Number(value) function to explicitly convert a value to a number. 
     Also happens in mathematical functions and expressions automatically. 
     - If string is not a valid number, the result of conversion is NaN
     
 Numeric Conversion Rules:
     undefined       -> NaN
     null            -> 0
-    true and false  -> 1 and 0
+    true            -> 1 
+    false           -> 0
 Strings | All whitespaces ("spaces, tabs \t, newlines \n, etc.) are trimmed (removed from start and end)
     empty string    -> 0
     number string   -> number is "read" from string
     error string    -> NaN
 
 Boolean Conversion
-    
+    Happens in logical operations but also be performed explicitly with a call
+    to Boolean(value) 
+
+Boolean Conversion Rules:
+    Values that are intuitively "empty":
+        0               ->  false
+        empty string    ->  false
+        null            ->  false
+        undefined       ->  false
+        NaN             ->  false
+    Non-Empty Values
+        "0"             ->  true
+        " "             ->  true
+         7              ->  true
+        everything else ->  true
 */
 
 /* String Conversion */
@@ -43,3 +58,21 @@ alert(typeof num);       // number
 
 let age = Number("an arbitrary string instead of a number");
 alert(age); // NaN, conversion failed
+
+/* Numeric Conversion Rules  */
+alert( Number("   123   ") ); // 123
+alert( Number("123z") );      // NaN (error reading a number at "z")
+alert( Number(true) );        // 1
+alert( Number(false) );       // 0
+alert( Number(null));         // 0
+alert( Number(undefined));    // NaN
+
+/* Boolean Conversion Rules */
+alert( Boolean(1) ); // true
+alert( Boolean(0) ); // false
+
+alert( Boolean("hello") ); // true
+alert( Boolean("") ); // false
+
+alert( Boolean("0") ); // true
+alert( Boolean(" ") ); // true for spaces (any non-empty string is true)
