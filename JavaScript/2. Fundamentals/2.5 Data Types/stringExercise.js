4bb2b905b91f34fb1457fb8a894f26e33627c836
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-16 16:12:25 -0800

    Create stringExercise.js

---
+++ JavaScript/2. Fundamentals/2.5 Data Types/stringExercise.js
@@ -0,0 +1,11 @@
+/* String Quotes find the output */
+let name = "Illyasviel";
+
+// the expression is a number 1
+alert( `hello ${1}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello Illyasviel
