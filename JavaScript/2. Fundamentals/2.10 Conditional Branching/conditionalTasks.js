7491a1f152caf77d166d5efeb4bee93ec73e38f0
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-17 19:58:54 -0800

    Create conditionalTasks.js

---
+++ JavaScript/2. Fundamentals/2.10 Conditional Branching/conditionalTasks.js
@@ -0,0 +1,10 @@
+/*  if(string zero) or if("0")
+Will alert be shown?
+*/
+if ("0") {
+    alert( 'Hello' );
+}
+// Answer: Yes, Boolean Conversion on "0" results in true, since it is a 
+// non-empty String
+
+
