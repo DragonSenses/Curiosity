47227c953768738627a970ea99c2a824ed33f5aa
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-18 21:55:15 -0800

    Create nullishCoalescing.js

---
+++ JavaScript/2. Fundamentals/2.12 Nullish Coalescing/nullishCoalescing.js
@@ -0,0 +1,14 @@
+/* Nullish Coalescing Operator ?? */
+/* Summary 
+    - The nullish coalescing operator ?? provides a short way to choose the 
+    first defined value from a list.
+    -It's used to assign default values to variables
+        e.g., // set height=100, if height is null or undefined
+        height = height ?? 100;
+
+    - The operator ?? has a very low precedence, only a bit higher than ? and =
+    so consider adding parentheses when using it an expression.
+    - It's forbidden to use it with || or && without explicit parentheses
+*/
+
+
