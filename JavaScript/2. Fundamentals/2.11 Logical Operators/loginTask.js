c54744789510c8f495a2ef0b307dafc2f21ae818
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-18 21:50:59 -0800

    attempt one

--- JavaScript/2. Fundamentals/2.11 Logical Operators/loginTask.js
+++ JavaScript/2. Fundamentals/2.11 Logical Operators/loginTask.js
@@ -1,6 +1,6 @@
-/* Check the login */
+/* Check the login 
 
-/* Write the code which asks for a login with prompt.
+Write the code which asks for a login with prompt.
 
 If the visitor enters "Admin", then prompt for a password, if the input is an
 empty line or Esc – show "Canceled", if it’s another string – then show "I don’t know you".
@@ -17,3 +17,23 @@ Hint: passing an empty input to a prompt returns an empty string ''.
 Pressing ESC during a prompt returns null.
 
 */
+
+let login = prompt("Who's there?",'');
+
+// Check if login is either "Admin", empty line/ESC (empty string), or other.
+if(login == "Admin"){   // login is Admin?
+    let password = prompt("Password?",'');
+
+    if(password == "TheMaster") {   // password is TheMaster?
+        console.log("Welcome!");
+    } else if( (login === '') || (login === null)) {
+        console.log("Canceled"); 
+    } else {
+        console.log("I don't know you");
+    }
+
+} else if( (login === '') || (login === null)){ // strictly an empty string or null?
+    console.log("Canceled");
+} else {    // login is any other String
+    console.log("I don't know you");
+}
