9322b7504aeea84de2a443bcbb982fe81283baef
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-20 14:59:16 -0800

    SecretSanta algorithm implemented

--- Java/Challenges/SecretSanta.java
+++ Java/Challenges/SecretSanta.java
@@ -2,9 +2,6 @@ package Java.Challenges;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A relevant problem cropped up where one is asked to organize a "Secret Santa"
@@ -24,54 +21,80 @@ import java.util.Map;
  *  1 -> 2 -> 3 -> 4 -> 1
  * 
  * Preconditions:
- *      - There is a list of participants' names. 
- *      - A person cannot be their own "Secret Santa" (paired with oneself)
+ * - There is a list of participants' names. 
+ * - A person cannot be their own "Secret Santa" (paired with themselves)
  * 
  * Additional Specifications:
  * 
  * Part 2) Read the list of names from a text file ("names.txt") where each name
  * is separated by a newline
  * 
- * Part 3) After second year of having Secret Santa gift exchange, participants
- * have complained of having the same "Secret Santa" every year. Modify the
- * program so that a participant can only have the same Secret Santa once every
- * 2 years. 
+ * Part 3) Disallowed Pairs: After the 2nd year of having Secret Santa,
+ * participants have complained of having the same "Secret Santa" every year. 
+ * Modify the program so that a participant can only have the same Secret Santa
+ * once every 2 years. 
  * 
- * It's an interesting problem to solve! My diffuse mode thinking was able to 
- * approach this problem through various ways. 
- * 
- * https://stackoverflow.com/questions/273567/secret-santa-algorithm
- * 
- * First Approach (Using Arrays, Streams, groupingBy, partitioning): 
+ * First Approach (Using Circular Array): 
  * 1. Store the names in the array. Shuffle.
- * 2. Create and populate the partition map by iterating through the
- * array. If the index is even, then set to true. If index is odd, then false.
+ * 2. Pair each String to its neighbor (index + 1), except for the last
+ * 3. Pair the last String to the first String (wrap around the Array)
+ * 4. Match Complete
+ * 
+ * Second Approach (Circularly Linked List)
+ * 
+ * Third Approach (Graph - each node has one outgoing edge to another node)
+ * a directed graph where every vertex has indegree = outdegree = 1.
  */ 
 public class SecretSanta {
-    private String[] participants;
-    private Map<Boolean, List<String>> partitionedNames;
 
-    // private static void storeAndShuffle(String[] arr){
-    //     Collections.shuffle(Arrays.asList(arr));
-    // }
+    /**
+     * Shuffles the array of String names. Randomizes the order.
+     * @param arr array of String names
+     */
+    private static void shuffle(String[] arr){
+        Collections.shuffle(Arrays.asList(arr));
+    }
+
+    /**
+     * Prints out the Secret Santa for each name
+     * @param arr array of String names
+     */
+    private static void print(String[] arr){
+        StringBuilder sb = new StringBuilder();
+        int len = arr.length;
+
+        // next index uses modular arithmetic to wrap around the array
+        int next = (0 + 1) % len;  
+
+        for(int k = 0; k < len; k++){
+            // format: Pair#)   name -> nextName
+            sb.append(k+1 + ")\t" + arr[k]  + " -> " + arr[next] + "\n");
+            next = (next+1) % len;
+        }
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * Combines all the steps to produce the list of "Secret Santa" for each
+     * name in the array of String names.
+     * @param arr array of String names
+     */
+    private static void make(String[] arr){
+        shuffle(arr);
+        print(arr);
+    }
 
     public static void main(String[] args){
         String[] arr = {"a","b","c","d","e","f","g","h"};
-        System.out.println(arr.length);
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Names before Shuffle:\t" + Arrays.toString(arr));
 
-        Collections.shuffle(Arrays.asList(arr));
-        System.out.println(Arrays.toString(arr));
+        shuffle(arr);
+        System.out.println("Names after Shuffle:\t" + Arrays.toString(arr));
 
+        print(arr);
 
-        Map<Boolean, List<String>> map = new HashMap<Boolean, List<String>>();
-        for(int i=0; i < arr.length; i++){
-            if((i & 1) == 0){ // even index?
-                // map.put(true,arr[i]);    // to do
-            } else {
-
-            }
-        }
+        String[] oddArr = {"Apple", "Mango", "Banana", "Orange", "Blueberry"};
+        make(oddArr);
     }
 
 }
