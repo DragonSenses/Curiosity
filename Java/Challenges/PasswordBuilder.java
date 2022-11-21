ab79cd04e4969b36f027e58645b52f99692fe9e7
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-21 01:47:37 -0800

    Create PasswordBuilder.java

---
+++ Java/Challenges/PasswordBuilder.java
@@ -0,0 +1,20 @@
+package Java.Challenges;
+
+// Cryptographically secure random number generator
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Generates a password with a given length. Ensures password policies that 
+ * contain: uppercase/lowercase/numbers/symbols. Using the Builder pattern
+ * allows the user to customize their password by setting a minimum count
+ * of characters of each category.
+ */
+public class PasswordBuilder {
+    
+}
