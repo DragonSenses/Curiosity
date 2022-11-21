e8a8695207cb573d7eba1708aa4dbdb0893b66ce
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-20 17:00:11 -0800

    Create switchTask.js

---
+++ JavaScript/2.14 Switch Statement/switchTask.js
@@ -0,0 +1,33 @@
+/* Rewrite switch statement into an if */
+
+switch (browser) {
+    case 'Edge':
+      alert( "You've got the Edge!" );
+      break;
+  
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+      alert( 'Okay we support these browsers too' );
+      break;
+  
+    default:
+      alert( 'We hope that this page looks ok!' );
+}
+
+
+/* Rewrite if statement into a switch statement */
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
