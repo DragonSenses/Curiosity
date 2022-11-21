201e8775bdf66e669c00bd73b80668f89ceba594
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-20 16:59:08 -0800

    Update switchStatement.js

--- JavaScript/2.14 Switch Statement/switchStatement.js
+++ JavaScript/2.14 Switch Statement/switchStatement.js
@@ -73,4 +73,62 @@ of the three alerts
     alert( "I don't know such values" );
 */
 
-/*  */
+/* Grouping "cases" as a side effect of no break */
+a = 3;
+
+switch (a) {
+  case 4:
+    alert('Right!');
+    break;
+
+  case 3: // (*) grouped two cases
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
+
+  default:
+    alert('The result is strange. Really.');
+}
+// Here we group cases 3 and 5, showing the same message, as execution starts
+// from case 3 line and goes through case 5 because there's no break
+
+/* Any expression can be a switch/case argument */
+a = "1";
+let b = 0;
+
+switch (+a) {
+  case b + 1:
+    alert("this runs, because +a is 1, exactly equals b+1");
+    break;
+
+  default:
+    alert("this doesn't run");
+}
+// Here +a gives 1, compared with b + 1 in case, and the corresponding code is executed
+
+/* Type Matters - equality check is always strict so values must be of the same
+type to match */
+
+let arg = prompt("Enter a value?");
+switch (arg) {
+  case '0':
+  case '1':
+    alert( 'One or zero' );
+    break;
+
+  case '2':
+    alert( 'Two' );
+    break;
+
+  case 3:
+    alert( 'Never executes!' );
+    break;
+  default:
+    alert( 'An unknown value' );
+}
+
+// case 0, 1 the first alert runs
+// case 2 the second alert runs
+// case 3, the result of the prompt is a string "3", which is not strictly
+// equal to the number 3. So it is dead code. default variant will execute.
