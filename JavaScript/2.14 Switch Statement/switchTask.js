e56100007ff228d681ea4a0f1f085f081e5f5175
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-20 17:39:47 -0800

    Update switchTask.js

--- JavaScript/2.14 Switch Statement/switchTask.js
+++ JavaScript/2.14 Switch Statement/switchTask.js
@@ -1,3 +1,5 @@
+let browser;
+
 /* Rewrite switch statement into an if */
 
 switch (browser) {
@@ -16,6 +18,17 @@ switch (browser) {
       alert( 'We hope that this page looks ok!' );
 }
 
+// Answer:
+if(browser == 'Edge'){
+    alert( "You've got the Edge!" );
+} else if(browser == 'Chrome' 
+        || browser == 'Firefox'
+        || browser == 'Safari'
+        || browser == 'Opera' ){
+    alert( 'Okay we support these browsers too' );
+} else {
+    alert( 'We hope that this page looks ok!' );
+}
 
 /* Rewrite if statement into a switch statement */
 
@@ -30,4 +43,25 @@ if (a == 1) {
 
 if (a == 2 || a == 3) {
   alert( '2,3' );
+}
+
+// Answer:
+/* Recall that + unary operator applied to non-numbers converts it into a number
+(same as Number(val) function). The return value of prompt() function is a 
+String. So +prompt() returns the String it receives converted into a number. 
+*/
+
+switch(a){
+    case 0:
+        alert( 0 );
+        break;
+
+    case 1:
+        alert( 1 );
+        break;
+
+    case 2:
+    case 3:
+        alert( '2,3' );
+        break;  // break future-proofs when adding more cases later
 }
