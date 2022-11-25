3554d35bf2851d5889873a006da44cee6842d05f
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-24 17:32:01 -0800

    Update power.js

--- JavaScript/2. Fundamentals/2.15 Functions/power.js
+++ JavaScript/2. Fundamentals/2.15 Functions/power.js
@@ -1,5 +1,6 @@
 /* The function pow(x,n) returns x raise to the power of n,
-without using exponentiation operator "**" */     
+without using exponentiation operator "**". It uses a for loop running n-1
+times. So run time is O(n). */     
 function pow(x, n) {
   let result = x;
 
@@ -10,7 +11,7 @@ function pow(x, n) {
   return result;
 }
 
-
+/* Prompts the user for input values x, and n, then returns x^n */
 let x = prompt("x?", "");
 let n = prompt("n?", "");
 
@@ -21,3 +22,27 @@ if (n < 1) {
   alert(`${x}^${n} is ` + pow(x, n));
 }
 
+
+/* A recursive function that employs a Divide-and-Conquer to get the
+result x^n. Run time should be O(log n) as it halves n. A slightly
+better way to  */
+function power(x,n){
+    let result;
+
+    // Base Case
+    if ( n == 0) {
+        return 1;
+    }
+
+    // Recursive calls halves n by 2, store result
+    result = power(x, n/2);
+
+    // is n even? Check least-significant bit. 
+    if( n & 1 == 0){
+        // Even then return 
+        return result * result;
+    } else { 
+        return x * result * result; 
+    }
+
+}
