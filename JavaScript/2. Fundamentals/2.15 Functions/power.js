a8c570598033615c32f2dbfa1691812d22e873f5
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-24 18:03:09 -0800

    Bug fixed in recursive call.

--- JavaScript/2. Fundamentals/2.15 Functions/power.js
+++ JavaScript/2. Fundamentals/2.15 Functions/power.js
@@ -19,30 +19,40 @@ let n = prompt("n?", "");
 if (n < 1) {
   alert(`Power ${n} is not supported, use a positive integer`);
 } else {
-  alert(`${x}^${n} is ` + pow(x, n));
+  alert(`${x}^${n} is ` + power(x, n));
 }
 
 
-/* A recursive function that employs a Divide-and-Conquer to get the
+/* 
+A recursive function that employs a Divide-and-Conquer to get the
 result x^n. Run time should be O(log n) as it halves n. A slightly
-better way to  */
+better way to  
+
+Bug Fixed: the statement ( n & 1 == 0) produces an infinite loop, because of
+Operator Precedence. 
+    - Bitwise & has a precedence of 4, yet equality "==" has a precedence of 8.
+
+    So this always evaluates to (n & (1 == 0)) -> (n & 0) -> (0) any number 
+    n & 0 will always produce since 0 has no set bits, and ANDING 
+    with 0 will result in 0. So condition is always falsy. 
+*/
 function power(x,n){
     let result;
 
     // Base Case
-    if ( n == 0) {
-        return 1;
-    }
+    if ( n == 0) { return 1; }
 
-    // Recursive calls halves n by 2, store result
+    // Recursive calls halves n by 2
     result = power(x, n/2);
 
-    // is n even? Check least-significant bit. 
-    if( n & 1 == 0){
-        // Even then return 
+    // Is exponent n even? Check least-significant bit. 
+    return ( (n & 1) == 0) ? (result * result) : (x * result * result);
+
+    /*
+    if( (n & 1) == 0){
         return result * result;
-    } else { 
+    } else {  
         return x * result * result; 
     }
-
+    */
 }
