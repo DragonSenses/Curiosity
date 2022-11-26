0321bdec68e90e00de8a34eb4e207580eeaf7e39
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-25 21:41:04 -0800

    Update specials.js

--- JavaScript/2. Fundamentals/2.18 JavaScript Specials/specials.js
+++ JavaScript/2. Fundamentals/2.18 JavaScript Specials/specials.js
@@ -36,9 +36,55 @@ Directive must be at top of script or beginning of a function body.
     - let
     - const (constant, can't be changed) 
     - var (old-style)
+*/
 
-Variable Names can include
+/* Variable Names can include:
 - Letters and digits, but the first character may not be a digit.
 - Characters $ and _ are normal, on par with letters.
 - Non-Latin alphabets and hieroglyphs are also allowed, but commonly not used.
 */
+
+/* Variables are Dynamically Typed. Can store any value: */
+let x = 5;
+x = "John";
+
+/* 8 Data Types:
+    1. number for both floating-point and integer numbers,
+    2. bigint for integer numbers of arbitrary length,
+    3. string for strings,
+    4. boolean for logical values: true/false,
+    5. null – a type with a single value null, meaning “empty” or “does not exist”,
+    6. undefined – a type with a single value undefined, meaning “not assigned”,
+    7. symbol for unique identifiers
+    8. object for complex data structures 
+*/
+
+/* typeof operator returns the type for a value with two exceptions */
+typeof null == "object" // error in the language
+typeof function(){} == "function" // functions are treated specially
+
+
+/* Interaction, using a browser as a working environment 
+Basic UI functions will be:
+    - prompt(question, [default])
+    Ask a question, and return either what the visitor entered or null if 
+    they clicked "cancel".
+    
+    - confirm(question)
+    Ask a question and suggest to choose between Ok and Cancel. 
+    The choice is returned as true/false.
+
+    - alert(message)
+    Output a message.
+
+All these functions are "Modal", they pause the code execution and prevent the
+visitor from interacting with the page until they answer.
+*/
+let userName = prompt("Your name?", "Alice");
+let isTeaWanted = confirm("Do you want some tea?");
+
+alert( "Visitor: " + userName ); // Alice
+alert( "Tea wanted: " + isTeaWanted ); // true
+
+
+/*  */
