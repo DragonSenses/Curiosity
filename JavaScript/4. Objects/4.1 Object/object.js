d6680ac5301b47c02624ad77dc33c9321c81ecf9
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-27 11:35:37 -0800

    Update object.js

--- JavaScript/4. Objects/4.1 Object/object.js
+++ JavaScript/4. Objects/4.1 Object/object.js
@@ -26,3 +26,50 @@ They have their special features. Some call them "Array type" or "Date type",
 but formally they are not types of their own, but belong to a single "object" 
 data type. And they extend it in various ways. 
 */
+
+/* Creating an empty object
+  Object can be created with figure brackets {...} with an optional list of
+properties. 
+  Properties are Key:Value pairs, where key is a String called a 
+property name, and value can be anything. 
+*/
+
+let user1 = new Object();  // "object constructor" syntax
+let user2 = {};  // "object literal" syntax
+
+/* Literals and Properties 
+A property has a key (aka name or identifier) before the colon ":" and a value
+to the right of it. Note the comma "," after every property except the last.
+
+The last property in the list may end with a comma, called a trailing/hanging 
+comma, which makes it easier to add/remove/move around properties, because all
+lines are alike.
+*/
+
+let user = {    // an object
+  name: "Leo",  // by key "name" store value "Leo"
+  age: 20       // by key "age" store value 30
+};
+
+// In the user object, there are two properties
+// <name,"Leo"> - first proeprty has the name "name" and value of "Leo"
+// <age,20> - second property has name "age" and value of 30
+
+/* Access Property values via dot notation: */
+alert( user.name ); // Leo
+alert( user.age );  // 20
+
+/* Adding a Property */
+user.isAdmin = true;
+
+/* Removing a Property using delete operator */
+delete user.age;
+
+/* Multiword property names can be used but must be quoted */
+user = {
+  name: "Luna",
+  age: 20,
+  "likes birds": true,   // multiword property name must be quoted
+}                        // Last property in list may end with a comma ,
+
+/* Square Brackets */
