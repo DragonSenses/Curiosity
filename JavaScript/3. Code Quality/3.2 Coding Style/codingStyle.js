a31ef1dbc3cfac6be666d231df48a8acfd51d3d7
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-26 17:10:41 -0800

    Create codingStyle.js

---
+++ JavaScript/3. Code Quality/3.2 Coding Style/codingStyle.js
@@ -0,0 +1,28 @@
+/* Coding Style
+The art of programming is to take a complex task and code it in a way that is 
+both correct and human-readable. A good code style greatly assists in that.
+
+Rules on Syntax aim to increase the readability of your code. They are subject
+to dispute. The main things to ask are:
+    - "What makes code more redable and easier to understand"
+    - "What can help us avoid errors?"
+
+Popular style guides allow you to keep up to date with latest ideas about code 
+style trends and best practices.
+*/
+
+/* Syntax Suggestions
+- No space between function name and parentheses, 
+between parentheses and the parameter
+- A space between parameters
+- Curly brace { on the same line, after a space
+- Indentation 2 spaces
+- A space after for/if/while...
+- Spaces around operators
+- A semicolon ; is mandatory
+- A space between arguments
+- An empty line between logical blocks
+- else { without a line break
+- Spaces around a nested call
+- Lines are not very long
+*/
