a1417b18d891574dd83a18d10bc8e292d46a9312
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-27 21:41:48 -0800

    Update codingStyle.js

--- JavaScript/3. Code Quality/3.2 Coding Style/codingStyle.js
+++ JavaScript/3. Code Quality/3.2 Coding Style/codingStyle.js
@@ -77,3 +77,94 @@ if (
   ) {
     letTheSorceryBegin();
 }
+
+/* Indents */
+/* 2 Types of Indents:
+1. Horizontal indents: 2 or 4 spaces.
+    A horizontal indentation is made using either 2 or 4 spaces or the 
+    horizontal tab symbol (key Tab). Which one to choose is an old holy war. 
+    Spaces are more common nowadays.
+
+One advantage of spaces over tabs is that spaces allow more flexible 
+configurations of indents than the tab symbol.
+
+2. Vertical indents: empty lines for splitting code into logical blocks.
+    Even a single function can often be divided into logical blocks. 
+
+    Insert an extra newline where it helps to make the code more readable. 
+    There should not be more than nine lines of code without a vertical indentation.
+
+In the example below, the initialization of variables, the main loop and 
+returning the result are split vertically:
+*/
+
+function pow(x, n) {
+    let result = 1;
+    //              <--
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+    //              <--
+    return result;
+}
+
+/* Semicolons */
+/* A semicolon should be present after each statement, even if it 
+could possibly be skipped. */
+
+/* Nesting Levels */
+/* Try to avoid nesting code too many levels deep.
+
+e.g., in the loop, it’s sometimes a good idea to use the continue directive 
+    to avoid extra nesting.
+
+e.g., instead of adding a nested if conditional like this:
+*/
+for (let i = 0; i < 10; i++) {
+    if (cond) {
+       // ... <- one more nesting level
+    }
+}
+
+// Instead, we can write:
+for (let i = 0; i < 10; i++) {
+    if (!cond) continue;
+      // ... <- no extra nesting level
+}
+
+/* A similar thing can be done with if/else and return 
+    e.g., Two Constructs below are identical:
+*/
+
+function pow(x, n) {    // Option 1
+    if (n < 0) {
+      alert("Negative 'n' not supported");
+    } else {
+      let result = 1;
+  
+      for (let i = 0; i < n; i++) {
+        result *= x;
+      }
+  
+      return result;
+    }
+}
+
+function pow(x, n) {    // Option 2
+    if (n < 0) {
+      alert("Negative 'n' not supported");
+      return;
+    }
+  
+    let result = 1;
+  
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+}
+
+// Second one is more redable because the "special case" of n < 0 is handled
+// very early on. Once the check is done we can move on to the "main" code
+// flow without the need for additional nesting
