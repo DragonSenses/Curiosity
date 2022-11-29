147f0f44399a2e48c508313ba67ddace1a6c6ad2
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-28 16:59:25 -0800

    Update objRefences.js

--- JavaScript/4. Objects/4.2 Object References and Copying/objRefences.js
+++ JavaScript/4. Objects/4.2 Object References and Copying/objRefences.js
@@ -17,3 +17,40 @@ so-called "shallow copy" (nested objects are copied by reference) or a
 "deep cloning" function structuredClone or use a custom cloning implementation,
 such as _.cloneDeep(obj)
 */
+
+// Copying a primitive (string) 
+let message = "Hello!";
+let phrase = message;
+
+// Two independent variables, each one storing the string "Hello!"
+
+
+/* Copying an Object */
+/* Variable assigned to an object stores NOT the object itself, but its 
+"address in memory" or "reference" to it 
+
+The object is stored somewhere in memory, while the user variable has a  
+reference to it. When we perform actions with the object, the JavaScript 
+engine looks at what address and performs the operations on the actual object. 
+
+When a variable is copied, the reference is copied, but the object itself is
+not duplicated.
+*/
+
+let user = {
+    name: "Leo"
+};
+
+let admin = user; // copy the reference
+
+// Now we have two variables, each storing a reference to the same object
+
+// Changing the "name" property using the "admin" reference
+admin.name = 'Luna';     
+
+// Calling the "name" property through the "user" reference
+console.log(user.name); // Luna
+
+
+/* Comparison by Reference */
+/* Two objects are equal only if they are the same object.  */
