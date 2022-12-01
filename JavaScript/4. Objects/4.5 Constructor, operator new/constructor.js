6a384647e1a3048e81a4e93c65f76c01ce64ed5b
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-30 18:22:48 -0800

    Create constructor.js

---
+++ JavaScript/4. Objects/4.5 Constructor, operator new/constructor.js
@@ -0,0 +1,12 @@
+/* Constructor, operator "new"
+Summary
+ - Constructor functions or, briefly, constructors, are regular functions, 
+ but there’s a common agreement to name them with capital letter first.
+ - Constructor functions should only be called using new. Such a call implies 
+ a creation of empty this at the start and returning the populated one at the end.
+
+We can use constructor functions to make multiple similar objects.
+
+JavaScript provides constructor functions for many built-in language objects: 
+like Date for dates, Set for sets & others 
+*/
