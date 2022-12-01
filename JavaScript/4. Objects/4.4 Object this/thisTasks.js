660fd3eb0cff8545b3e859dc4070302e1cd68221
DragonSenses <kendrickfrancisco@gmail.com>
2022-11-30 19:08:34 -0800

    Update thisTasks.js

--- JavaScript/4. Objects/4.4 Object this/thisTasks.js
+++ JavaScript/4. Objects/4.4 Object this/thisTasks.js
@@ -12,7 +12,48 @@ function makeUser() {
 
 let user = makeUser();
 
-alert(user.ref.name); // What's the result?
+console.log(user.ref.name); // Error: Cannot read property 'name' of undefined
+
+/* Answer: an error. 
+Result is an Error: "cannot read property 'name' of undefined" because
+rules that set "this" do not look at object definition. Only the moment of call
+matters.
+
+Here the value of "this" inside "makeUser()" is undefined, because it is called
+as a function, not as a method wtih "dot" syntax.
+
+The value of "this" is one for the whole function, code blocks and object 
+literals do not affect it.
+
+So "ref: this" actually takes current "this" of the function.
+
+Can rewrite the function and return the same "this" with undefined value:
+*/
+function makeUser() {
+    return this; // this time there's no object literal
+}
+
+console.log(makeUser().name); // Error: Cannot read property 'name' of undefined
+
+/* Result of console.log(makeUser().name) is the same result of
+console.log(user.ref.name) from previous example 
+
+To fix, make user.ref() a method. And the value of this is set to the object
+before the dot.
+*/
+function makeUser() {
+    return {
+        name: "John",
+        ref() {
+            return this;
+        }
+    };
+}
+
+user = makeUser();
+
+console.log(user.ref().name); // John
+
 
 
 /* Create a calculator
