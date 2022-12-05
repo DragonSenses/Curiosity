b596fa1fef254e0f408053b2edb4fb3726a17abe
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-04 17:09:38 -0800

    Update constructorTasks.js
    enforce semicolons at end of functions

--- JavaScript/4. Objects/4.5 Constructor, operator new/constructorTasks.js
+++ JavaScript/4. Objects/4.5 Constructor, operator new/constructorTasks.js
@@ -39,11 +39,11 @@ function Calculator(){
   this.read = function() {
     this.x = +prompt("x?",0);
     this.y = +prompt("y?",0);
-  }
+  };
 
   this.sum = function(){
     return this.x + this.y;
-  }
+  };
 
   this.mul = function(){
     return this.x * this.y;
