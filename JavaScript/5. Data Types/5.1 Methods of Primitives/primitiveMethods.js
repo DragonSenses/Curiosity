08e419c0b4605ac9fe28604eb946d829964fbb95
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-05 16:20:22 -0800

    Update primitiveMethods.js

--- JavaScript/5. Data Types/5.1 Methods of Primitives/primitiveMethods.js
+++ JavaScript/5. Data Types/5.1 Methods of Primitives/primitiveMethods.js
@@ -38,3 +38,47 @@ Objects are "heavier" than primitives, i.e. they require additional resources
 to support the internal machinery. 
 */
 
+/* A primitive as an object
+
+The Paradox faced by creator of JavaScript:
+  - There are many things one would want to do with a primitive, it would be great
+  to access them using methods
+  - Primitives must be as fast and lightweight as possible
+
+Solution:
+  1. Primitives are still primitive. A single value, as desired.
+  2. The language allows access to methods and properties of strings, numbers, booleans and symbols.
+  3. In order for that to work, a special “object wrapper” that provides the 
+  extra functionality is created, and then is destroyed.
+
+The “object wrappers” are different for each primitive type and are called: 
+  String, Number, Boolean, Symbol and BigInt. 
+  
+Thus, they provide different sets of methods.
+
+For instance, there exists a string method str.toUpperCase() that 
+returns a capitalized str. e.g:
+*/
+let str = "Hello";
+alert( str.toUpperCase() ); // HELLO
+
+/* What actually happens in str.toUpperCase()
+1. The string str is a primitive. So in the moment of accessing its property, 
+  a special object is created that knows the value of the string, and has 
+  useful methods, like toUpperCase().
+
+2. That method runs and returns a new string (shown by alert).
+
+3. The special object is destroyed, leaving the primitive str alone.
+
+So primitives can provide methods, but they still remain lightweight.
+
+The JavaScript engine highly optimizes this process. It may even skip the 
+creation of the extra object at all. But it must still adhere to the 
+specification and behave as if it creates one.
+*/
+
+/* A number has methods of its own, for instance, toFixed(n) rounds 
+the number to the given precision: */
+let n = 1.23456;
+alert( n.toFixed(2) ); // 1.23
