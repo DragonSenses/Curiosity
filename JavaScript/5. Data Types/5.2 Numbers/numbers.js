d7ff3fcacdafd3a2cae9a2e8e5838e15d751997d
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-06 20:16:21 -0800

    Update numbers.js

--- JavaScript/5. Data Types/5.2 Numbers/numbers.js
+++ JavaScript/5. Data Types/5.2 Numbers/numbers.js
@@ -1,4 +1,40 @@
 /* Numbers in modern JavaScript */
+/* Summary
+To write numbers with many zeros:
+  -Append "e" with the zeroes count to the number. 
+  Like: 123e6 is the same as 123 with 6 zeroes 123000000.
+  -A negative number after "e" causes the number to be 
+  divided by 1 with given zeroes. E.g. 123e-6 means 0.000123 (123 millionths).
+
+For different numeral systems:
+  -Can write numbers directly in hex (0x), octal (0o) and binary (0b) systems.
+  -parseInt(str, base) parses the string str into an integer in numeral 
+  system with given base, 2 ≤ base ≤ 36.
+  -num.toString(base) converts a number to a string in the numeral system 
+  with the given base.
+
+For regular number tests:
+  -isNaN(value) converts its argument to a number and then tests it for being NaN
+  -Number.isNaN(value) checks whether its argument belongs to the number type, 
+  and if so, tests it for being NaN
+  -isFinite(value) converts its argument to a number and then tests it for not 
+  being NaN/Infinity/-Infinity
+  -Number.isFinite(value) checks whether its argument belongs to the number type, 
+  and if so, tests it for not being NaN/Infinity/-Infinity
+
+For converting values like 12pt and 100px to a number:
+  -Use parseInt/parseFloat for the “soft” conversion, which reads a number from 
+  a string and then returns the value they could read before the error.
+
+For fractions:
+  -Round using Math.floor, Math.ceil, Math.trunc, Math.round or num.toFixed(precision).
+  -Make sure to remember there’s a loss of precision when working with fractions.
+
+More mathematical functions:
+  -See the Math object when you need them. The library is very small, 
+  but can cover basic needs.
+*/
+
 /* Two types of numbers:
   1. Regular numbers in JavaScript are stored in 64-bit format IEEE-754, also 
   known as “double precision floating point numbers”.
@@ -340,3 +376,20 @@ alert( parseInt('2n9c', 36) ); // 123456
 
 
 /* Other Math Functions */
+/* JavaScript has a built-in Math object which contains a small library of 
+mathematical functions and constants. 
+
+  Math.random() - Returns a random number from 0 to 1 (not including 1).
+*/
+alert( Math.random() ); // 0.1234567894322
+alert( Math.random() ); // 0.5435252343232
+alert( Math.random() ); // ... (any random numbers)
+
+/* Math.max(a, b, c...) and Math.min(a, b, c...)  
+  Returns the greatest and smallest from the arbitrary number of arguments.
+*/
+alert( Math.max(3, 5, -10, 0, 1) ); // 5
+alert( Math.min(1, 2) ); // 1
+
+/* Math.pow(n, power) - Returns n raised to the given power. */
+/* alert( Math.pow(2, 10) ); // 2 in power 10 = 1024 */
