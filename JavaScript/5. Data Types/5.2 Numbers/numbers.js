6b65b0100e0fc6a5d8d3a1daf98d4849a199d753
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-06 19:55:19 -0800

    Update numbers.js

--- JavaScript/5. Data Types/5.2 Numbers/numbers.js
+++ JavaScript/5. Data Types/5.2 Numbers/numbers.js
@@ -287,3 +287,56 @@ than isNaN and isFinite functions. In practice though, isNaN and isFinite
 are mostly used, as they’re shorter to write. */
 
 /* Comparison with Object.is */
+/* There is a special built-in method Object.is that compares values like ===,
+but is more reliable for two edge cases: 
+
+1. It works with NaN: Object.is(NaN, NaN) === true, that’s a good thing.
+2. Values 0 and -0 are different: Object.is(0, -0) === false, technically 
+that’s correct, because internally the number has a sign bit that may be 
+different even if all other bits are zeroes.
+
+In all other cases, Object.is(a, b) is the same as a === b.
+
+We mention Object.is here, because it’s often used in JavaScript specification.
+ When an internal algorithm needs to compare two values for being exactly the 
+ same, it uses Object.is (internally called SameValue).
+*/
+
+/* parseInt and parseFloat */
+/* Numeric conversion using a plus + or Number() is strict. If a value is not 
+exactly a number, it fails: */
+alert( +"100px" ); // NaN
+
+/* The sole exception is spaces at the beginning or at the end of the string, 
+as they are ignored. 
+
+But in real life we often have values in units, like "100px" or "12pt" in CSS. 
+Also in many countries the currency symbol goes after the amount, 
+so we have "19€" and would like to extract a numeric value out of that.
+
+That’s what parseInt and parseFloat are for.
+
+They “read” a number from a string until they can’t. In case of an error, 
+the gathered number is returned. The function parseInt returns an integer, 
+whilst parseFloat will return a floating-point number:
+*/
+alert( parseInt('100px') ); // 100
+alert( parseFloat('12.5em') ); // 12.5
+
+alert( parseInt('12.3') ); // 12, only the integer part is returned
+alert( parseFloat('12.3.4') ); // 12.3, the second point stops the reading
+
+// There are situations when parseInt/parseFloat will return NaN. 
+// It happens when no digits could be read:
+alert( parseInt('a123') ); // NaN, the first symbol stops the process
+
+/* The second argument of parseInt(str, radix) 
+The parseInt() function has an optional second parameter. It specifies the 
+base of the numeral system, so parseInt can also parse strings of hex numbers, 
+binary numbers and so on:*/
+alert( parseInt('0xff', 16) ); // 255
+alert( parseInt('ff', 16) ); // 255, without 0x also works
+alert( parseInt('2n9c', 36) ); // 123456
+
+
+/* Other Math Functions */
