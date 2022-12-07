bcd543553f99f5ac9a02fa39a718c526afd0bf3e
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-06 19:34:36 -0800

    Update numbers.js

--- JavaScript/5. Data Types/5.2 Numbers/numbers.js
+++ JavaScript/5. Data Types/5.2 Numbers/numbers.js
@@ -225,3 +225,65 @@ treat them as the same.
 
 
 /* Tests: isFinite and isNaN */
+/* - Infinity (and -Infinity) is a special numeric value that is greater (less) than anything.
+   - NaN represents an error. 
+   
+They belong to the type number, but are not “normal” numbers, so there are 
+special functions to check for them:
+  - isNaN(value) converts its argument to a number and then tests it for being NaN:
+*/
+alert( isNaN(NaN) ); // true
+alert( isNaN("str") ); // true
+
+/* But do we need this function? Can’t we just use the comparison === NaN? 
+Unfortunately not. The value NaN is unique in that it does not equal anything, 
+including itself: */
+// eslint-disable-next-line use-isnan
+alert( NaN === NaN ); // false
+
+/* isFinite(value) converts its argument to a number and returns true 
+if it’s a regular number, not NaN/Infinity/-Infinity: */
+alert( isFinite("15") ); // true
+alert( isFinite("str") ); // false, because a special value: NaN
+alert( isFinite(Infinity) ); // false, because a special value: Infinity
+
+/* Sometimes isFinite is used to validate whether a string value is a regular number: */
+num = +prompt("Enter a number", '');
+
+// will be true unless you enter Infinity, -Infinity or not a number
+alert( isFinite(num) );
+
+/* Please note that an empty or a space-only string is treated as 0 in 
+all numeric functions including isFinite. */
+
+/* Number.isNaN and Number.isFinite */
+/* Number.isNaN and Number.isFinite methods are the more “strict” versions of 
+isNaN and isFinite functions. They do not autoconvert their argument into a number, 
+but check if it belongs to the number type instead. 
+
+Number.isNaN(value) returns true if the argument belongs to the number type 
+and it is NaN. In any other case it returns false.
+*/
+alert( Number.isNaN(NaN) ); // true
+alert( Number.isNaN("str" / 2) ); // true
+
+// Note the difference:
+alert( Number.isNaN("str") ); // false, because "str" belongs to the string type, not the number type
+// Because isNaN converts string "str" into a number and gets NaN as a result of this conversion
+alert( isNaN("str") ); // true
+
+/* Number.isFinite(value) returns true if the argument belongs to the number 
+type and it is not NaN/Infinity/-Infinity. In any other case it returns false. */
+alert( Number.isFinite(123) ); // true
+alert( Number.isFinite(Infinity) ); // false
+alert( Number.isFinite(2 / 0) ); // false
+
+// Note the difference:
+alert( Number.isFinite("123") ); // false, because "123" belongs to the string type, not the number type
+alert( isFinite("123") ); // true, because isFinite converts string "123" into a number 123
+
+/* In a way, Number.isNaN and Number.isFinite are simpler and more straightforward 
+than isNaN and isFinite functions. In practice though, isNaN and isFinite 
+are mostly used, as they’re shorter to write. */
+
+/* Comparison with Object.is */
