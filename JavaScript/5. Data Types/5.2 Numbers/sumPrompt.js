059fe8f0c3e7b6fbdb564d3ca64a7ea9b8544066
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-07 16:03:21 -0800

    Update sumPrompt.js

--- JavaScript/5. Data Types/5.2 Numbers/sumPrompt.js
+++ JavaScript/5. Data Types/5.2 Numbers/sumPrompt.js
@@ -1,6 +1,17 @@
 "use strict";
 
-let x = +prompt("x?",0);
-let y = +prompt("y?",0);
+/* Create a script that prompts the visitor to enter two numbers and then shows their sum. 
+P.S. There is a gotcha with types.
 
-alert(x+y);
+    Since prompt returns a string, the sum of both x and y would be their 
+    concatenation, e.g., "1" + "2" = "12"
+
+    Therefore, the unary plus is added before prompt. It immediately converts
+    the value to a number. And so the sum between x and y will return the 
+    sum of both numbers.
+*/
+
+let x = +prompt("The first number?", "");
+let y = +prompt("The second number?", "");
+
+alert( x + y );
