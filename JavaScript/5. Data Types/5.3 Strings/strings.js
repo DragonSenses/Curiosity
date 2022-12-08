1cce5392fd7566e9a92e0dd31107b50adb838a2e
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-07 21:42:41 -0800

    Update strings.js

--- JavaScript/5. Data Types/5.3 Strings/strings.js
+++ JavaScript/5. Data Types/5.3 Strings/strings.js
@@ -133,4 +133,85 @@ Unicode codes \u...., it’s rarely used and is covered later in Unicode section
 
 /* String Length */
 /* The length property has the string length: */
+
 console.log( `My\n`.length ); // 3
+// Note that \n is a single “special” character, so the length is indeed 3.
+
+/* Length is a property. People with a background in some other languages
+sometimes mistype by calling str.length() instead of just str.length. 
+
+str.length is a numeric property, not a function. 
+No need to add parentheses after it. Not .length(), but .length */
+
+/* Accessing Characters */
+/* To get a  character at position pos, use square brackets [pos] or call the 
+method str.at(pos). The first character starts from the zero position: */
+let str = `Hello`;
+
+// the first character
+alert( str[0] ); // H
+alert( str.at(0) ); // H
+
+// the last character
+alert( str[str.length - 1] ); // o
+alert( str.at(-1) );
+
+/* As you can see, the .at(pos) method has a benefit of allowing negative position. 
+If pos is negative, then it’s counted from the end of the string. 
+
+So .at(-1) means the last character, and .at(-2) is the one before it, etc.
+
+The square brackets always return undefined for negative indexes, for instance: */
+// let str = `Hello`;
+alert( str[-2] ); // undefined
+alert( str.at(-2) ); // l
+
+/* We can also iterate over characters using for..of: */
+for (let char of "Hello") {
+    alert(char); // H,e,l,l,o (char becomes "H", then "e", then "l" etc)
+}
+
+/* Strings are Immutable */
+/* Strings can't be changed in JavaScript, it is impossible to change a character */
+str = 'Hi';
+
+str[0] = 'h';    // error
+alert( str[0] ); // doesn't work  
+
+/* The usual workaround is to create a whole new string and assign it to str 
+instead of the old one. For instance: */
+str = 'Hi';
+
+str = 'h' + str[1]; // replace the string
+
+alert( str ); // hi
+
+
+/* Changing the case */
+/* Methods toLowerCase() and toUpperCase() change the case: */
+console.log( 'Interface'.toUpperCase() ); // INTERFACE
+console.log( 'Interface'.toLowerCase() ); // interface
+
+// Or, if we want a single character lowercased:
+console.log( 'Interface'[0].toLowerCase() ); // 'i'
+
+/* Searching for a substring */
+/* Multiple wayts to look for a substring within a string. 
+
+str.indexOf(substr,pos) - looks for substr in str, starting from the given 
+position pos, and returns the position where the match was found or -1 if 
+nothing can be found. 
+e.g: */
+str = 'Widget with id';
+
+alert( str.indexOf('Widget') ); // 0, because 'Widget' is found at the beginning
+alert( str.indexOf('widget') ); // -1, not found, the search is case-sensitive
+alert( str.indexOf("id") ); // 1, "id" is found at the position 1 (..idget with id)
+
+/* The optional second parameter allows us to start searching from a given position.
+For instance, the first occurrence of "id" is at position 1. 
+To look for the next occurrence, let’s start the search from position 2:
+*/
+str = 'Widget with id';
+
+alert( str.indexOf('id', 2) ); // 12
