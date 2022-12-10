d445770c9f0d15bedf2fcaf433dc05d7a18796a8
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-09 20:04:22 -0800

    Update arrays.js

--- JavaScript/5. Data Types/5.4 Arrays/arrays.js
+++ JavaScript/5. Data Types/5.4 Arrays/arrays.js
@@ -270,3 +270,54 @@ for (let i = 0; i < arr.length; i++) {
 for (let fruit of fruits) {
   alert( fruit );
 }
+
+/* Note: for..of doesn't give access to the number of the current element, just
+its value, but in most cases that's enough and more concise. */
+
+/* Technically, because arrays are objects, it is also possible to use for..in: */
+for (let key in arr) {
+  alert( arr[key] ); // Apple, Orange, Pear
+}
+
+/* But that’s actually a bad idea. There are potential problems with it:
+  1. The loop for..in iterates over all properties, not only the numeric ones.
+    There are so-called “array-like” objects in the browser and in other environments, 
+    that look like arrays. That is, they have length and indexes properties, 
+    but they may also have other non-numeric properties and methods, which we 
+    usually don’t need. 
+    
+    The for..in loop will list them though. So if we need to work with 
+    array-like objects, then these “extra” properties can become a problem.
+
+  2. The for..in loop is optimized for generic objects, not arrays, and thus 
+  is 10-100 times slower. Of course, it’s still very fast. The speedup may only 
+  matter in bottlenecks. But still we should be aware of the difference.
+
+Generally, we shouldn’t use for..in for arrays. */
+
+/* A word about "length" */
+/* The length property automatically updates when we modify the array. 
+
+To be precise, it is actually not the count of values in the array, 
+  but the greatest numeric index plus one. 
+  
+For instance, a single element with a large index gives a big length:
+*/
+fruits = [];
+fruits[123] = "Apple";  // Not usually how arrays are used
+alert( fruits.length ); // 124
+
+/* length property is writable. */
+/* If we increase it manually, nothing interesting happens. 
+But if we decrease it, the array is truncated. The process is irreversible, 
+e.g: */
+arr = [1, 2, 3, 4, 5];
+
+arr.length = 2; // truncate to 2 elements
+alert( arr ); // [1, 2]
+
+arr.length = 5; // return length back
+alert( arr[3] ); // undefined: the values do not return
+
+/* So, the simplest way to clear the array is: arr.length = 0; */
+arr.length = 0; // Simplest way to clear the array
