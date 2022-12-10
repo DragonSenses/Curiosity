9a97778209f21c01ee83d2983931740979fbdf7c
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-09 19:33:54 -0800

    Update arrays.js

--- JavaScript/5. Data Types/5.4 Arrays/arrays.js
+++ JavaScript/5. Data Types/5.4 Arrays/arrays.js
@@ -172,3 +172,60 @@ fruits.unshift("Pineapple", "Lemon"); // unshift 2 elements
 
 // ["Pineapple", "Lemon", "Apple", "Orange", "Peach"]
 alert( fruits );
+
+
+/* Internals - An array is a special kind of object. 
+  The square brackets used to access a property arr[0] actually come from the 
+object syntax. That’s essentially the same as obj[key], where arr is the 
+object, while numbers are used as keys.
+
+They extend objects providing special methods to work with ordered collections
+of data and also the length property. But at the core it’s still an object.
+
+Remember, there are only eight basic data types in JavaScript. 
+  Array is an object and thus behaves like an object.
+    For instance, it is copied by reference:
+*/
+fruits = ["Banana"];
+
+let arr = fruits; // copy by reference (two variables reference the same array)
+
+alert( arr === fruits ); // true
+
+arr.push("Pear"); // modify the array by reference
+
+alert( fruits ); // Banana, Pear - 2 items now
+
+/* What makes arrays really special is their internal representation. 
+  The engine tries to store its elements in the contiguous memory area, 
+  one after another; and there are other optimizations as well, 
+  to make arrays work really fast.
+
+But they all break if we quit working with an array as with an “ordered collection” 
+and start working with it as if it were a regular object.
+
+  For instance, technically we can do this:
+*/
+fruits = []; // make an array
+
+fruits[99999] = 5; // assign a property with the index far greater than its length
+
+fruits.age = 25; // create a property with an arbitrary name
+
+/* That’s possible, because arrays are objects at their base. We can add any 
+properties to them.
+
+But the engine will see that we’re working with the array as with a regular object. 
+Array-specific optimizations are not suited for such cases and will be turned off, 
+their benefits disappear. 
+
+The ways to misuse an array:
+  * Add a non-numeric property like arr.test = 5.
+  * Make holes, like: add arr[0] and then arr[1000] (and nothing between them).
+  * Fill the array in the reverse order, like arr[1000], arr[999] and so on.
+  
+Please think of arrays as special structures to work with the ordered data. 
+They provide special methods for that. Arrays are carefully tuned inside 
+JavaScript engines to work with contiguous ordered data, please use them this way. 
+And if you need arbitrary keys, chances are high that you actually require a regular object {}.
+*/
