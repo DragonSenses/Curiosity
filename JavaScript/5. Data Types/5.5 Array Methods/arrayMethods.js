f73c9b5b73e1453786ff8034a7696b1028d4a386
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-10 20:55:35 -0800

    Update arrayMethods.js

--- JavaScript/5. Data Types/5.5 Array Methods/arrayMethods.js
+++ JavaScript/5. Data Types/5.5 Array Methods/arrayMethods.js
@@ -239,3 +239,50 @@ For instance, this shows each element of the array:
     alert(`${item} is at index ${index} in ${array}`);
 });
 
+/* Searching in Array */
+/* indexOf/lastIndexOf and includes
+The methods arr.indexOf and arr.includes have the similar syntax and do essentially 
+the same as their string counterparts, but operate on items instead of characters: 
+
+  - arr.indexOf(item, from) – looks for item starting from index from, and returns 
+                              the index where it was found, otherwise -1.
+  - arr.includes(item, from) – looks for item starting from index from, returns true if found.
+
+Usually these methods are used with only one argument: the item to search. 
+By default, the search is from the beginning.
+
+Please note that indexOf uses the strict equality === for comparison. 
+    So, if we look for false, it finds exactly false and not the zero.
+
+If we want to check if item exists in the array, and don’t need the exact index, 
+then arr.includes is preferred.
+
+For instance:
+*/
+arr = [1, 0, false];
+
+console.log( arr.indexOf(0) );      // 1
+console.log( arr.indexOf(false) );  // 2
+console.log( arr.indexOf(null) );   // -1
+
+console.log( arr.includes(1) );     // true
+
+/* The method arr.lastIndexOf is the same as indexOf, but looks for from right to left. */
+let fruits = ['Apple', 'Orange', 'Apple'];
+
+alert( fruits.indexOf('Apple') );       // 0 (first Apple)
+alert( fruits.lastIndexOf('Apple') );   // 2 (last Apple)
+
+/* The includes method handles NaN correctly
+A minor, but noteworthy feature of includes is that it correctly handles NaN, 
+unlike indexOf. That’s because includes was added to JavaScript much later 
+and uses the more up to date comparison algorithm internally. */
+const nanArray = [NaN];
+console.log( nanArray.indexOf(NaN) );     // -1 (wrong, should be 0)
+console.log( nanArray.includes(NaN) );    // true (correct)
+
+/* find, findIndex, findLastIndex */
+/* arr.find(fn) helps find objects with a specific condition:
+Syntax:
+    
+*/
