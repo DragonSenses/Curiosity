d3d99510ad5ed933c1ff363c6fb519962c8bc364
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-13 18:22:53 -0800

    add 2nd test

--- JavaScript/3. Code Quality/3.5 Automated Testing/test.js
+++ JavaScript/3. Code Quality/3.5 Automated Testing/test.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-undef */
 
-describe("pow", function () {
+describe("pow", function() {
 
-    it("raises to n-th power", function () {
-        assert.equal(pow(2, 3), 8);
+    it("2 raised to power 3 is 8", function() {
+      assert.equal(pow(2, 3), 8);
     });
-
+  
+    it("3 raised to power 4 is 81", function() {
+      assert.equal(pow(3, 4), 81);
+    });
+  
 });
