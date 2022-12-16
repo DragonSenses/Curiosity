8e8bcfeea4de3eb773dfaa24304cae75f4113cdb
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-15 20:16:20 -0800

    Update iterables.js

--- JavaScript/5. Data Types/5.6 Iterables/iterables.js
+++ JavaScript/5. Data Types/5.6 Iterables/iterables.js
@@ -22,3 +22,28 @@ Array.from(obj[, mapFn, thisArg]) makes a real Array from an iterable or array-l
  and we can then use array methods on it. The optional arguments mapFn and thisArg 
  allow us to apply a function to each item.
 */
+
+/* Iterable objects are a generalization of arrays. 
+That’s a concept that allows us to make any object useable in a for..of loop. 
+
+Of course, Arrays are iterable. But there are many other built-in objects, 
+that are iterable as well. For instance, strings are also iterable.
+
+If an object isn’t technically an array, but represents a collection (list, set) 
+of something, then for..of is a great syntax to loop over it, 
+so let’s see how to make it work.
+*/
+
+/* Symbol.iterator */
+/* Grasp the concept of iterables by making one of our own. 
+For instance, we have an object that is not an array, but looks suitable for 
+"for..of" 
+
+A "range" object that represents an interval of numbers: */
+let range = {
+    from: 1,
+    to: 5
+};
+
+// We want the for..of to work:
+// for(let num of range) ... num=1,2,3,4,5
