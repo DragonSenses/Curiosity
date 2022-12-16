656fdb8265dc224c0112733d804aa8c28d9f5609
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-15 20:19:02 -0800

    Update iterables.js

--- JavaScript/5. Data Types/5.6 Iterables/iterables.js
+++ JavaScript/5. Data Types/5.6 Iterables/iterables.js
@@ -47,3 +47,42 @@ let range = {
 
 // We want the for..of to work:
 // for(let num of range) ... num=1,2,3,4,5
+
+/* To make the range object iterable (and thus let for..of work) we need to add
+a method to the object named Symbol.iterator (a special built-in symbol just for that).
+
+1. When for..of starts, it calls that method once (or errors if not found). 
+   The method must return an iterator – an object with the method next.
+2. Onward, for..of works only with that returned object.
+3. When for..of wants the next value, it calls next() on that object.
+4. The result of next() must have the form {done: Boolean, value: any}, 
+   where done=true means that the loop is finished, otherwise value is the next value.
+*/
+
+/* Here’s the full implementation for range with remarks: */
+
+// 1. call to for..of initially calls this
+range[Symbol.iterator] = function() {
+
+    // ...it returns the iterator object:
+    // 2. Onward, for..of works only with the iterator object below, asking it for next values
+    return {
+      current: this.from,
+      last: this.to,
+  
+      // 3. next() is called on each iteration by the for..of loop
+      next() {
+        // 4. it should return the value as an object {done:.., value :...}
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  };
+  
+  // now it works!
+  for (let num of range) {
+    alert(num); // 1, then 2, 3, 4, 5
+  }
