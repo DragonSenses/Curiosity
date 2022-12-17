2dafe9e733d99da76cce10a91a03d566fccca6f3
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-16 21:05:24 -0800

    Update iterables.js

--- JavaScript/5. Data Types/5.6 Iterables/iterables.js
+++ JavaScript/5. Data Types/5.6 Iterables/iterables.js
@@ -307,3 +307,13 @@ alert(chars);
 /* …But it is shorter.
 
 We can even build surrogate-aware slice on it: */
+function slice(str, start, end) {
+  return Array.from(str).slice(start, end).join('');
+}
+
+str = '𝒳😂𩷶';
+
+alert( slice(str, 1, 3) ); // 😂𩷶
+
+// the native method does not support surrogate pairs
+alert( str.slice(1, 3) ); // garbage (two pieces from different surrogate pairs)
