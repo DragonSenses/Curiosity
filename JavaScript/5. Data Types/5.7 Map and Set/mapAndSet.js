beae18f793a38117461aeb6a735e493bef5b4c5d
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-16 21:06:28 -0800

    Create mapAndSet.js

---
+++ JavaScript/5. Data Types/5.7 Map and Set/mapAndSet.js
@@ -0,0 +1,6 @@
+/* Map and Set */
+/* Till now, we’ve learned about the following complex data structures:
+    - Objects are used for storing keyed collections.
+    - Arrays are used for storing ordered collections.
+But that’s not enough for real life. That’s why Map and Set also exist. 
+*/
