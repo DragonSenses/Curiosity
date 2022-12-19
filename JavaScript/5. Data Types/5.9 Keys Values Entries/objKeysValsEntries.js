4118b564cee5f217e825b3540e0b84c1af7e74b7
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 20:06:14 -0800

    Update objKeysValsEntries.js

--- JavaScript/5. Data Types/5.9 Keys Values Entries/objKeysValsEntries.js
+++ JavaScript/5. Data Types/5.9 Keys Values Entries/objKeysValsEntries.js
@@ -50,3 +50,46 @@ Object.entries(user) = [ ["name","Luna"], ["age",20] ]
 console.log(Object.keys(user));
 console.log(Object.values(user));
 console.table(Object.entries(user));
+
+/* Here’s an example of using Object.values to loop over property values: */
+// loop over values
+for (let value of Object.values(user)) {
+    console.log(value); // Luna, then 20
+}
+
+/* Object.keys/values/entries ignore symbolic properties
+Just like a for..in loop, these methods ignore properties that use Symbol(...) as keys.
+
+Usually that’s convenient. But if we want symbolic keys too, then there’s a 
+separate method Object.getOwnPropertySymbols that returns an array of only 
+symbolic keys. 
+
+Also, there exist a method Reflect.ownKeys(obj) that returns all keys. */
+
+
+/* Transforming Objects */
+/* Objects lack many methods that exist for arrays, e.g. map, filter and others.
+
+If we’d like to apply them, then we can use Object.entries followed by Object.fromEntries:
+
+ 1. Use Object.entries(obj) to get an array of key/value pairs from obj.
+ 2. Use array methods on that array, e.g. map, to transform these key/value pairs.
+ 3. Use Object.fromEntries(array) on the resulting array to turn it back into an object.
+
+For example, we have an object with prices, and would like to double them: */
+let prices = {
+    banana: 1,
+    orange: 2,
+    meat: 4,
+};
+
+let doublePrices = Object.fromEntries(
+    // convert prices to array, map each key/value pair into another pair
+    // and then fromEntries gives back the object
+    Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
+);
+
+alert(doublePrices.meat); // 8
+
+/* It may look difficult at first sight, but becomes easy to understand after 
+you use it once or twice. We can make powerful chains of transforms this way. */
