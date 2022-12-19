bc47c45538ddd0155ecadc1381e9c4c9370e9f0c
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 19:18:39 -0800

    Create objKeysValsEntries.js

---
+++ JavaScript/5. Data Types/5.9 Keys Values Entries/objKeysValsEntries.js
@@ -0,0 +1,21 @@
+/* Object.keys, values, entries */
+/* Previously, we saw methods: map.keys(), map.values(), map.entries(). 
+These methods are generic, there is a common agreement to use them for data structures. 
+If we ever create a data structure of our own, we should implement them too.
+
+They are supported for:
+* Map
+* Set
+* Array
+
+Plain objects also support similar methods, but the syntax is a bit different.
+
+Object.keys, values, entries
+For plain objects, the following methods are available:
+
+* Object.keys(obj) – returns an array of keys.
+* Object.values(obj) – returns an array of values.
+* Object.entries(obj) – returns an array of [key, value] pairs.
+
+Please note the distinctions (compared to map for example):
+ */
