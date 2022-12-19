894aab83937748ef98d8791280c0120d44149ea9
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 19:31:41 -0800

    Update objKeysValsEntries.js

--- JavaScript/5. Data Types/5.9 Keys Values Entries/objKeysValsEntries.js
+++ JavaScript/5. Data Types/5.9 Keys Values Entries/objKeysValsEntries.js
@@ -18,4 +18,35 @@ For plain objects, the following methods are available:
 * Object.entries(obj) – returns an array of [key, value] pairs.
 
 Please note the distinctions (compared to map for example):
+
+                Map                 Object
+Call Syntax     maps.keys()         Object.keys(obj)        , but not obj.keys()
+Returns         iterable            "real" Array
+
+The first difference is that we have to call Object.keys(obj), and not obj.keys().
+
+Why so? The main reason is flexibility. Remember, objects are a base of all 
+complex structures in JavaScript. So we may have an object of our own like data 
+that implements its own data.values() method. And we still can call Object.values(data) on it.
+
+The second difference is that Object.* methods return “real” array objects, 
+not just an iterable. That’s mainly for historical reasons.
+
+For instance:
  */
+let user = {
+    name: "Luna",
+    age: 20
+};
+
+console.log(user);
+
+/* 
+Object.keys(user) = ["name", "age"]
+Object.values(user) = ["Luna", 20]
+Object.entries(user) = [ ["name","Luna"], ["age",20] ]
+*/
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.table(Object.entries(user));
