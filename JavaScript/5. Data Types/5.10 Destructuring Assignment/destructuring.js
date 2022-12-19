c3a948d386f833d7fe2e1ec91c61dc53122558b5
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 20:40:47 -0800

    Update destructuring.js

--- JavaScript/5. Data Types/5.10 Destructuring Assignment/destructuring.js
+++ JavaScript/5. Data Types/5.10 Destructuring Assignment/destructuring.js
@@ -19,3 +19,31 @@ The full array syntax:
 - It’s possible to extract data from nested arrays/objects, for that the left side 
 must have the same structure as the right one.
 */
+
+/* The two most used data structures in JavaScript are Object and Array.
+
+    - Objects allow us to create a single entity that stores data items by key.
+    - Arrays allow us to gather data items into an ordered list.
+
+Although, when we pass those to a function, it may need not be an object/array 
+as a whole. It may need individual pieces.
+
+Destructuring assignment is a special syntax that allows us to “unpack” arrays 
+or objects into a bunch of variables, as sometimes that’s more convenient.
+
+Destructuring also works great with complex functions that have a lot of 
+parameters, default values, and so on.
+*/
+
+/* Array Destructuring */
+/* Here’s an example of how an array is destructured into variables: */
+// we have an array with the name and surname
+let arr = ["Luna", "Berry"];
+
+// destructuring assignment
+// sets firstName = arr[0]
+// and surname = arr[1]
+let [firstName, surname] = arr;
+
+console.log(firstName); // Luna
+console.log(surname);   // Berry
