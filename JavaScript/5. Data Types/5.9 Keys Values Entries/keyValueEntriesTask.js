c2a380eb3f85df24097f361221bbcdff7aacb463
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 20:35:15 -0800

    Update keyValueEntriesTask.js

--- JavaScript/5. Data Types/5.9 Keys Values Entries/keyValueEntriesTask.js
+++ JavaScript/5. Data Types/5.9 Keys Values Entries/keyValueEntriesTask.js
@@ -1,7 +1,8 @@
 /* Sum the Properties */
 /* There is a salaries object with arbitrary number of salaries.
 
-Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+Write the function sumSalaries(salaries) that returns the sum of all 
+salaries using Object.values and the for..of loop.
 
 If salaries is empty, then the result must be 0.
 
@@ -11,13 +12,28 @@ let salaries = {
     "Pete": 300,
     "Mary": 250
 };
-  
-alert( sumSalaries(salaries) ); // 650
+
+console.log( sumSalaries(salaries) ); // 650
 
 /* Answer */
 function sumSalaries(){
-
+    let sum = 0;
+    
+    for(let salary of Object.values(salaries)){
+        sum += salary;
+    }
+    
+    return sum;
 }
+
+// Alternatively, get sum using Object.values and reduce
+// reduce loops over array of salaries, adding them up, and returns result
+// Take a salaries object, map it to its values, reduce to sum
+function sumSalariesConcise(salaries){
+    return Object.values(salaries).reduce((a,b) => a + b, 0);
+}
+
+console.log(sumSalariesConcise(salaries)); // 650
 
 /* Count Properties */
 /* Write a function count(obj) that returns the number of properties in the object: */
@@ -26,10 +42,10 @@ let user = {
     age: 30
 };
   
-alert( count(user) ); // 2
+console.log( count(user) ); // 2
 
 
 /* Answer */
-function count(){
-
+function count(obj){
+    return Object.keys(obj).length;
 }
