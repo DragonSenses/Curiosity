1840c6f147534f6474e9fc40b5691fe113651bfe
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-18 20:26:53 -0800

    Create keyValueEntriesTask.js

---
+++ JavaScript/5. Data Types/5.9 Keys Values Entries/keyValueEntriesTask.js
@@ -0,0 +1,35 @@
+/* Sum the Properties */
+/* There is a salaries object with arbitrary number of salaries.
+
+Write the function sumSalaries(salaries) that returns the sum of all salaries using Object.values and the for..of loop.
+
+If salaries is empty, then the result must be 0.
+
+For instance: */
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+  
+alert( sumSalaries(salaries) ); // 650
+
+/* Answer */
+function sumSalaries(){
+
+}
+
+/* Count Properties */
+/* Write a function count(obj) that returns the number of properties in the object: */
+let user = {
+    name: 'John',
+    age: 30
+};
+  
+alert( count(user) ); // 2
+
+
+/* Answer */
+function count(){
+
+}
