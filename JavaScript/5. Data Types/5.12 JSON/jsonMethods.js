079ae5e538959cc046809ebf13b81a2c6f91626b
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-20 20:52:55 -0800

    Create jsonMethods.js

---
+++ JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
@@ -0,0 +1,15 @@
+/* JSON methods, toJSON */
+/* Summary
+ - JSON is a data format that has its own independent standard and libraries 
+ for most programming languages.
+
+ - JSON supports plain objects, arrays, strings, numbers, booleans, and null.
+
+ - JavaScript provides methods JSON.stringify to serialize into JSON and 
+ JSON.parse to read from JSON.
+
+ - Both methods support transformer functions for smart reading/writing.
+
+ - If an object has toJSON, then it is called by JSON.stringify. */
+
+ 
