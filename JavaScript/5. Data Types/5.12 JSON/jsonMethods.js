4bf2dcaede80cd8630fbdfa1f124741ccd475a87
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-21 21:10:44 -0800

    Update jsonMethods.js

--- JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
+++ JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
@@ -372,9 +372,9 @@ meetup = {
     room
 };
 
-alert( JSON.stringify(room) ); // 23
+console.log( JSON.stringify(room) ); // 23
 
-alert( JSON.stringify(meetup) );
+console.log( JSON.stringify(meetup) );
 /*
 {
     "title":"Conference",
@@ -384,3 +384,52 @@ alert( JSON.stringify(meetup) );
 
 /* As we can see, toJSON is used both for the direct call JSON.stringify(room) 
 and when room is nested in another encoded object. */
+
+
+/* Json.parse */
+/* To decode a JSON-string, we need another method named JSON.parse. 
+The syntax:
+    let value = JSON.parse(str, [reviver]);
+str
+    * JSON-string to parse.
+reviver
+    * Optional function(key,value) that will be called for each (key, value) 
+    pair and can transform the value.
+
+For instance: */
+// stringified array
+let numbers = "[0, 1, 2, 3]";
+
+numbers = JSON.parse(numbers);
+
+console.log( numbers[1] ); // 1
+
+/* Or for nested objects: */
+let userData = '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+
+user = JSON.parse(userData);
+
+console.log( user.friends[1] ); // 1
+
+/* The JSON may be as complex as necessary, objects and arrays can include 
+other objects and arrays. But they must obey the same JSON format.
+
+Here are typical mistakes in hand-written JSON (sometimes we have to write 
+    it for debugging purposes): */
+let jsonMistakes = `{
+    name: "John",                     // mistake: property name without quotes
+    "surname": 'Smith',               // mistake: single quotes in value (must be double)
+    'isAdmin': false                  // mistake: single quotes in key (must be double)
+    "birthday": new Date(2000, 2, 3), // mistake: no "new" is allowed, only bare values
+    "friends": [0,1,2,3]              // here all fine
+}`;
+
+console.log(jsonMistakes);
+
+/* Besides, JSON does not support comments. Adding a comment to JSON makes it invalid.
+
+There’s another format named JSON5, which allows unquoted keys, comments etc. 
+But this is a standalone library, not in the specification of the language.
+
+The regular JSON is that strict not because its developers are lazy, but to 
+allow easy, reliable and very fast implementations of the parsing algorithm. */
