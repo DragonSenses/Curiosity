5ce1731251eb083734a2135b3b167e5a1d4dee37
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-21 17:02:21 -0800

    Update jsonMethods.js

--- JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
+++ JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
@@ -80,22 +80,44 @@ console.log(json);
 
 
 /* The method JSON.stringify(student) takes the object and converts it into a string.
-
-The resulting json string is called a JSON-encoded or serialized or stringified or marshalled object. We are ready to send it over the wire or put into a plain data store.
+    The resulting json string is called a JSON-encoded or serialized or stringified 
+or marshalled object. We are ready to send it over the wire or put into a plain data store.
 
 Please note that a JSON-encoded object has several important differences from the object literal:
 
-Strings use double quotes. No single quotes or backticks in JSON. So 'John' becomes "John".
-Object property names are double-quoted also. That’s obligatory. So age:30 becomes "age":30.
+Strings use double quotes. 
+ - No single quotes or backticks in JSON. 
+ - So 'Leo' becomes "Leo".
+
+Object property names are double-quoted also. That’s obligatory. 
+ - So age:20 becomes "age":20.
+
 JSON.stringify can be applied to primitives as well.
 
 JSON supports following data types:
+ - Objects { ... }
+ - Arrays [ ... ]
+ - Primitives:
+ - strings,
+ - numbers,
+ - boolean values true/false,
+ - null.
 
-Objects { ... }
-Arrays [ ... ]
-Primitives:
-strings,
-numbers,
-boolean values true/false,
-null.
 For instance: */
+// a number in JSON is just a number
+console.log( JSON.stringify(1) ) ;// 1
+
+// a string in JSON is still a string, but double-quoted
+console.log( JSON.stringify('test') ); // "test"
+
+console.log( JSON.stringify(true) ); // true
+
+console.log( JSON.stringify([1, 2, 3]) ); // [1,2,3]
+
+/* JSON is data-only language-independent specification, so some 
+JavaScript-specific object properties are skipped by JSON.stringify. 
+    Namely:
+ - Function properties (methods).
+ - Symbolic keys and values.
+ - Properties that store undefined.
+*/
