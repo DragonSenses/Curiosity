31da201bfb5e248adfc77147395895832429a476
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-21 21:13:00 -0800

    Update jsonMethods.js

--- JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
+++ JavaScript/5. Data Types/5.12 JSON/jsonMethods.js
@@ -433,3 +433,48 @@ But this is a standalone library, not in the specification of the language.
 
 The regular JSON is that strict not because its developers are lazy, but to 
 allow easy, reliable and very fast implementations of the parsing algorithm. */
+
+/* Using reviver */
+/* Imagine, we got a stringified meetup object from the server.
+
+It looks like this: */
+// title: (meetup title), date: (meetup date)
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+/* …And now we need to deserialize it, to turn back into JavaScript object.
+
+Let’s do it by calling JSON.parse: */
+meetup = JSON.parse(str);
+
+alert( meetup.date.getDate() ); // Error!
+
+/* Whoops! An error!
+The value of meetup.date is a string, not a Date object.
+How could JSON.parse know that it should transform that string into a Date? 
+
+Let’s pass to JSON.parse the reviving function as the second argument, 
+that returns all values “as is”, but date will become a Date:
+*/
+str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+meetup = JSON.parse(str, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+console.log( meetup.date.getDate() ); // now works!
+
+/* By the way, that works for nested objects as well: */
+let schedule = `{
+    "meetups": [
+      {"title":"Conference","date":"2017-11-30T12:00:00.000Z"},
+      {"title":"Birthday","date":"2017-04-18T12:00:00.000Z"}
+    ]
+}`;
+
+schedule = JSON.parse(schedule, function(key, value) {
+    if (key == 'date') return new Date(value);
+    return value;
+});
+
+alert( schedule.meetups[1].date.getDate() ); // works!
