650e12e656e177ea873e3225f593919242da3ee1
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-23 21:49:10 -0800

    Update arrayMethodsTasks.js

--- JavaScript/5. Data Types/5.5 Array Methods/arrayMethodsTasks.js
+++ JavaScript/5. Data Types/5.5 Array Methods/arrayMethodsTasks.js
@@ -73,24 +73,33 @@ console.log( filtered ); // 3,1 (matching values)
 
 console.log( arr ); // 5,3,8,1 (not modified)
 
+
 /* Filter range "in place" */
 /* Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 
 The function should only modify the array. It should not return anything.
 
 For instance: */
-arr = [5, 3, 8, 1];
-
-filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
-
-alert( arr ); // [3, 1]
-
-
 function filterRangeInPlace(arr, a, b) {
-    //TODO
+    for (let i = 0; i < arr.length; i++){
+        let val = arr[i];
+
+        // remove if outside of the interval
+        if (val < a || val > b){
+            arr.splice(i,1);
+            i--;    // Remember to reduce the index after splice (removal)
+        }
+    }
     return arr + a + b;
 }
 
+arr = [5, 3, 8, 1];
+
+filtered = filterRangeInPlace(arr, 1, 4);
+
+console.log( filtered ); // 3,1 (matching values)
+
+console.log( arr ); // 5,3,8,1 (not modified)
 
 /* Sort in decreasing order */
 function sortInDecreasingOrder(){
