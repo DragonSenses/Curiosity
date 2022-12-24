856016db11d8e62eb0806a8a16cc2a5611824a46
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-23 22:42:50 -0800

    Update arrayMethodsTasks.js

--- JavaScript/5. Data Types/5.5 Array Methods/arrayMethodsTasks.js
+++ JavaScript/5. Data Types/5.5 Array Methods/arrayMethodsTasks.js
@@ -309,3 +309,62 @@ let arr = [ john, pete, mary ];
 
 console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
 }
+
+
+/* Filter unique array members */
+/* Let arr be an array.
+
+Create a function unique(arr) that should return an array with unique items of arr.
+
+For instance:
+*/
+
+function unique(arr) {
+    return arr;    /* your code */
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+"Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+  
+console.log( unique(strings) ); // Hare, Krishna, :-O
+
+
+/* Create keyed object from array */
+/* Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+Create a function groupById(arr) that creates an object from it, with id as the key, 
+and array items as values.
+
+Such function is really handy when working with server data.
+
+In this task we assume that id is unique. There may be no two array items with the same id.
+
+Please use array .reduce method in the solution.
+
+For example: */
+
+function groupById(arr){
+    return arr;
+}
+
+{
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+
+console.log(usersById);
+/*
+// after the call we should have:
+
+usersById = {
+john: {id: 'john', name: "John Smith", age: 20},
+ann: {id: 'ann', name: "Ann Smith", age: 24},
+pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+}
