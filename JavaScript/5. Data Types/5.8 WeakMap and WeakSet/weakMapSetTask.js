e32ace001047277e12648a32b19ab822296f0f20
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-26 15:02:23 -0800

    Update weakMapSetTask.js

--- JavaScript/5. Data Types/5.8 WeakMap and WeakSet/weakMapSetTask.js
+++ JavaScript/5. Data Types/5.8 WeakMap and WeakSet/weakMapSetTask.js
@@ -21,6 +21,54 @@ structure as well.
 P.P.S. We shouldn’t modify message objects, add our properties to them. 
 As they are managed by someone else’s code, that may lead to bad consequences.
 */
+let messages = [
+  {text: "Hello", from: "John"},
+  {text: "How goes?", from: "John"},
+  {text: "See you soon", from: "Alice"}
+];
+
+/* Let store read messages in WeakSet */
+let readMessages = new WeakSet();
+
+// two messages have been read
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+// readMessages (WeakSet) has 2 elements
+
+// ...let's read the first message again!
+readMessages.add(messages[0]);
+// readMessages still has 2 unique elements
+
+// answer: was the message[0] read?
+console.log("Read message 0: " + readMessages.has(messages[0])); // true
+
+messages.shift();
+// now readMessages has 1 element (technically memory may be cleaned later)
+
+/* The WeakSet allows to store a set of messages and easily check for the 
+existence of a message in it.
+
+It cleans up itself automatically. The tradeoff is that we can’t iterate over it, 
+can’t get “all read messages” from it directly. 
+
+But we can do it by iterating over all messages and filtering those that are in the set. */
+
+/* Another, different solution could be to add a property like 
+  message.isRead=true to a message after it’s read. 
+  
+As messages objects are managed by another code, that’s generally discouraged, 
+but we can use a symbolic property to avoid conflicts.
+
+Like this: */
+// the symbolic property is only known to our code
+let isRead = Symbol("isRead");
+messages[0][isRead] = true;
+
+/* Now third-party code probably won’t see our extra property.
+
+Although symbols allow to lower the probability of problems, 
+using WeakSet is better from the architectural point of view. 
+*/
 
 
 
