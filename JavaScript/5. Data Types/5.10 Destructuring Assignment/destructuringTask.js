aa360036a6fc01cd9eb12db5b3c24f3e1687e975
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-26 18:02:19 -0800

    Update destructuringTask.js

--- JavaScript/5. Data Types/5.10 Destructuring Assignment/destructuringTask.js
+++ JavaScript/5. Data Types/5.10 Destructuring Assignment/destructuringTask.js
@@ -23,8 +23,13 @@ console.log( name ); // John
 console.log( age ); // 30
 console.log( isAdmin ); // false
 */
+let user = { name: "Luna", years: 20 };
 
+let {name, years: age, isAdmin = false} = user;
 
+console.log( name );    // Luna
+console.log( age );     // 20
+console.log( isAdmin ); // false
 
 
 /* The maximal salary */
@@ -43,3 +48,33 @@ Create the function topSalary(salaries) that returns the name of the top-paid pe
 P.S. Use Object.entries and destructuring to iterate over key/value pairs.
 
 */
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+/**
+ * Tests whether an object is empty or not.
+ * @param {object} obj object to test
+ * @returns true if object is an empty object as well as
+ * source: https://stackoverflow.com/questions/679915/how-do-i-test-for-an-empty-javascript-object  
+ */
+function isEmpty(obj){
+  // because Object.keys(new Date()).length === 0;
+  // we have to do some additional check
+  return obj // 👈 null and undefined check
+  && Object.keys(obj).length === 0
+  && Object.getPrototypeOf(obj) === Object.prototype;
+
+  // Note, though, that this creates an unnecessary array (the return value of keys).
+}
+
+function topSalary(salaries){
+  if(isEmpty(salaries)){
+    return null;
+  }
+
+}
+
+topSalary();
