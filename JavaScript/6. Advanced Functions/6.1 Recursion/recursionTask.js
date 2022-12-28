d07d3137f8aa87bb986c347b3cd978d6f131dbca
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-27 20:11:41 -0800

    Create recursionTask.js

---
+++ JavaScript/6. Advanced Functions/6.1 Recursion/recursionTask.js
@@ -0,0 +1,31 @@
+/* Sum all numbers till the given one */
+/* Write a function sumTo(n) that calculates the sum of numbers 1 + 2 + ... + n.
+
+For instance: 
+
+sumTo(1) = 1
+sumTo(2) = 2 + 1 = 3
+sumTo(3) = 3 + 2 + 1 = 6
+sumTo(4) = 4 + 3 + 2 + 1 = 10
+...
+sumTo(100) = 100 + 99 + ... + 2 + 1 = 5050
+
+Make 3 solution variants:
+ 1. Using a for loop.
+ 2. Using a recursion, cause sumTo(n) = n + sumTo(n-1) for n > 1.
+ 3. Using the arithmetic progression formula.
+
+P.S. Which solution variant is the fastest? The slowest? Why?
+
+P.P.S. Can we use recursion to count sumTo(100000)?
+*/
+
+/**
+ * Calculates the sum of numbers 1 + 2 + ... + n
+ * @param {number} n how many numbers to sum to
+ */
+function sumTo(n) { 
+    
+}
+
+alert( sumTo(100) ); // 5050
