bf6de773438a571c681595e32f33a5adfa0babcf
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-28 18:48:58 -0800

    Update variableScope.js

--- JavaScript/6. Advanced Functions/6.3 Variable Scope/variableScope.js
+++ JavaScript/6. Advanced Functions/6.3 Variable Scope/variableScope.js
@@ -49,3 +49,67 @@ with variables that only belong to it: */
     let message = "Goodbye";
     console.log(message);
 }
+
+/* Note: There’d be an error without blocks
+If we use let with the existing variable name and without blocks:
+Error: variable already declared. */
+
+/* For if, for, while and so on, variables declared in {...} are also 
+only visible inside: */
+// eslint-disable-next-line no-constant-condition
+if (true) {
+    let phrase = "Hello!";
+  
+    console.log(phrase); // Hello!
+}
+  
+// console.log(phrase); // Error, no such variable!
+
+/* Here, after if finishes, the console.log below won’t see the phrase, hence the error.
+
+That’s great, as it allows us to create block-local variables, specific to an if branch. 
+
+The similar thing holds true for for and while loops.
+*/
+for (let i = 0; i < 3; i++) {
+    // the variable i is only visible inside this for
+    console.log(i); // 0, then 1, then 2
+}
+  
+// console.log(i); // Error, no such variable
+
+/* Visually, let i is outside of {...}. But the for construct is special here: 
+    the variable, declared inside it, is considered a part of the block.
+*/
+
+
+/* Nested Functions */
+/* A function is called “nested” when it is created inside another function.
+
+It is easily possible to do this with JavaScript.
+
+We can use it to organize our code, like this: */
+function sayHiBye(firstName, lastName) {
+
+    // helper nested function to use below
+    function getFullName() {
+      return firstName + " " + lastName;
+    }
+  
+    console.log( "Hello, " + getFullName() );
+    console.log( "Bye, " + getFullName() );
+  
+}
+
+sayHiBye("Luna", "Berry");
+
+/* Here the nested function getFullName() is made for convenience. 
+It can access the outer variables and so can return the full name. 
+
+Nested functions are quite common in JavaScript.
+
+What’s much more interesting, a nested function can be returned: either as 
+    a property of a new object or as a result by itself. 
+    
+It can then be used somewhere else. No matter where, it still has access 
+to the same outer variables. */
