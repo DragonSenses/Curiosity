9e05a77a9fcb7bffeed81b4863f737c48fff9450
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-29 15:00:18 -0800

    Update var.js

--- JavaScript/6. Advanced Functions/6.4 var/var.js
+++ JavaScript/6. Advanced Functions/6.4 var/var.js
@@ -190,3 +190,73 @@ An IIFE looks like this: */
     console.log(message); // Hello
   
 })();
+
+/* Here, a Function Expression is created and immediately called. 
+So the code executes right away and has its own private variables. 
+
+The Function Expression is wrapped with parenthesis (function {...}), because 
+when JavaScript engine encounters "function" in the main code, it understands 
+it as the start of a Function Declaration. But a Function Declaration must 
+have a name, so this kind of code will give an error: 
+
+// Tries to declare and immediately call a function
+function() { // <-- SyntaxError: Function statements require a function name
+
+    var message = "Hello";
+  
+    console.log(message); // Hello
+  
+}();
+
+
+Even if we say: “okay, let’s add a name”, that won’t work, as JavaScript does 
+not allow Function Declarations to be called immediately:
+
+// syntax error because of parentheses below
+function go() {
+
+}(); // <-- can't call Function Declaration immediately
+
+So, the parentheses around the function is a trick to show JavaScript that the 
+function is created in the context of another expression, and hence it’s a 
+Function Expression: it needs no name and can be called immediately.
+
+There exist other ways besides parentheses to tell JavaScript that we mean a 
+    Function Expression:
+*/
+// Ways to create IIFE
+(function() {
+    console.log("Parentheses around the function");
+})();
+  
+(function() {
+    console.log("Parentheses around the whole thing");
+}());
+  
+!function() {
+    console.log("Bitwise NOT operator starts the expression");
+}();
+  
++function() {
+    console.log("Unary plus starts the expression");
+}();
+
+/* In all the above cases we declare a Function Expression and run it immediately. 
+
+Let’s note again: nowadays there’s no reason to write such code. */
+
+/* Recap on IIFE 
+
+It is a design pattern which is also known as a Self-Executing Anonymous Function 
+and contains two major parts:
+ 1. The first is the anonymous function with lexical scope enclosed within the 
+ Grouping Operator (). This prevents accessing variables within the IIFE idiom 
+ as well as polluting the global scope.
+ 2. The second part creates the immediately invoked function expression () 
+ through which the JavaScript engine will directly interpret the function.
+
+Use Cases
+    - Avoid Polluting the global namespace
+
+Source: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+*/
