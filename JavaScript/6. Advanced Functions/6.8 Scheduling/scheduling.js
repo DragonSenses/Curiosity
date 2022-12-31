9c64db66d36d4f281a6056f38109abc828c051fb
DragonSenses <kendrickfrancisco@gmail.com>
2022-12-30 17:00:27 -0800

    Update scheduling.js

--- JavaScript/6. Advanced Functions/6.8 Scheduling/scheduling.js
+++ JavaScript/6. Advanced Functions/6.8 Scheduling/scheduling.js
@@ -28,4 +28,68 @@ All that may increase the minimal timer resolution (the minimal delay) to
 */
 
 /* Scheduling a Call - to decide to execute a function not right now, but at
-a certain time later. */
+a certain time later. 
+
+ -setTimeout() allows us to run a function once after the interval of time.
+ -setInterval() allows us to run a function repeatedly, starting after the 
+ interval of time, then repeating continuously at that interval. 
+ 
+These methods are not a part of JavaScript specification. But most environments 
+have the internal scheduler and provide these methods. In particular, they are 
+supported in all browsers and Node.js.
+*/
+
+
+/* setTimeout() */
+/* Syntax
+        let timerId = setTimeout(func|code, [delay], [arg1], [arg2], ...) 
+        
+Parameters:
+    func|code
+        Function or a string of code to execute. Usually, that’s a function. 
+        For historical reasons, a string of code can be passed, but that’s 
+        not recommended.
+     
+    delay
+        The delay before run, in milliseconds (1000 ms = 1 second), by default 0.
+
+    arg1, arg2…
+        Arguments for the function
+*/
+
+// For instance, this code calls sayHi() after one second:
+function sayHi() {
+    console.log('Hello');
+}
+
+setTimeout(sayHi, 1000);    // 'Hello' after 1 second
+
+// With arguments:
+function sayHi2(phrase, who) {
+    alert( phrase + ', ' + who );
+}
+  
+setTimeout(sayHi2, 1000, "Hello", "Luna"); // Hello, Luna
+
+/* If the first argument is a string, then JavaScript creates a function from it.
+So, this will also work: */
+
+setTimeout("console.log('Hello')", 1000);
+
+/* BUT using strings is not recommended, use arrow functions instead of them, 
+like this: */
+setTimeout(() => console.log('Hello'), 1000);
+
+
+/* Pass a function, but don't run it 
+Novice developers sometimes make a mistake by adding brackets () after the function:
+
+// wrong!
+setTimeout(sayHi(), 1000);
+
+That doesn’t work, because setTimeout expects a reference to a function. And here 
+sayHi() runs the function, and the result of its execution is passed to setTimeout.
+ 
+In our case the result of sayHi() is undefined (the function returns nothing), 
+so nothing is scheduled.
+*/
