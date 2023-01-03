e79b3e85465e1bf0d6eb2b16078b1afcf8bc0f9d
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-02 17:35:17 -0800

    Update decorators.js

--- JavaScript/6.9 Decorators/decorators.js
+++ JavaScript/6.9 Decorators/decorators.js
@@ -111,3 +111,86 @@ instead of altering the code of slow itself:
   - We can combine multiple decorators if needed (other decorators will follow).
 */
 
+/* Using "func.call" for the context */
+/* The caching decorator mentioned above is not suited to work with object methods.
+
+For instance, in the code below worker.slow() stops working after the decoration: */
+// we'll make worker.slow caching
+let worker = {
+  someMethod() {
+    return 1;
+  },
+
+  slow(x) {
+    // scary CPU-heavy task here
+    console.log("Called with " + x);
+    return x * this.someMethod(); // (*)
+  }
+};
+
+/* same code as before 
+function cachingDecorator(func) {
+  let cache = new Map();
+  return function(x) {
+    if (cache.has(x)) {
+      return cache.get(x);
+    }
+    let result = func(x); // (**)
+    cache.set(x, result);
+    return result;
+  };
+}
+*/
+
+
+console.log( worker.slow(1) ); // the original method works
+
+worker.slow = cachingDecorator(worker.slow); // now make it caching
+
+console.log( worker.slow(2) ); // Whoops! Error: Cannot read property 'someMethod' of undefined
+
+/* The error occurs in the line (*) that tries to access this.someMethod and fails. 
+
+The reason is that the wrapper calls the original function as func(x) in the 
+  line (**). And, when called like that, the function gets this = undefined.
+
+We would observe a similar symptom if we tried to run: */
+let func = worker.slow;
+func(2);
+
+/* So, the wrapper passes the call to the original method, but without the 
+context this. Hence the error. 
+
+Let’s fix it. */
+
+/* func.call(context, ...args) */
+/* There’s a special built-in function method func.call(context, …args) that 
+allows to call a function explicitly setting this.
+
+The syntax is:
+      func.call(context, arg1, arg2, ...)
+     
+It runs func providing the first argument as this, and the next as the arguments.
+
+To put it simply, these two calls do almost the same:
+
+func(1, 2, 3);
+func.call(obj, 1, 2, 3);
+
+They both call func with arguments 1, 2 and 3. The only difference is that 
+  func.call also sets this to obj.
+*/
+
+/* As an example, in the code below we call sayHi in the context of different 
+objects: sayHi.call(user) runs sayHi providing this=user, 
+and the next line sets this=admin: */
+function sayHi() {
+  console.log(this.name);
+}
+
+let user = { name: "Luna" };
+let admin = { name: "Admin" };
+
+// use call to pass different objects as "this"
+sayHi.call( user );   // Luna
+sayHi.call( admin );  // Admin
