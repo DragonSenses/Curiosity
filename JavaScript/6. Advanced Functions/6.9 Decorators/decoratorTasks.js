f171ead84c5f57828c954448efd670594920b8b0
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-03 15:10:22 -0800

    Update decoratorTasks.js

--- JavaScript/6. Advanced Functions/6.9 Decorators/decoratorTasks.js
+++ JavaScript/6. Advanced Functions/6.9 Decorators/decoratorTasks.js
@@ -23,10 +23,21 @@ for (let args of work.calls) {
 /* P.S. That decorator is sometimes useful for unit-testing. 
 Its advanced form is sinon.spy in Sinon.JS library. */
 
-function spy() {
+/**
+ * Decorator with a wrapper, returned by spy(f), that stores all arguments and
+ * then use f.apply to forward the call.
+ * @param {function} func the function to decorate
+ * @returns a wrapper that saves all calls to function in its calls property
+ */
+function spy(func) {
 
-  return function () {
+  function wrapper(...args) {
+    // using ...args instead of arguments to store "real" array in wrapper.calls
+    wrapper.calls.push(args);
+    return func.apply(this, args); 
+  }
 
-  };
+  wrapper.calls = [];
 
+  return wrapper;
 }
