9e811b682474530450a09e516296341233564aed
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-03 18:40:08 -0800

    Update decoratorTasks.js

--- JavaScript/6. Advanced Functions/6.9 Decorators/decoratorTasks.js
+++ JavaScript/6. Advanced Functions/6.9 Decorators/decoratorTasks.js
@@ -257,3 +257,59 @@ f1000(3); // (throttling, 1000ms not out yet)
 /* P.S. Arguments and the context this passed to f1000 should be passed to 
 the original f. */
 
+/**
+ * A decorator that returns a wrapper that when it’s called multiple times, 
+ * it passes the call to f at maximum once per ms milliseconds.
+ * 
+ * Runs it not more often than given ms time. Good for regular updates 
+ * that shouldn’t be very often.
+ * 
+ * @param {function} func function to wrap
+ * @param {number} ms milliseconds 
+ * @returns 
+ */
+function throttle(func, ms){
+
+  let isThrottled = false, // Flag for Cooldown State
+    savedArgs,  // save Arguments in memory
+    savedThis;  // save Context in memory
+
+  function wrapper() {
+
+    if (isThrottled) { // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+    isThrottled = true;
+
+    func.apply(this, arguments); // (1)
+
+    setTimeout(function() {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+
+}
+
+/* A call to throttle(func, ms) returns wrapper. 
+  1. During the first call, the wrapper just runs func and sets the cooldown 
+  state (isThrottled = true).
+  
+  2. In this state all calls are memorized in savedArgs/savedThis. Please note 
+  that both the context and the arguments are equally important and should be 
+  memorized. We need them simultaneously to reproduce the call.
+
+  3. After ms milliseconds pass, setTimeout triggers. The cooldown state is 
+  removed (isThrottled = false) and, if we had ignored calls, wrapper is 
+  executed with the last memorized arguments and context.
+ 
+The 3rd step runs not func, but wrapper, because we not only need to execute 
+func, but once again enter the cooldown state and setup the timeout to reset it.
+*/
