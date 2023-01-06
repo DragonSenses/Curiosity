a5e40ce70b145e38a26a890c1cafbeac7faf50b2
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-05 22:34:52 -0800

    Create propertyFlags.js

---
+++ JavaScript/7. Object Properties Configuration/7.1 Property Flags/propertyFlags.js
@@ -0,0 +1,7 @@
+/* Property flags and descriptors */
+/* As we know, objects can store properties.
+
+Until now, a property was a simple “key-value” pair to us. But an object 
+property is actually a more flexible and powerful thing.
+
+Introducing additional configuration options. */
