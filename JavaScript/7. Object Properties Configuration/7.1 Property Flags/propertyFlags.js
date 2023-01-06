ef134a3abd102a151babcada19a77546cf82c248
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-05 23:15:46 -0800

    Update propertyFlags.js

--- JavaScript/7. Object Properties Configuration/7.1 Property Flags/propertyFlags.js
+++ JavaScript/7. Object Properties Configuration/7.1 Property Flags/propertyFlags.js
@@ -55,4 +55,36 @@ alert( JSON.stringify(descriptor, null, 2 ) );
 The syntax is: 
   Object.defineProperty(obj, propertyName, descriptor)
   
+  - obj, propertyName: The object and its property to apply the descriptor.
+  - descriptor: Property descriptor object to apply.
+
+If the property exists, defineProperty updates its flags. Otherwise, it creates 
+the property with the given value and flags; in that case, if a flag is not 
+supplied, it is assumed false.
+
+For instance, here a property name is created with all falsy flags:
+
+let user = {};
 */
+user = {};
+
+Object.defineProperty(user, "name", {
+  value: "John"
+});
+
+descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+alert( JSON.stringify(descriptor, null, 2 ) );
+/*
+{
+  "value": "John",
+  "writable": false,
+  "enumerable": false,
+  "configurable": false
+}
+ */
+
+/* Compare it with “normally created” user.name above: now all flags are falsy. 
+If that’s not what we want then we’d better set them to true in descriptor.
+
+Now let’s see effects of the flags by example. */
