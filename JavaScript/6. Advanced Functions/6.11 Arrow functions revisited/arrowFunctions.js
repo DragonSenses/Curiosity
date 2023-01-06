5d58f3fe5cb7737e31c9605a38723e0256a4a65d
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-05 20:44:33 -0800

    Update arrowFunctions.js

--- JavaScript/6. Advanced Functions/6.11 Arrow functions revisited/arrowFunctions.js
+++ JavaScript/6. Advanced Functions/6.11 Arrow functions revisited/arrowFunctions.js
@@ -47,3 +47,48 @@ It’s in the very spirit of JavaScript to create a function and pass it somewhe
 
 And in such functions we usually don’t want to leave the current context. 
 That’s where arrow functions come in handy. */
+
+
+/* Arrow Functions have no "this" */
+/* As we remember from the chapter Object methods, "this", arrow functions do 
+not have this. If this is accessed, it is taken from the outside.
+
+For instance, we can use it to iterate inside an object method: */
+let group = {
+  title: "Our Group",
+  students: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.students.forEach(
+      student => alert(this.title + ': ' + student)
+    );
+  }
+};
+
+group.showList();
+
+/* Here in forEach, the arrow function is used, so this.title in it is exactly 
+the same as in the outer method showList. That is: group.title.
+
+If we used a “regular” function, there would be an error: 
+
+let group = {
+  title: "Our Group",
+  students: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.students.forEach(function(student) {
+      // Error: Cannot read property 'title' of undefined
+      alert(this.title + ': ' + student);
+    });
+  }
+};
+
+group.showList();
+
+
+The error occurs because forEach runs functions with this=undefined by default, 
+so the attempt to access undefined.title is made.
+
+That doesn’t affect arrow functions, because they just don’t have this.
+*/
