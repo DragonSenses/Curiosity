eac259b9061f5abc092d29af8edb2ea5a7d282cf
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-05 22:35:30 -0800

    Update propertyFlags.js

--- JavaScript/7. Object Properties Configuration/7.1 Property Flags/propertyFlags.js
+++ JavaScript/7. Object Properties Configuration/7.1 Property Flags/propertyFlags.js
@@ -5,3 +5,22 @@ Until now, a property was a simple “key-value” pair to us. But an object
 property is actually a more flexible and powerful thing.
 
 Introducing additional configuration options. */
+
+/* Property Flags */
+/* Object properties, besides a value, have three special attributes 
+(so-called “flags”):
+
+  - writable – if true, the value can be changed, otherwise it’s read-only.
+  - enumerable – if true, then listed in loops, otherwise not listed.
+  - configurable – if true, the property can be deleted and these attributes 
+  can be modified, otherwise not.
+
+We didn’t see them yet, because generally they do not show up. When we create a 
+property “the usual way”, all of them are true. But we also can change them anytime.
+
+First, let’s see how to get those flags.
+
+The method Object.getOwnPropertyDescriptor allows to query the full information 
+about a property.
+
+The syntax is: */
