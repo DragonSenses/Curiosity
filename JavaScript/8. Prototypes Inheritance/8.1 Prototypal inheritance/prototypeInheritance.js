28bd36e0d9602186a877154bec0ebc77b5b5ca6f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-06 17:15:28 -0800

    Update prototypeInheritance.js

--- JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
+++ JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
@@ -87,3 +87,103 @@ Here we can say that "animal is the prototype of rabbit" or
 
 So if animal has a lot of useful properties and methods, then they become 
 automatically available in rabbit. Such properties are called “inherited”. */
+
+/* If we have a method in animal, it can be called on rabbit: */
+{
+let animal = {
+  eats: true,
+  walk() {
+    alert("Animal walk");
+  }
+};
+
+let rabbit = {
+  jumps: true,
+  __proto__: animal
+};
+
+// walk is taken from the prototype
+rabbit.walk(); // Animal walk
+}
+/* The method is automatically taken from the prototype, like this: 
+
+animal
+  [eats: true    ] 
+  [walk: function]
+       /\
+        | [[Prototype]]
+rabbit  |        
+  [jumps: true]
+  
+*/
+
+/* The prototype chain can be longer: */
+{
+let animal = {
+  eats: true,
+  walk() {
+    alert("Animal walk");
+  }
+};
+
+let rabbit = {
+  jumps: true,
+  __proto__: animal
+};
+
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit
+};
+
+// walk is taken from the prototype chain
+longEar.walk(); // Animal walk
+alert(longEar.jumps); // true (from rabbit)  
+}
+/* animal
+  [eats: true    ] 
+  [walk: function]
+       /\
+        | [[Prototype]]
+rabbit  |        
+  [jumps: true] 
+       /\
+        | [[Prototype]]
+        |  
+longEar
+  [earLength: 10]
+
+Now if we read something from longEar, and it’s missing, JavaScript will look 
+for it in rabbit, and then in animal.
+*/
+
+/* There are only two limitations: 
+  1. The references can’t go in circles. JavaScript will throw an error 
+  if we try to assign __proto__ in a circle.
+  
+  2. The value of __proto__ can be either an object or null. Other types are ignored.
+
+Also it may be obvious, but still: there can be only one [[Prototype]]. 
+An object may not inherit from two others.
+*/
+
+/* __proto__ is a historical getter/setter for [[Prototype]] */
+/* It’s a common mistake of novice developers not to know the difference 
+between these two.
+
+Please note that __proto__ is not the same as the internal [[Prototype]] property.
+
+It’s a getter/setter for [[Prototype]]. Later we’ll see situations where it 
+matters, for now let’s just keep it in mind.
+
+The __proto__ property is a bit outdated. It exists for historical reasons, 
+modern JavaScript suggests that we should use 
+  Object.getPrototypeOf/Object.setPrototypeOf functions 
+  instead that get/set the prototype. We’ll also cover these functions later.
+
+By the specification, __proto__ must only be supported by browsers. 
+  In fact though, all environments including server-side support __proto__, 
+  so we’re quite safe using it.
+
+As the __proto__ notation is a bit more intuitively obvious, we use it in the examples. 
+*/
