62ed642cd68a5fb5be3237dcf8f63e8991e2e738
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-06 17:26:21 -0800

    Update prototypeInheritance.js

--- JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
+++ JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
@@ -21,6 +21,9 @@
 
   - The for..in loop iterates over both its own and its inherited properties. 
   All other key/value-getting methods only operate on the object itself.
+
+  - No matter where the method is found: in an object or its prototype. In a 
+  method call, this is always the object before the dot.
 */
 
 /* Prototypical Inheritance */
@@ -66,12 +69,12 @@ automatically take it from animal. For instance: */
 rabbit.__proto__ = animal; // (*)
 
 // we can find both properties in rabbit now:
-alert( rabbit.eats ); // true (**)
-alert( rabbit.jumps ); // true
+console.log( rabbit.eats ); // true (**)
+console.log( rabbit.jumps ); // true
 
 /* Here the line (*) sets animal to be the prototype of rabbit.
 
-Then, when alert tries to read property rabbit.eats (**), it’s not in rabbit, 
+Then, when console.log tries to read property rabbit.eats (**), it’s not in rabbit, 
 so JavaScript follows the [[Prototype]] reference and finds it in animal 
 (look from the bottom up): 
 
@@ -93,7 +96,7 @@ automatically available in rabbit. Such properties are called “inherited”. *
 let animal = {
   eats: true,
   walk() {
-    alert("Animal walk");
+    console.log("Animal walk");
   }
 };
 
@@ -122,7 +125,7 @@ rabbit  |
 let animal = {
   eats: true,
   walk() {
-    alert("Animal walk");
+    console.log("Animal walk");
   }
 };
 
@@ -138,7 +141,7 @@ let longEar = {
 
 // walk is taken from the prototype chain
 longEar.walk(); // Animal walk
-alert(longEar.jumps); // true (from rabbit)  
+console.log(longEar.jumps); // true (from rabbit)  
 }
 /* animal
   [eats: true    ] 
@@ -187,3 +190,142 @@ By the specification, __proto__ must only be supported by browsers.
 
 As the __proto__ notation is a bit more intuitively obvious, we use it in the examples. 
 */
+
+/* Writing doesn't use prototype */
+/* The prototype is only used for reading properties.
+
+Write/delete operations work directly with the object.
+
+In the example below, we assign its own walk method to rabbit: */
+{
+let animal = {
+  eats: true,
+  walk() {
+    /* this method won't be used by rabbit */
+  }
+};
+
+let rabbit = {
+  __proto__: animal
+};
+
+rabbit.walk = function() {
+  console.log("Rabbit! Bounce-bounce!");
+};
+
+rabbit.walk(); // Rabbit! Bounce-bounce!
+}
+/* From now on, rabbit.walk() call finds the method immediately in the object 
+and executes it, without using the prototype: 
+
+animal
+  [eats: true    ] 
+  [walk: function]
+       /\
+        | [[Prototype]]
+rabbit  |        
+  [walk: function]
+  
+*/
+
+/* Accessor properties are an exception, as assignment is handled by a setter 
+function. So writing to such a property is actually the same as calling a function.
+
+For that reason admin.fullName works correctly in the code below: */
+{
+let user = {
+  name: "Luna",
+  surname: "Berry",
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  }
+};
+
+let admin = {
+  __proto__: user,
+  isAdmin: true
+};
+
+console.log(admin.fullName); // Luna Berry (*)
+
+// setter triggers!
+admin.fullName = "Alice Cooper"; // (**)
+
+console.log(admin.fullName); // Alice Cooper, state of admin modified
+console.log(user.fullName); // Luna Berry, state of user protected
+}
+
+/* Here in the line (*) the property admin.fullName has a getter in the 
+prototype user, so it is called. And in the line (**) the property has a setter
+in the prototype, so it is called. */
+
+
+/* The value of “this” */
+/* An interesting question may arise in the example above: what’s the value 
+of this inside set fullName(value)? Where are the properties this.name and 
+this.surname written: into user or admin? 
+
+The answer is simple: this is not affected by prototypes at all.
+
+No matter where the method is found: in an object or its prototype. 
+In a method call, this is always the object before the dot. 
+
+So, the setter call admin.fullName= uses admin as this, not user.
+
+This is vital, because we may have a big object with many methods, and have 
+objects that inherit from it. And when the inheriting objects run the 
+inherited methods, they will modify only their own states, not the state of 
+the big object. */
+
+/* Example: here animal represents a “method storage”, and rabbit makes use of it.
+
+The call rabbit.sleep() sets this.isSleeping on the rabbit object: */
+{
+// animal has methods
+let animal = {
+  walk() {
+    if (!this.isSleeping) {
+      console.log(`I walk`);
+    }
+  },
+  sleep() {
+    this.isSleeping = true;
+  }
+};
+
+let rabbit = {
+  name: "White Rabbit",
+  __proto__: animal
+};
+
+// modifies rabbit.isSleeping
+rabbit.sleep();
+
+console.log(rabbit.isSleeping); // true
+console.log(animal.isSleeping); // undefined (no such property in the prototype)
+}
+/* The resulting picture: 
+
+animal
+  [walk: function ] 
+  [sleep: function]
+       /\
+        | [[Prototype]]
+rabbit  |        
+  [name: "White Rabbit"]
+  [isSleeping: true    ]
+  
+If we had other objects, like bird, snake, etc., inheriting from animal, they 
+would also gain access to methods of animal. But this in each method call would
+be the corresponding object, evaluated at the call-time (before dot), not animal. 
+So when we write data into this, it is stored into these objects.
+
+As a result, methods are shared, but the object state is not. */
+
+
+/* for...in loop */
