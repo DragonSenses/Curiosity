ee09df4d6c80b5531eed53370af0a0a2990a6115
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-06 16:45:58 -0800

    Update propertyGettersSetters.js

--- JavaScript/7. Object Properties Configuration/7.2 Property Getters & Setters/propertyGettersSetters.js
+++ JavaScript/7. Object Properties Configuration/7.2 Property Getters & Setters/propertyGettersSetters.js
@@ -97,3 +97,73 @@ That is, an accessor descriptor may have:
    
 For instance, to create an accessor fullName with defineProperty, 
 we can pass a descriptor with get and set: */
+user = {
+  name: "Luna",
+  surname: "Berry"
+};
+
+Object.defineProperty(user, 'fullName', {
+  get() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set(value) {
+    [this.name, this.surname] = value.split(" ");
+  }
+});
+
+console.log(user.fullName); // LLuna Berry
+
+for(let key in user) console.log(key); // name, surname
+
+/* Please note that a property can be either an accessor (has get/set methods) 
+or a data property (has a value), not both.
+
+If we try to supply both get and value in the same descriptor, 
+there will be an error: */
+
+// Error: Invalid property descriptor.
+Object.defineProperty({}, 'prop', {
+  get() {
+    return 1;
+  },
+
+  value: 2
+});
+
+
+/* Smarter getters/setters */
+/* Getters/setters can be used as wrappers over “real” property values to gain 
+more control over operations with them.
+
+For instance, if we want to forbid too short names for user, we can have a 
+setter name and keep the value in a separate property _name: */
+{
+let user = {
+  get name() {
+    return this._name;
+  },
+
+  set name(value) {
+    if (value.length < 4) {
+      console.log("Name is too short, need at least 4 characters");
+      return;
+    }
+    this._name = value;
+  }
+};
+
+user.name = "Luna";
+console.log(user.name); // Luna
+
+user.name = ""; // Name is too short...  
+}
+
+/* So, the name is stored in _name property, and the access is done 
+via getter and setter.
+
+Technically, external code is able to access the name directly by using 
+user._name. But there is a widely known convention that properties starting 
+with an underscore "_" are internal and should not be touched from outside 
+the object. */
+
