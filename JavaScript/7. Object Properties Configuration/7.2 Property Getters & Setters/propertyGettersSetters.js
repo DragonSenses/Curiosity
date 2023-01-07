ca8f06a9a9b63dcb3ed59641ffa5cef0831fcdb5
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-06 16:42:32 -0800

    Update propertyGettersSetters.js

--- JavaScript/7. Object Properties Configuration/7.2 Property Getters & Setters/propertyGettersSetters.js
+++ JavaScript/7. Object Properties Configuration/7.2 Property Getters & Setters/propertyGettersSetters.js
@@ -48,3 +48,52 @@ let user = {
 };
 
 console.log(user.fullName); // Luna Berry
+
+/* From the outside, an accessor property looks like a regular one. 
+That’s the idea of accessor properties. We don’t call user.fullName as a 
+function, we read it normally: the getter runs behind the scenes.
+
+As of now, fullName has only a getter. If we attempt to assign user.fullName=, 
+there will be an error: 
+
+user.fullName = "Test"; // Error (property has only a getter)
+
+*/
+
+/* Let’s fix it by adding a setter for user.fullName: */
+user = {
+  name: "Luna",
+  surname: "Berry",
+  
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  }
+};
+
+// set fullName is executed with the given value.
+user.fullName = "Alice Cooper";
+
+console.log(user.name); // Alice
+console.log(user.surname); // Cooper
+
+/* As the result, we have a “virtual” property fullName. It is readable and writable. */
+
+
+/* Accessor descriptors */
+/* Descriptors for accessor properties are different from those for data properties.
+
+For accessor properties, there is no value or writable, but instead there are get and set functions.
+
+That is, an accessor descriptor may have: 
+
+  * get – a function without arguments, that works when a property is read,
+  * set – a function with one argument, that is called when the property is set,
+  * enumerable – same as for data properties,
+  * configurable – same as for data properties.
+   
+For instance, to create an accessor fullName with defineProperty, 
+we can pass a descriptor with get and set: */
