9271525f9e4f8a6fea663e092668f7e78f5933df
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-07 15:53:25 -0800

    Update prototypeInheritance.js

--- JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
+++ JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
@@ -402,3 +402,25 @@ rabbit  |
   [jumps: true]
 
 */
+
+/* Note: Where is the method rabbit.hasOwnProperty coming from? 
+
+We did not define it. Looking at the chain we can see that the method is 
+provided by Object.prototype.hasOwnProperty. In other words, it’s inherited.
+
+…But why does hasOwnProperty not appear in the for..in loop like eats and jumps 
+do, if for..in lists inherited properties?
+
+The answer is simple: it’s not enumerable. Just like all other properties of 
+Object.prototype, it has enumerable:false flag. And for..in only lists 
+enumerable properties. That’s why it and the rest of the Object.prototype 
+properties are not listed. */
+
+/* Almost all other key/value-getting methods ignore inherited properties 
+
+Almost all other key/value-getting methods, such as Object.keys, 
+Object.values and so on ignore inherited properties.
+
+They only operate on the object itself. Properties from the prototype are not 
+taken into account.
+*/
