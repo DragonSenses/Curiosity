08f62058aa47feb1a40daced804291e4d6eaf5af
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-06 17:09:49 -0800

    Update prototypeInheritance.js

--- JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
+++ JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritance.js
@@ -31,3 +31,59 @@ For instance, we have a user object with its properties and methods, and want
 to make admin and guest as slightly modified variants of it. We’d like to reuse
 what we have in user, not copy/reimplement its methods, just build a new object
 on top of it. */
+
+
+/* [[Prototype]] */
+/* In JavaScript, objects have a special hidden property [[Prototype]] (as named 
+in the specification), that is either null or references another object. 
+That object is called “a prototype”: 
+
+prototype object
+  [prototype object] 
+       /\
+        | [[Prototype]]
+object  |        
+    [object]
+
+When we read a property from object, and it’s missing, JavaScript automatically 
+takes it from the prototype. In programming, this is called “prototypal inheritance”. 
+
+The property [[Prototype]] is internal and hidden, but there are many ways to set it.
+
+One of them is to use the special name __proto__, like this:
+*/
+let animal = {
+  eats: true
+};
+let rabbit = {
+  jumps: true
+};
+
+rabbit.__proto__ = animal; // sets rabbit.[[Prototype]] = animal
+
+/* Now if we read a property from rabbit, and it’s missing, JavaScript will 
+automatically take it from animal. For instance: */
+rabbit.__proto__ = animal; // (*)
+
+// we can find both properties in rabbit now:
+alert( rabbit.eats ); // true (**)
+alert( rabbit.jumps ); // true
+
+/* Here the line (*) sets animal to be the prototype of rabbit.
+
+Then, when alert tries to read property rabbit.eats (**), it’s not in rabbit, 
+so JavaScript follows the [[Prototype]] reference and finds it in animal 
+(look from the bottom up): 
+
+animal
+  [eats: true] 
+       /\
+        | [[Prototype]]
+rabbit  |        
+  [jumps: true]
+      
+Here we can say that "animal is the prototype of rabbit" or 
+  "rabbit prototypically inherits from animal".
+
+So if animal has a lot of useful properties and methods, then they become 
+automatically available in rabbit. Such properties are called “inherited”. */
