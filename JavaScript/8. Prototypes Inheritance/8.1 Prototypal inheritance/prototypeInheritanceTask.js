5dea2aa9bbe8d24c48460a6c43c8b7c65337e193
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-07 17:31:19 -0800

    Update prototypeInheritanceTask.js

--- JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritanceTask.js
+++ JavaScript/8. Prototypes Inheritance/8.1 Prototypal inheritance/prototypeInheritanceTask.js
@@ -123,4 +123,113 @@ The method rabbit.eat is first found in the prototype, then executed with this=r
 */
 
 
-/*  */
+/* Why are both hamsters full? */
+/* We have two hamsters: speedy and lazy inheriting from the general hamster object.
+
+When we feed one of them, the other one is also full. Why? How can we fix it? */
+{
+  let hamster = {
+    stomach: [],
+  
+    eat(food) {
+      this.stomach.push(food);
+    }
+  };
+  
+  let speedy = {
+    __proto__: hamster
+  };
+  
+  let lazy = {
+    __proto__: hamster
+  };
+  
+  // This one found the food
+  speedy.eat("apple");
+  console.log( speedy.stomach ); // apple
+  
+  // This one also has it, why? fix please.
+  console.log( lazy.stomach ); // apple
+}
+
+/* Answer: 
+Let’s look carefully at what’s going on in the call speedy.eat("apple").
+
+  1. The method speedy.eat is found in the prototype (=hamster), then 
+  executed with this=speedy (the object before the dot).
+
+  2. Then this.stomach.push() needs to find stomach property and call push on 
+  it. It looks for stomach in this (=speedy), but nothing found.
+
+  3. Then it follows the prototype chain and finds stomach in hamster.
+
+  4. Then it calls push on it, adding the food into the stomach of the prototype
+
+So all hamsters share a single stomach!
+
+Both for lazy.stomach.push(...) and speedy.stomach.push(), the property stomach
+is found in the prototype (as it’s not in the object itself), then the new 
+data is pushed into it.
+
+Please note that such thing doesn’t happen in case of a simple assignment 
+this.stomach=:
+*/
+{
+  let hamster = {
+    stomach: [],
+  
+    eat(food) {
+      // assign to this.stomach instead of this.stomach.push
+      this.stomach = [food];
+    }
+  };
+  
+  let speedy = {
+     __proto__: hamster
+  };
+  
+  let lazy = {
+    __proto__: hamster
+  };
+  
+  // Speedy one found the food
+  speedy.eat("apple");
+  console.log( speedy.stomach ); // apple
+  
+  // Lazy one's stomach is empty
+  console.log( lazy.stomach ); // <nothing>
+}
+/* Now all works fine, because this.stomach= does not perform a lookup of stomach. 
+The value is written directly into this object.
+
+Also we can totally avoid the problem by making sure that each hamster has 
+their own stomach: */
+{
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  }
+};
+
+let speedy = {
+  __proto__: hamster,
+  stomach: []
+};
+
+let lazy = {
+  __proto__: hamster,
+  stomach: []
+};
+
+// Speedy one found the food
+speedy.eat("apple");
+console.log( speedy.stomach ); // apple
+
+// Lazy one's stomach is empty
+console.log( lazy.stomach ); // <nothing>
+}
+/* As a common solution, all properties that describe the state of a 
+particular object, like stomach above, should be written into that object. 
+That prevents such problems. */
