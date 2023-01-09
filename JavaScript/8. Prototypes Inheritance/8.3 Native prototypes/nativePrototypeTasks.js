7a7c3f67684def1060dde55fe0cf94a9a31c3aed
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-08 18:25:32 -0800

    Update nativePrototypeTasks.js

--- JavaScript/8. Prototypes Inheritance/8.3 Native prototypes/nativePrototypeTasks.js
+++ JavaScript/8. Prototypes Inheritance/8.3 Native prototypes/nativePrototypeTasks.js
@@ -2,14 +2,26 @@
 /* Add to the prototype of all functions the method defer(ms), that runs 
 the function after ms milliseconds.
 
-After you do it, such code should work: */
+After you do it, such code should work: 
+
 function f() {
-  alert("Hello!");
+  console.log("Hello!");
 }
 
 f.defer(1000); // shows "Hello!" after 1 second
 
+*/
 
+/* Answer */
+Function.prototype.defer = function(ms) {
+  setTimeout(this, ms);
+};
+
+function f() {
+  console.log("Hello!");
+}
+
+f.defer(1000); // shows "Hello!" after 1 sec
 
 
 
@@ -18,11 +30,47 @@ f.defer(1000); // shows "Hello!" after 1 second
 /* Add to the prototype of all functions the method defer(ms), that returns a 
 wrapper, delaying the call by ms milliseconds.
 
-Here’s an example of how it should work: */
-function f(a, b) {
-  alert( a + b );
+Here’s an example of how it should work: 
+
+function sum(a, b) {
+  console.log( a + b );
 }
 
-f.defer(1000)(1, 2); // shows 3 after 1 second
+sum.defer(1000)(1, 2); // shows 3 after 1 second
+
+*/
 
 /* Please note that the arguments should be passed to the original function. */
+Function.prototype.defer = function(ms) {
+  let f = this;
+  return function(...args) {
+    setTimeout(() => f.apply(this, args), ms);
+  };
+};
+
+// check it
+function sum(a, b) {
+  console.log( a + b );
+}
+
+sum.defer(1000)(1, 2); // shows 3 after 1 second
+
+
+
+/* Polyfill */
+if (!String.prototype.repeat) { // if there's no such method
+  // add it to the prototype
+
+  String.prototype.repeat = function(n) {
+    // repeat the string n times
+    return new Array(n + 1).join(this);
+  };
+}
+/* new Array(n + 1).join(this) 
+Uses the string "this" as the delimiter between n+1 empty elements,
+repeating "this" string n times. 
+
+If "La" is the string that calls repeat, there will be n+1 empty elements in the
+new Array, while this="La" will be used as the delimiter between them. */
+
+console.log( "La".repeat(3) ); // LaLaLa
