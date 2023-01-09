612306da1da2b7691a6eda922d0296b4ce1acf84
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-08 18:12:53 -0800

    Update nativePrototypes.js

--- JavaScript/8. Prototypes Inheritance/8.3 Native prototypes/nativePrototypes.js
+++ JavaScript/8. Prototypes Inheritance/8.3 Native prototypes/nativePrototypes.js
@@ -153,4 +153,50 @@ console.log(f.__proto__ == Function.prototype); // true
 console.log(f.__proto__.__proto__ == Object.prototype); // true, inherit from objects
 
 
-/*  */
+/* Primitives */
+/* The most intricate thing happens with strings, numbers and booleans.
+
+As we remember, they are not objects. But if we try to access their properties, 
+temporary wrapper objects are created using built-in constructors String, Number 
+and Boolean. They provide the methods and disappear.
+
+These objects are created invisibly to us and most engines optimize them out, 
+but the specification describes it exactly this way. Methods of these objects 
+also reside in prototypes, available as String.prototype, Number.prototype and 
+Boolean.prototype. */
+
+/* Note: Values null and undefined have no object wrappers */
+/* Special values null and undefined stand apart. They have no object wrappers, 
+so methods and properties are not available for them. And there are no 
+corresponding prototypes either. */
+
+
+/* Changing native prototypes */
+/* Native prototypes can be modified. For instance, if we add a method to 
+String.prototype, it becomes available to all strings: */
+String.prototype.show = function() {
+  console.log(this);
+};
+
+"BOOM!".show(); // BOOM!
+/* During the process of development, we may have ideas for new built-in 
+methods we’d like to have, and we may be tempted to add them to native prototypes. 
+But that is generally a bad idea. */
+
+/* Important: Prototypes are global, so it’s easy to get a conflict. 
+
+If two libraries add a method String.prototype.show, then one of them will be 
+overwriting the method of the other.
+
+So, generally, modifying a native prototype is considered a bad idea.*/
+
+/* Pollyfilling */
+/* In modern programming, there is only one case where modifying native 
+prototypes is approved. That’s polyfilling. 
+
+Polyfilling is a term for making a substitute for a method that exists in the 
+JavaScript specification, but is not yet supported by a particular JavaScript engine.
+
+We may then implement it manually and populate the built-in prototype with it.
+
+For instance: */
