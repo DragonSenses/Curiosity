acdfe9ff519041bba980c75b55283afbeaa8437f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-10 21:08:59 -0800

    Update classInheritance.js

--- JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
@@ -212,10 +212,85 @@ If accessed, it’s taken from the outer function. For instance: */
   }
 
   console.log(Rabbit);
-  
+
 /* The super in the arrow function is the same as in stop(), so it works as 
 intended. If we specified a “regular” function here, there would be an error: */
 
   // Unexpected super
   // setTimeout(function() { super.stop() }, 1000);
 }
+
+
+/* Overriding constructor */
+/* With constructors it gets a little bit tricky.
+
+Until now, Rabbit did not have its own constructor.
+
+According to the specification, if a class extends another class and has no 
+constructor, then the following “empty” constructor is generated: */
+{
+  class Rabbit extends Animal {
+    // generated for extending classes without own constructors
+    constructor(...args) {
+      super(...args);
+    }
+  }
+
+  console.log(Rabbit);
+
+}
+/* As we can see, it basically calls the parent constructor passing it all 
+the arguments. That happens if we don’t write a constructor of our own.
+
+Now let’s add a custom constructor to Rabbit. It will specify the earLength 
+in addition to name: 
+{
+  class Animal {
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+    // ...
+  }
+  
+  class Rabbit extends Animal {
+  
+    constructor(name, earLength) {
+      this.speed = 0;
+      this.name = name;
+      this.earLength = earLength;
+    }
+  
+    // ...
+  }
+  
+  // Doesn't work!
+  
+}
+
+*/
+/* Whoops! We’ve got an error. Now we can’t create rabbits. What went wrong?
+
+The short answer is: 
+- Constructors in inheriting classes must call super(...), and (!) do it 
+before using this.
+
+…But why? What’s going on here? Indeed, the requirement seems strange.
+
+Of course, there’s an explanation. Let’s get into details, so you’ll really 
+understand what’s going on.
+
+In JavaScript, there’s a distinction between a constructor function of an 
+inheriting class (so-called “derived constructor”) and other functions. 
+A derived constructor has a special internal property [[ConstructorKind]]:"derived". 
+That’s a special internal label.
+
+That label affects its behavior with new.
+  - When a regular function is executed with new, it creates an empty object 
+  and assigns it to this.
+  - But when a derived constructor runs, it doesn’t do this. It expects the 
+  parent constructor to do this job.
+
+So a derived constructor must call super in order to execute its parent (base) 
+constructor, otherwise the object for this won’t be created. And we’ll get an error.
+*/
