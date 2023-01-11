282e959c8f68be4feaf4150ea3302a51e52fb704
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-10 20:49:51 -0800

    Update classInheritance.js

--- JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
@@ -22,7 +22,7 @@ So we can create new functionality on top of the existing. */
 
 /* The “extends” keyword */
 /* Let’s say we have class Animal: */
-{
+
   class Animal {
     constructor(name) {
       this.speed = 0;
@@ -74,7 +74,7 @@ let rabbit = new Rabbit("White Rabbit");
 
 rabbit.run(5); // White Rabbit runs with speed 5.
 rabbit.hide(); // White Rabbit hides!
-}
+
 
 /* Object of Rabbit class have access both to Rabbit methods, such as 
 rabbit.hide(), and also to Animal methods, such as rabbit.run().
@@ -109,3 +109,113 @@ As we can recall from the chapter Native prototypes, JavaScript itself uses
 prototypal inheritance for built-in objects. E.g. Date.prototype.[[Prototype]] 
 is Object.prototype. That’s why dates have access to generic object methods.
 */
+
+/* Any expression is allowed after extends */
+/* Class syntax allows to specify not just a class, but any expression after 
+extends.
+
+For instance, a function call that generates the parent class: */
+{
+  // eslint-disable-next-line no-inner-declarations
+  function f(phrase) {
+    return class {
+      sayHi() { console.log(phrase); }
+    };
+  }
+  
+  class User extends f("Hello") {}
+  
+  new User().sayHi(); // Hello
+
+/* Here class User inherits from the result of f("Hello").
+
+That may be useful for advanced programming patterns when we use functions 
+to generate classes depending on many conditions and can inherit from them. */
+}
+
+
+/* Overriding a method */
+/* Now let’s move forward and override a method. By default, all methods that
+ are not specified in class Rabbit are taken directly “as is” from class Animal.
+
+But if we specify our own method in Rabbit, such as stop() then it will be 
+used instead: */
+{
+  class Rabbit extends Animal {
+    stop() {
+      // ...now this will be used for rabbit.stop()
+      // instead of stop() from class Animal
+    }
+  }
+
+  console.log(Rabbit);
+}
+/* Usually, however, we don’t want to totally replace a parent method, but 
+rather to build on top of it to tweak or extend its functionality. We do 
+something in our method, but call the parent method before/after it or in 
+the process. */
+
+/* Classes provide "super" keyword for that. 
+  - super.method(...) to call a parent method.
+  - super(...) to call a parent constructor (inside our constructor only).
+
+For instance, let our rabbit autohide when stopped: */
+{
+  class Animal {
+
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+  
+    run(speed) {
+      this.speed = speed;
+      console.log(`${this.name} runs with speed ${this.speed}.`);
+    }
+  
+    stop() {
+      this.speed = 0;
+      console.log(`${this.name} stands still.`);
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    hide() {
+      console.log(`${this.name} hides!`);
+    }
+  
+    stop() {
+      super.stop(); // call parent stop
+      this.hide(); // and then hide
+    }
+  }
+  
+  let rabbit = new Rabbit("White Rabbit");
+  
+  rabbit.run(5); // White Rabbit runs with speed 5.
+  rabbit.stop(); // White Rabbit stands still. White Rabbit hides!
+}
+/* Now Rabbit has the stop method that calls the parent super.stop() in the process. */
+
+
+/* Arrow functions have no super */
+/* As was mentioned in the chapter Arrow functions revisited, arrow functions 
+do not have super.
+
+If accessed, it’s taken from the outer function. For instance: */
+{
+  class Rabbit extends Animal {
+    stop() {
+      setTimeout(() => super.stop(), 1000); // call parent stop after 1sec
+    }
+  }
+
+  console.log(Rabbit);
+  
+/* The super in the arrow function is the same as in stop(), so it works as 
+intended. If we specified a “regular” function here, there would be an error: */
+
+  // Unexpected super
+  // setTimeout(function() { super.stop() }, 1000);
+}
