1cf7abc994ff2d5aa24fb0ed86d99780db4555d0
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-10 20:35:00 -0800

    Update classInheritance.js

--- JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
@@ -16,3 +16,46 @@
   - Arrow functions don’t have their own this or super, so they transparently fit 
   into the surrounding context.
 */
+/* Class inheritance is a way for one class to extend another class.
+
+So we can create new functionality on top of the existing. */
+
+/* The “extends” keyword */
+/* Let’s say we have class Animal: */
+{
+  class Animal {
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+    run(speed) {
+      this.speed = speed;
+      alert(`${this.name} runs with speed ${this.speed}.`);
+    }
+    stop() {
+      this.speed = 0;
+      alert(`${this.name} stands still.`);
+    }
+  }
+  
+  let animal = new Animal("My animal");
+  console.log(animal);
+
+
+/* Here’s how we can represent animal object and Animal class graphically:
+
+Animal  prototype Animal.prototype
+[     ] --------> [constructor: Animal] 
+                  [run: function      ]
+                  [stop:function      ]
+                        /\
+                        | [[Prototype]]
+                        |
+            new Animal  |
+                  [name: "My animal"  ]
+*/
+
+
+
+
+}
