166499677166bfb382a56d274560b6d35256d12f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-10 21:19:41 -0800

    Update classInheritance.js

--- JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
@@ -424,3 +424,122 @@ on, so we’re explaining it here.
 
 If it becomes a problem, one can fix it by using methods or getters/setters 
 instead of fields. */
+
+
+/* Super: internals, [[HomeObject]] */
+/* Advanced Information: It’s about the internal mechanisms behind inheritance 
+and super. */
+/* Let’s get a little deeper under the hood of super. We’ll see some 
+interesting things along the way.
+
+First to say, from all that we’ve learned till now, it’s impossible for super 
+to work at all!
+
+Yeah, indeed, let’s ask ourselves, how it should technically work? When an 
+object method runs, it gets the current object as this. If we call super.method() 
+then, the engine needs to get the method from the prototype of the current object. 
+But how?
+
+The task may seem simple, but it isn’t. The engine knows the current object this, 
+so it could get the parent method as this.__proto__.method. Unfortunately, such 
+a “naive” solution won’t work.
+
+Let’s demonstrate the problem. Without classes, using plain objects for the sake 
+of simplicity.
+
+You may skip this part and go below to the [[HomeObject]] subsection if you don’t 
+want to know the details. That won’t harm. Or read on if you’re interested in 
+understanding things in-depth.
+
+In the example below, rabbit.__proto__ = animal. Now let’s try: in rabbit.eat() 
+we’ll call animal.eat(), using this.__proto__: */
+{
+  let animal = {
+    name: "Animal",
+    eat() {
+      console.log(`${this.name} eats.`);
+    }
+  };
+  
+  let rabbit = {
+    __proto__: animal,
+    name: "Rabbit",
+    eat() {
+      // that's how super.eat() could presumably work
+      this.__proto__.eat.call(this); // (*)
+    }
+  };
+  
+  rabbit.eat(); // Rabbit eats.
+}
+
+/* At the line (*) we take eat from the prototype (animal) and call it in the 
+context of the current object. Please note that .call(this) is important here,
+because a simple this.__proto__.eat() would execute parent eat in the context 
+of the prototype, not the current object.
+
+And in the code above it actually works as intended: we have the correct console.log.
+
+Now let’s add one more object to the chain. We’ll see how things break: */
+{
+  let animal = {
+    name: "Animal",
+    eat() {
+      console.log(`${this.name} eats.`);
+    }
+  };
+  
+  let rabbit = {
+    __proto__: animal,
+    eat() {
+      // ...bounce around rabbit-style and call parent (animal) method
+      this.__proto__.eat.call(this); // (*)
+    }
+  };
+  
+  let longEar = {
+    __proto__: rabbit,
+    eat() {
+      // ...do something with long ears and call parent (rabbit) method
+      this.__proto__.eat.call(this); // (**)
+    }
+  };
+  
+  longEar.eat(); // Error: Maximum call stack size exceeded
+}
+/* The code doesn’t work anymore! We can see the error trying to call longEar.eat().
+
+It may be not that obvious, but if we trace longEar.eat() call, then we 
+can see why. In both lines (*) and (**) the value of this is the current 
+object (longEar). That’s essential: all object methods get the current object 
+as this, not a prototype or something.
+
+So, in both lines (*) and (**) the value of this.__proto__ is exactly the 
+same: rabbit. They both call rabbit.eat without going up the chain in the 
+endless loop. */
+
+/* 1. Inside longEar.eat(), the line (**) calls rabbit.eat providing it with this=longEar. 
+
+// inside longEar.eat() we have this = longEar
+this.__proto__.eat.call(this) // (**)
+// becomes
+longEar.__proto__.eat.call(this)
+// that is
+rabbit.eat.call(this);
+
+2. Then in the line (*) of rabbit.eat, we’d like to pass the call even higher 
+in the chain, but this=longEar, so this.__proto__.eat is again rabbit.eat!
+
+// inside rabbit.eat() we also have this = longEar
+this.__proto__.eat.call(this) // (*)
+// becomes
+longEar.__proto__.eat.call(this)
+// or (again)
+rabbit.eat.call(this);
+
+3. …So rabbit.eat calls itself in the endless loop, because it can’t ascend any further.
+  The problem can’t be solved by using this alone.
+*/
+
+
+/* [[HomeObject]] */
