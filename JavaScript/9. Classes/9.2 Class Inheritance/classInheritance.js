875dad5845a47e8b91182d7342716bbdb2a05b09
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-10 20:45:38 -0800

    Update classInheritance.js

--- JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/classInheritance.js
@@ -30,11 +30,11 @@ So we can create new functionality on top of the existing. */
     }
     run(speed) {
       this.speed = speed;
-      alert(`${this.name} runs with speed ${this.speed}.`);
+      console.log(`${this.name} runs with speed ${this.speed}.`);
     }
     stop() {
       this.speed = 0;
-      alert(`${this.name} stands still.`);
+      console.log(`${this.name} stands still.`);
     }
   }
   
@@ -53,9 +53,59 @@ Animal  prototype Animal.prototype
                         |
             new Animal  |
                   [name: "My animal"  ]
+
+
+…And we would like to create another class Rabbit.
+
+As rabbits are animals, Rabbit class should be based on Animal, have access 
+to animal methods, so that rabbits can do what “generic” animals can do.
+
+The syntax to extend another class is: class Child extends Parent.
+
+Let’s create class Rabbit that inherits from Animal:
 */
-
-
-
-
+class Rabbit extends Animal {
+  hide() {
+    console.log(`${this.name} hides!`);
+  }
 }
+
+let rabbit = new Rabbit("White Rabbit");
+
+rabbit.run(5); // White Rabbit runs with speed 5.
+rabbit.hide(); // White Rabbit hides!
+}
+
+/* Object of Rabbit class have access both to Rabbit methods, such as 
+rabbit.hide(), and also to Animal methods, such as rabbit.run().
+
+Internally, extends keyword works using the good old prototype mechanics. 
+It sets Rabbit.prototype.[[Prototype]] to Animal.prototype. So, if a method 
+is not found in Rabbit.prototype, JavaScript takes it from Animal.prototype.
+
+Animal  prototype Animal.prototype
+[     ] --------> [constructor: Animal] 
+                  [run : function     ]
+                  [stop: function     ]
+                        /\
+                        | [[Prototype]]   // extends
+                        |
+      Rabbit.prototype  |
+Rabbit            [constructor: Rabbit  ]
+[constructor] --> [hide: function       ] 
+                        /\
+                        | [[Prototype]]
+            new Rabbit  |
+                  [name: "White Rabbit" ]
+
+For instance, to find rabbit.run method, the engine checks (bottom-up on 
+the picture):
+  1. The rabbit object (has no run).
+  2. Its prototype, that is Rabbit.prototype (has hide, but not run).
+  3. Its prototype, that is (due to extends) Animal.prototype, that 
+  finally has the run method.
+
+As we can recall from the chapter Native prototypes, JavaScript itself uses 
+prototypal inheritance for built-in objects. E.g. Date.prototype.[[Prototype]] 
+is Object.prototype. That’s why dates have access to generic object methods.
+*/
