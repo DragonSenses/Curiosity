f99f9fbf2b6c994b7fb4f1559ca1bf872164a578
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-11 18:10:05 -0800

    Update staticPropertiesMethods.js

--- JavaScript/9. Classes/9.3 Static Properties and Methods/staticPropertiesMethods.js
+++ JavaScript/9. Classes/9.3 Static Properties and Methods/staticPropertiesMethods.js
@@ -157,3 +157,48 @@ properties, but prepended by static: */
   /* That is the same as a direct assignment to Article: */
   Article.publisher = "Ilya Kantor";
 }
+
+
+/* Inheritance of static properties and methods */
+/* Static properties and methods are inherited.
+
+For instance, Animal.compare and Animal.planet in the code below are inherited 
+and accessible as Rabbit.compare and Rabbit.planet: */
+{
+  class Animal {
+    static planet = "Earth";
+  
+    constructor(name, speed) {
+      this.speed = speed;
+      this.name = name;
+    }
+  
+    run(speed = 0) {
+      this.speed += speed;
+      console.log(`${this.name} runs with speed ${this.speed}.`);
+    }
+  
+    static compare(animalA, animalB) {
+      return animalA.speed - animalB.speed;
+    }
+  
+  }
+  
+  // Inherit from Animal
+  class Rabbit extends Animal {
+    hide() {
+      console.log(`${this.name} hides!`);
+    }
+  }
+  
+  let rabbits = [
+    new Rabbit("White Rabbit", 10),
+    new Rabbit("Black Rabbit", 5)
+  ];
+  
+  rabbits.sort(Rabbit.compare);
+  
+  rabbits[0].run(); // Black Rabbit runs with speed 5.
+  
+  console.log(Rabbit.planet); // Earth
+}
