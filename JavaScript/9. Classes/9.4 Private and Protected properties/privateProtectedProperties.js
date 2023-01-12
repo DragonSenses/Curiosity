b4a7e83370f28a0cf6d8a14714d321b800d9e48d
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-12 15:14:54 -0800

    Update privateProtectedProperties.js

--- JavaScript/9. Classes/9.4 Private and Protected properties/privateProtectedProperties.js
+++ JavaScript/9. Classes/9.4 Private and Protected properties/privateProtectedProperties.js
@@ -94,7 +94,7 @@ That was a general introduction.
   - Private: accessible only from inside the class. 
   These are for the internal interface.
 
-In many other languages there also exist “protected” fields: accessible only 
+In many other languages there also exist "protected" fields: accessible only 
 from inside the class and those extending it (like private, but plus access 
 from inheriting classes). They are also useful for the internal interface. 
 They are in a sense more widespread than private ones, because we usually 
@@ -107,8 +107,7 @@ Now we'll make a coffee machine in JavaScript with all these types of properties
 A coffee machine has a lot of details, we won't model them to stay simple (though we could).
 */
 
-
-/* Protecting “waterAmount” */
+/* Protecting "waterAmount" */
 /* Let's make a simple coffee machine class first: */
 {
   class CoffeeMachine {
@@ -171,7 +170,7 @@ So our property will be called _waterAmount: */
 below zero becomes impossible. */
 
 
-/* Read-only “power” */
+/* Read-only "power" */
 /* For power property, let's make it read-only. It sometimes happens that a 
 property must be set at creation time only, and then never modified.
 
@@ -237,3 +236,99 @@ we'll see below. */
 
 
 /* Private "#waterLimit" */
+/* There's a finished JavaScript proposal, almost in the standard, that 
+provides language-level support for private properties and methods.
+
+Privates should start with #. They are only accessible from inside the class.
+
+For instance, here's a private #waterLimit property and the water-checking 
+private method #fixWaterAmount: */
+{
+  class CoffeeMachine {
+    #waterLimit = 200;
+  
+    #fixWaterAmount(value) {
+      if (value < 0) return 0;
+      if (value > this.#waterLimit) return this.#waterLimit;
+    }
+  
+    setWaterAmount(value) {
+      this.#waterLimit = this.#fixWaterAmount(value);
+    }
+  
+  }
+  
+  let coffeeMachine = new CoffeeMachine();
+  
+  console.log(coffeeMachine);
+
+  // can't access privates from outside of the class
+  // coffeeMachine.#fixWaterAmount(123); // Error
+  // coffeeMachine.#waterLimit = 1000; // Error
+}
+
+/* On the language level, # is a special sign that the field is private. 
+We can't access it from outside or from inheriting classes.
+
+Private fields do not conflict with public ones. We can have both 
+private #waterAmount and public waterAmount fields at the same time.
+
+For instance, let's make waterAmount an accessor for #waterAmount: */
+{
+  class CoffeeMachine {
+
+    #waterAmount = 0;
+  
+    get waterAmount() {
+      return this.#waterAmount;
+    }
+  
+    set waterAmount(value) {
+      if (value < 0) value = 0;
+      this.#waterAmount = value;
+    }
+  }
+  
+  let machine = new CoffeeMachine();
+  
+  machine.waterAmount = 100;
+  // console.log(machine.#waterAmount); // Error
+
+/* Unlike protected ones, private fields are enforced by the language itself. 
+That's a good thing.
+
+But if we inherit from CoffeeMachine, then we'll have no direct access 
+to #waterAmount. We'll need to rely on waterAmount getter/setter: */
+
+  class MegaCoffeeMachine extends CoffeeMachine {
+    method() {
+      // console.log( this.#waterAmount ); // Error: can only access from CoffeeMachine
+    }
+  }
+
+  let megaMachine = new MegaCoffeeMachine();
+
+  console.log(megaMachine);
+}
+/* In many scenarios such limitation is too severe. If we extend a 
+CoffeeMachine, we may have legitimate reasons to access its internals. 
+That's why protected fields are used more often, even though they are not 
+supported by the language syntax. */
+
+/* Private fields are not available as this[name] */
+/* Private fields are special.
+
+As we know, usually we can access fields using this[name]: */
+{
+  // eslint-disable-next-line no-unused-vars
+  class User {
+    // ...
+    sayHi() {
+      let fieldName = "name";
+      console.log(`Hello, ${this[fieldName]}`);
+    }
+  }
+
+}
+/* With private fields that's impossible: this['#name'] doesn't work. 
+That's a syntax limitation to ensure privacy. */
