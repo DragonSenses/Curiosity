d50a8a35c2df17832fe7af63945a2ff2a8cf3059
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-12 17:19:18 -0800

    Create mixins.js

---
+++ JavaScript/9. Classes/9.7 Mixins/mixins.js
@@ -0,0 +1,16 @@
+/* Mixins */
+/* Summary
+Mixin – is a generic object-oriented programming term: a class that contains 
+methods for other classes.
+
+Some other languages allow multiple inheritance. JavaScript does not support 
+multiple inheritance, but mixins can be implemented by copying methods into 
+prototype.
+
+We can use mixins as a way to augment a class by adding multiple behaviors, 
+like event-handling as we have seen above.
+
+Mixins may become a point of conflict if they accidentally overwrite 
+existing class methods. So generally one should think well about the naming 
+methods of a mixin, to minimize the probability of that happening.
+*/
