139d4ac8af37d9cd3f5fbb9dc22af05f26a70a49
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-13 19:15:07 -0800

    Update errorHandling.js

--- JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
+++ JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
@@ -377,4 +377,59 @@ But by its nature, catch gets all errors from try. Here it gets an
 unexpected error, but still shows the same "JSON Error" message. That’s wrong 
 and also makes the code more difficult to debug. */
 
-/*  */
+/* To avoid such problems, we can employ the “rethrowing” technique. 
+The rule is simple:
+
+Catch should only process errors that it knows and “rethrow” all others.
+
+The “rethrowing” technique can be explained in more detail as:
+  1. Catch gets all errors.
+  2. In the catch (err) {...} block we analyze the error object err.
+  3. If we don’t know how to handle it, we do throw err.
+
+Usually, we can check the error type using the instanceof operator: */
+{
+  try {
+    user = { /*...*/ };
+  } catch (err) {
+    if (err instanceof ReferenceError) {
+      alert('ReferenceError'); // "ReferenceError" for accessing an undefined variable
+    }
+  }
+}
+
+/* We can also get the error class name from err.name property. 
+All native errors have it. Another option is to read err.constructor.name.
+
+In the code below, we use rethrowing so that catch only handles SyntaxError: */
+{
+  let json = '{ "age": 20 }'; // incomplete data
+  try {
+
+    let user = JSON.parse(json);
+
+    if (!user.name) {
+      throw new SyntaxError("Incomplete data: no name");
+    }
+
+    blabla(); // unexpected error
+
+    alert( user.name );
+
+  } catch (err) {
+
+    if (err instanceof SyntaxError) {
+      alert( "JSON Error: " + err.message );
+    } else {
+      throw err; // rethrow (*)
+    }
+
+  }
+}
+
+/* The error throwing on line (*) from inside catch block “falls out” of 
+try...catch and can be either caught by an outer try...catch construct (if 
+  it exists), or it kills the script.
+
+So the catch block actually handles only errors that it knows how to deal 
+with and “skips” all others. */
