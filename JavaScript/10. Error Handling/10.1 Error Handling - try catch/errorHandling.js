594cd7190cc6fb1a885dc39acfeac5d14e4bdf22
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-13 19:04:14 -0800

    Update errorHandling.js

--- JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
+++ JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
@@ -75,3 +75,58 @@ try {
 
 So, an error inside the try {...} block does not kill the script – we have a 
 chance to handle it in catch. */
+
+/* Examples: */
+/* An errorless example: shows alert (1) and (2): */
+try {
+
+  alert('Start of try runs');  // (1) <--
+
+  // ...no errors here
+
+  alert('End of try runs');   // (2) <--
+
+} catch (err) {
+
+  alert('Catch is ignored, because there are no errors'); // (3)
+
+}
+
+/* An example with an error: shows (1) and (3): */
+try {
+
+  alert('Start of try runs');  // (1) <--
+
+  // eslint-disable-next-line no-undef
+  lalala; // error, variable is not defined!
+
+  alert('End of try (never reached)');  // (2)
+
+} catch (err) {
+
+  alert(`Error has occurred!`); // (3) <--
+
+}
+
+
+/* try...catch only works for runtime errors */
+/* For try...catch to work, the code must be runnable. 
+In other words, it should be valid JavaScript.
+
+It won’t work if the code is syntactically wrong, for instance it has 
+unmatched curly braces: 
+
+try {
+  {{{{{{{{{{{{
+} catch (err) {
+  alert("The engine can't understand this code, it's invalid");
+}
+
+The JavaScript engine first reads the code, and then runs it. 
+The errors that occur on the reading phase are called “parse-time” errors 
+and are unrecoverable (from inside that code). That’s because the engine 
+can’t understand the code.
+
+So, try...catch can only handle errors that occur in valid code. 
+Such errors are called “runtime errors” or, sometimes, “exceptions”.
+*/
