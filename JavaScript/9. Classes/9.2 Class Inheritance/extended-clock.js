f5ef4c24e87a432eeecdf0abd7abdbe939c70974
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-15 12:44:27 -0800

    made extendedClock instance

--- JavaScript/9. Classes/9.2 Class Inheritance/extended-clock.js
+++ JavaScript/9. Classes/9.2 Class Inheritance/extended-clock.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars, no-undef
 class ExtendedClock extends Clock {
   constructor(options) {
     super(options);
+    // Define precision parameter with a default parameter of 1000ms, or 1sec
     let { precision = 1000 } = options;
     this.precision = precision;
   }
