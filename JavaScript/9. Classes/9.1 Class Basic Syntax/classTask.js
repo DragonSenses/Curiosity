39c81595a0c20da6ad424f671f6e9404be27e7c5
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-14 19:25:29 -0800

    Update classTask.js

--- JavaScript/9. Classes/9.1 Class Basic Syntax/classTask.js
+++ JavaScript/9. Classes/9.1 Class Basic Syntax/classTask.js
@@ -1,8 +1,6 @@
 /* Rewrite to class */
 /* The Clock class is written in functional style. 
-
 > In index.html
-
 <!doctype html>
 <script>
 
@@ -53,3 +51,43 @@ Rewrite it in the "class" syntax.
 
 P.S. The clock ticks in the console, open it to see. 
 */
+
+/* Answer:  */
+
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
