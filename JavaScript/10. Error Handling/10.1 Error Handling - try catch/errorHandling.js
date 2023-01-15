ded208dc6a8a418e29d8873ded2e9ffb0298722f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-14 19:08:57 -0800

    Update errorHandling.js

--- JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
+++ JavaScript/10. Error Handling/10.1 Error Handling - try catch/errorHandling.js
@@ -441,6 +441,7 @@ function readData() {
 
   try {
     // ...
+    console.log(json);
     blabla(); // error!
   } catch (err) {
     // ...
@@ -456,7 +457,6 @@ try {
   alert( "External catch got: " + err ); // caught it!
 }
 
-console.log(json);
 
 /* Here readData only knows how to handle SyntaxError, while the outer 
 try...catch knows how to handle everything. */
@@ -487,4 +487,52 @@ try {
   alert( 'catch' );
 } finally {
   alert( 'finally' );
+}
+
+/* The code has two ways of execution:
+
+  1. If you answer “Yes” to “Make an error?”, then try -> catch -> finally.
+  2. If you say “No”, then try -> finally.
+
+The finally clause is often used when we start doing something and want to 
+finalize it in any case of outcome.
+
+For instance, we want to measure the time that a Fibonacci numbers 
+function fib(n) takes. Naturally, we can start measuring before it runs and 
+finish afterwards. But what if there’s an error during the function call? 
+In particular, the implementation of fib(n) in the code below returns an 
+error for negative or non-integer numbers.
+
+The finally clause is a great place to finish the measurements no matter what. 
+*/
+
+/* Here finally guarantees that the time will be measured correctly in both 
+situations – in case of a successful execution of fib and in case of an error 
+in it: */
+{
+  let num = +prompt("Enter a positive integer number?", 35);
+
+  let diff, result;
+
+  // eslint-disable-next-line no-inner-declarations
+  function fib(n) {
+    if (n < 0 || Math.trunc(n) != n) {
+      throw new Error("Must not be negative, and also an integer.");
+    }
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  }
+
+  let start = Date.now();
+
+  try {
+    result = fib(num);
+  } catch (err) {
+    result = 0;
+  } finally {
+    diff = Date.now() - start;
+  }
+
+  alert(result || "error occurred");
+
+  alert( `execution took ${diff}ms` );
 }
