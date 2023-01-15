f40f964597a2419b686bf7fbfebb3d68c89c23a0
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-15 13:34:20 -0800

    Update instanceofTask.js

--- JavaScript/9. Classes/9.6 instanceof/instanceofTask.js
+++ JavaScript/9. Classes/9.6 instanceof/instanceofTask.js
@@ -10,3 +10,18 @@ A.prototype = B.prototype = {};
 let a = new A();
 
 console.log( a instanceof B ); // true
+
+
+/* Answer:
+Recall the syntax:
+  obj instanceof Class
+
+instanceof returns true if obj belongs to the Class OR a class inheriting from it.
+
+instanceof does not care about the function, but rather about its prototype, 
+that it matches against the prototype chain.
+
+And here a.__proto__ == B.prototype, so instanceof returns true.
+
+So, by the logic of instanceof, the prototype actually defines the type, not 
+the constructor function. */
