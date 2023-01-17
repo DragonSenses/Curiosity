589290f54d3b5a55f9f49e0ab469cba3ec7fd62e
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-16 21:10:11 -0800

    Update callbacks.js

--- JavaScript/11. Promises async await/11.1 callbacks/callbacks.js
+++ JavaScript/11. Promises async await/11.1 callbacks/callbacks.js
@@ -10,3 +10,30 @@ tutorial.
 
 Although, we’ll try to make things clear anyway. There won’t be anything 
 really complex browser-wise. */
+
+/* Many functions are provided by JavaScript host environments that allow you 
+to schedule asynchronous actions. In other words, actions that we initiate now, 
+but they finish later.
+
+For instance, one such function is the setTimeout function.
+
+There are other real-world examples of asynchronous actions, e.g. loading 
+scripts and modules (we’ll cover them in later chapters).
+
+Take a look at the function loadScript(src), that loads a script with the 
+given src: */
+function loadScript(src) {
+  // creates a <script> tag and append it to the page
+  // this causes the script with given src to start loading and run when complete
+  let script = document.createElement('script');
+  script.src = src;
+  document.head.append(script);
+}
+
+/* It inserts into the document a new, dynamically created, 
+tag <script src="…"> with the given src. The browser automatically starts 
+loading it and executes when complete. 
+
+We can use this function like this: */
+// load and execute the script at the given path
+loadScript('/my/script.js');
