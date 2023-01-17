e745e6762c03ab0d3005d2bfb9bd50477051be21
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-16 21:16:02 -0800

    Update callbacks.js

--- JavaScript/11. Promises async await/11.1 callbacks/callbacks.js
+++ JavaScript/11. Promises async await/11.1 callbacks/callbacks.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* Introduction: callbacks */
 /* We use browser methods in examples here 
 To demonstrate the use of callbacks, promises and other abstract concepts, 
@@ -120,3 +121,46 @@ A function that does something asynchronously should provide a callback
 argument where we put the function to run after it’s complete.
 
 Here we did it in loadScript, but of course it’s a general approach. */
+
+
+/* Callback in callback */
+/* How can we load two scripts sequentially: the first one, and then the 
+second one after it?
+
+The natural solution would be to put the second loadScript call inside the 
+callback, like this: */
+{
+  loadScript('/my/script.js', function(script) {
+
+    alert(`Cool, the ${script.src} is loaded, let's load one more`);
+  
+    loadScript('/my/script2.js', function(script) {
+      alert(`Cool, the second script is loaded`);
+    });
+  
+  });
+}
+
+/* After the outer loadScript is complete, the callback initiates 
+the inner one.
+
+What if we want one more script…? */
+{
+  loadScript('/my/script.js', function(script) {
+
+    loadScript('/my/script2.js', function(script) {
+  
+      loadScript('/my/script3.js', function(script) {
+        // ...continue after all scripts are loaded
+      });
+  
+    });
+  
+  });
+}
+
+/* So, every new action is inside a callback. That’s fine for few actions, 
+but not good for many, so we’ll see other variants soon. */
+
+
+/* Handling errors */
