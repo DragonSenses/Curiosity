45044e51f0223f1335e643ea85f56045a719e83f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-17 15:20:53 -0800

    Update promiseChaining.js

--- JavaScript/11. Promises async await/11.3 Promise chaining/promiseChaining.js
+++ JavaScript/11. Promises async await/11.3 Promise chaining/promiseChaining.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* Promises Chaining */
 /* Summary 
@@ -129,3 +130,79 @@ promise. So in the code above all alert show the same: 1.
 
 In practice we rarely need multiple handlers for one promise. 
 Chaining is used much more often.*/
+
+/* Returning promises */
+/* A handler, used in .then(handler) may create and return a promise.
+
+In that case further handlers wait until it settles, and then get its result.
+
+For instance: */
+{
+  new Promise(function(resolve, reject) {
+
+    setTimeout(() => resolve(1), 1000);
+  
+  }).then(function(result) {
+  
+    alert(result); // 1
+  
+    return new Promise((resolve, reject) => { // (*)
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  
+  }).then(function(result) { // (**)
+  
+    alert(result); // 2
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  
+  }).then(function(result) {
+  
+    alert(result); // 4
+  
+  });
+}
+/* Here the first .then shows 1 and returns new Promise(…) in the line (*). 
+After one second it resolves, and the result (the argument of resolve, here 
+it’s result * 2) is passed on to the handler of the second .then. That handler
+is in the line (**), it shows 2 and does the same thing.
+
+So the output is the same as in the previous example: 1 → 2 → 4, but now 
+with 1 second delay between alert calls.
+
+Returning promises allows us to build chains of asynchronous actions. */
+
+/* Example: loadScript */
+/* Let’s use this feature with the promisified loadScript to load scripts 
+one by one, in sequence: */
+{
+  loadScript("/article/promise-chaining/one.js")
+  .then(function(script) {
+    return loadScript("/article/promise-chaining/two.js");
+  })
+  .then(function(script) {
+    return loadScript("/article/promise-chaining/three.js");
+  })
+  .then(function(script) {
+    // use functions declared in scripts
+    // to show that they indeed loaded
+    one();
+    two();
+    three();
+  });
+}
+/* This code can be made bit shorter with arrow functions: */
+{
+  loadScript("/article/promise-chaining/one.js")
+  .then(script => loadScript("/article/promise-chaining/two.js"))
+  .then(script => loadScript("/article/promise-chaining/three.js"))
+  .then(script => {
+    // scripts are loaded, we can use functions declared there
+    one();
+    two();
+    three();
+  });
+}
+
