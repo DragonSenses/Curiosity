646b3045f3874a67fcf74c336d7c28d1f9b904fa
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-16 17:32:50 -0800

    Update customErrors.js

--- JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrors.js
+++ JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrors.js
@@ -158,3 +158,57 @@ it in the line (**). The catch block only knows how to handle validation and
 syntax errors, other kinds (caused by a typo in the code or other unknown 
 reasons) should fall through.
 */
+
+/* Further inheritance */
+/* The ValidationError class is very generic. Many things may go wrong. 
+The property may be absent or it may be in a wrong format (like a string 
+value for age instead of a number). Let’s make a more concrete class 
+PropertyRequiredError, exactly for absent properties. It will carry 
+additional information about the property that’s missing. */
+{
+  class ValidationError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "ValidationError";
+    }
+  }
+  
+  class PropertyRequiredError extends ValidationError {
+    constructor(property) {
+      super("No property: " + property);
+      this.name = "PropertyRequiredError";
+      this.property = property;
+    }
+  }
+  
+  // Usage
+  function readUser(json) {
+    let user = JSON.parse(json);
+  
+    if (!user.age) {
+      throw new PropertyRequiredError("age");
+    }
+    if (!user.name) {
+      throw new PropertyRequiredError("name");
+    }
+  
+    return user;
+  }
+  
+  // Working example with try..catch
+  
+  try {
+    let user = readUser('{ "age": 25 }');
+    console.log(user);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      alert("Invalid data: " + err.message); // Invalid data: No property: name
+      alert(err.name); // PropertyRequiredError
+      alert(err.property); // name
+    } else if (err instanceof SyntaxError) {
+      alert("JSON Syntax Error: " + err.message);
+    } else {
+      throw err; // unknown error, rethrow it
+    }
+  }
+}
