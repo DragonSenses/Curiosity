21bc48c9070c57a8ca9747a8189e1ddbcf318f05
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-16 18:45:41 -0800

    Update customErrors.js

--- JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrors.js
+++ JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrors.js
@@ -332,3 +332,62 @@ error, it can check its cause property. */
 
 /* Here’s the code that defines ReadError and demonstrates its use in readUser 
 and try..catch: */
+{
+  class ReadError extends Error {
+    constructor(message, cause) {
+      super(message);
+      this.cause = cause;
+      this.name = 'ReadError';
+    }
+  }
+  
+  class ValidationError extends Error { /*...*/ }
+  class PropertyRequiredError extends ValidationError { /* ... */ }
+  
+  function validateUser(user) {
+    if (!user.age) {
+      throw new PropertyRequiredError("age");
+    }
+  
+    if (!user.name) {
+      throw new PropertyRequiredError("name");
+    }
+  }
+  
+  function readUser(json) {
+    let user;
+  
+    try {
+      user = JSON.parse(json);
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        throw new ReadError("Syntax Error", err);
+      } else {
+        throw err;
+      }
+    }
+  
+    try {
+      validateUser(user);
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        throw new ReadError("Validation Error", err);
+      } else {
+        throw err;
+      }
+    }
+  
+  }
+  
+  try {
+    readUser('{bad json}');
+  } catch (e) {
+    if (e instanceof ReadError) {
+      alert(e);
+      // Original error: SyntaxError: Unexpected token b in JSON at position 1
+      alert("Original error: " + e.cause);
+    } else {
+      throw e;
+    }
+  }
+}
