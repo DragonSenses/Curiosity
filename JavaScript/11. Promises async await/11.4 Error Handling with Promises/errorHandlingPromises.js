06ff319857723268d15cad4139f3fecec478e412
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-18 18:03:50 -0800

    Update errorHandlingPromises.js

--- JavaScript/11. Promises async await/11.4 Error Handling with Promises/errorHandlingPromises.js
+++ JavaScript/11. Promises async await/11.4 Error Handling with Promises/errorHandlingPromises.js
@@ -11,17 +11,17 @@
   and know how to handle them. The handler should analyze errors (custom error 
     classes help) and rethrow unknown ones (maybe they are programming mistakes).
 
-  - It’s ok not to use .catch at all, if there’s no way to recover from an error.
+  - It's ok not to use .catch at all, if there's no way to recover from an error.
 
   - In any case we should have the unhandledrejection event handler (for 
     browsers, and analogs for other environments) to track unhandled errors 
     and inform the user (and probably our server) about them, so that our 
-    app never “just dies”. 
+    app never "just dies". 
     
 */
 
 /* Promise chains are great at error handling. When a promise rejects, the 
-control jumps to the closest rejection handler. That’s very convenient in practice.
+control jumps to the closest rejection handler. That's very convenient in practice.
 
 For instance, in the code below the URL to fetch is wrong (no such site) and 
 .catch handles the error: */
@@ -31,7 +31,7 @@ For instance, in the code below the URL to fetch is wrong (no such site) and
     .catch(err => alert(err)); // TypeError: failed to fetch (the text may vary)
 }
 
-/* As you can see, the .catch doesn’t have to be immediate. It may appear 
+/* As you can see, the .catch doesn't have to be immediate. It may appear 
 after one or maybe several .then.
 
 Or, maybe, everything is all right with the site, but the response is not 
@@ -55,7 +55,7 @@ fetch('/article/promise-chaining/user.json')
   }))
   .catch(error => alert(error.message));
 }
-/* Normally, such .catch doesn’t trigger at all. But if any of the promises 
+/* Normally, such .catch doesn't trigger at all. But if any of the promises 
 above rejects (a network problem or invalid json or whatever), then it would 
 catch it. */
 
@@ -83,7 +83,7 @@ This happens not only in the executor function, but in its handlers as well.
 If we throw inside a .then handler, that means a rejected promise, so the 
 control jumps to the nearest error handler. */
 
-/* Here’s an example: */
+/* Here's an example: */
 {
   new Promise((resolve, reject) => {
     resolve("ok");
@@ -111,7 +111,7 @@ try..catch. We may have as many .then handlers as we want, and then use
 a single .catch at the end to handle errors in all of them.
 
 In a regular try..catch we can analyze the error and maybe rethrow it if 
-it can’t be handled. The same thing is possible for promises.
+it can't be handled. The same thing is possible for promises.
 
 If we throw inside .catch, then the control goes to the next closest error 
 handler. And if we handle the error and finish normally, then it continues 
@@ -135,7 +135,7 @@ In the example below the .catch successfully handles the error: */
 handler is called.
 
 In the example below we see the other situation with .catch. The 
-handler (*) catches the error and just can’t handle it (e.g. it only knows 
+handler (*) catches the error and just can't handle it (e.g. it only knows 
 how to handle URIError), so it throws it again: */
 {
   // the execution: catch -> catch
@@ -164,3 +164,54 @@ how to handle URIError), so it throws it again: */
 }  
 
 /* The execution jumps from the first .catch (*) to the next one (**) down the chain. */
+
+/* Unhandled rejections */
+/* What happens when an error is not handled? For instance, we forgot to append 
+.catch to the end of the chain, like here: */
+{
+  new Promise(function() {
+    // eslint-disable-next-line no-undef
+    noSuchFunction(); // Error here (no such function)
+  })
+    .then(() => {
+      // successful promise handlers, one or more
+    }); // without .catch at the end!
+}
+/* In case of an error, the promise becomes rejected, and the execution should 
+jump to the closest rejection handler. But there is none. So the error gets 
+"stuck". There's no code to handle it.
+
+In practice, just like with regular unhandled errors in code, it means that 
+something has gone terribly wrong.
+
+What happens when a regular error occurs and is not caught by try..catch? The 
+script dies with a message in the console. A similar thing happens with 
+unhandled promise rejections.
+
+The JavaScript engine tracks such rejections and generates a global error in 
+that case. You can see it in the console if you run the example above.
+
+In the browser we can catch such errors using the event unhandledrejection: */
+{
+  window.addEventListener('unhandledrejection', function(event) {
+    // the event object has two special properties:
+    alert(event.promise); // [object Promise] - the promise that generated the error
+    alert(event.reason); // Error: Whoops! - the unhandled error object
+  });
+  
+  new Promise(function() {
+    throw new Error("Whoops!");
+  }); // no catch to handle the error
+}
+
+/* The event is the part of the HTML standard.
+
+If an error occurs, and there's no .catch, the unhandledrejection handler 
+triggers, and gets the event object with the information about the error, 
+so we can do something.
+
+Usually such errors are unrecoverable, so our best way out is to inform the 
+user about the problem and probably report the incident to the server.
+
+In non-browser environments like Node.js there are other ways to track 
+unhandled errors. */
