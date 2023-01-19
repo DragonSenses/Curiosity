70f0ceb38b2a660972fda7accf8e01b07a74bbdb
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-18 18:01:13 -0800

    Update errorHandlingPromises.js

--- JavaScript/11. Promises async await/11.4 Error Handling with Promises/errorHandlingPromises.js
+++ JavaScript/11. Promises async await/11.4 Error Handling with Promises/errorHandlingPromises.js
@@ -104,3 +104,63 @@ statement. For example, a programming error: */
 }
 /* The final .catch not only catches explicit rejections, but also 
 accidental errors in the handlers above. */
+
+/* Rethrowing */
+/* As we already noticed, .catch at the end of the chain is similar to 
+try..catch. We may have as many .then handlers as we want, and then use 
+a single .catch at the end to handle errors in all of them.
+
+In a regular try..catch we can analyze the error and maybe rethrow it if 
+it can’t be handled. The same thing is possible for promises.
+
+If we throw inside .catch, then the control goes to the next closest error 
+handler. And if we handle the error and finish normally, then it continues 
+to the next closest successful .then handler.
+
+In the example below the .catch successfully handles the error: */
+{
+  // the execution: catch -> then
+  new Promise((resolve, reject) => {
+
+    throw new Error("Whoops!");
+
+  }).catch(function(error) {
+
+    alert("The error is handled, continue normally");
+
+  }).then(() => alert("Next successful handler runs"));
+}
+
+/* Here the .catch block finishes normally. So the next successful .then 
+handler is called.
+
+In the example below we see the other situation with .catch. The 
+handler (*) catches the error and just can’t handle it (e.g. it only knows 
+how to handle URIError), so it throws it again: */
+{
+  // the execution: catch -> catch
+  new Promise((resolve, reject) => {
+
+    throw new Error("Whoops!");
+
+  }).catch(function(error) { // (*)
+
+    if (error instanceof URIError) {
+      // handle it
+    } else {
+      alert("Can't handle such error");
+
+      throw error; // throwing this or another error jumps to the next catch
+    }
+
+  }).then(function() {
+    /* doesn't run here */
+  }).catch(error => { // (**)
+
+    alert(`The unknown error has occurred: ${error}`);
+    // don't return anything => execution goes the normal way
+
+  });
+}  
+
+/* The execution jumps from the first .catch (*) to the next one (**) down the chain. */
