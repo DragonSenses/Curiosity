8771058b82186b1430d883e55950819a0eca5c37
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-19 17:36:13 -0800

    Update promisification.js

--- JavaScript/11. Promises async await/11.6 Promisification/promisification.js
+++ JavaScript/11. Promises async await/11.6 Promisification/promisification.js
@@ -22,3 +22,67 @@ function loadScript(src, callback) {
 
 // usage:
 // loadScript('path/script.js', (err, script) => {...})
+
+
+/* The function loads a script with the given src, and then calls callback(err)
+in case of an error, or callback(null, script) in case of successful loading. 
+That’s a widespread agreement for using callbacks, we saw it before.
+
+Let’s promisify it.
+
+We’ll make a new function loadScriptPromise(src), that does the same (loads 
+  the script), but returns a promise instead of using callbacks.
+
+In other words, we pass it only src (no callback) and get a promise in return, 
+that resolves with script when the load is successful, and rejects with the 
+error otherwise. 
+
+Here it is: */
+// eslint-disable-next-line no-unused-vars
+let loadScriptPromise = function(src) {
+  return new Promise((resolve, reject) => {
+    loadScript(src, (err, script) => {
+      if (err) reject(err);
+      else resolve(script);
+    });
+  });
+};
+
+// usage:
+// loadScriptPromise('path/script.js').then(...)
+
+/* As we can see, the new function is a wrapper around the original loadScript 
+function. It calls it providing its own callback that translates to promise 
+resolve/reject.
+
+Now loadScriptPromise fits well in promise-based code. If we like promises more 
+than callbacks (and soon we’ll see more reasons for that), then we will use it instead.
+
+In practice we may need to promisify more than one function, so it makes sense 
+to use a helper.
+
+We’ll call it promisify(f): it accepts a to-promisify function f and returns a 
+wrapper function. */
+function promisify(f) {
+  return function (...args) { // return a wrapper-function (*)
+    return new Promise((resolve, reject) => {
+      function callback(err, result) { // our custom callback for f (**)
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback); // append our custom callback to the end of f arguments
+
+      f.call(this, ...args); // call the original function
+    });
+  };
+}
+
+{
+  // usage:
+  let loadScriptPromise = promisify(loadScript);
+  loadScriptPromise(/* ... */).then(/* ... */);
+}
