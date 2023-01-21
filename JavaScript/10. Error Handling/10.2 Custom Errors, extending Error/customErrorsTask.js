9dbf3887ddf7d7fd075d2bd513f5579a54729479
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-20 16:19:43 -0800

    Update customErrorsTask.js

--- JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrorsTask.js
+++ JavaScript/10. Error Handling/10.2 Custom Errors, extending Error/customErrorsTask.js
@@ -7,11 +7,28 @@ Usage example:
 
 let err = new FormatError("formatting error");
 
-alert( err.message ); // formatting error
-alert( err.name ); // FormatError
-alert( err.stack ); // stack
+console.log( err.message ); // formatting error
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
 
-alert( err instanceof FormatError ); // true
-alert( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
+console.log( err instanceof FormatError ); // true
+console.log( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
 
 */
+
+/* Answer:  */
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+let err = new FormatError("formatting error");
+
+console.log( err.message ); // formatting error
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof FormatError ); // true
+console.log( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
