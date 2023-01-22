4be70ad3c825a9adde88e89869b16251357cb28a
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-21 20:00:31 -0800

    Update asyncAwaitTasks.js

--- JavaScript/11. Promises async await/11.8 Async and Await/asyncAwaitTasks.js
+++ JavaScript/11. Promises async await/11.8 Async and Await/asyncAwaitTasks.js
@@ -18,7 +18,39 @@ loadJson('https://javascript.info/no-such-user.json')
   .catch(alert); // Error: 404
 }
 
+/* Answer: */
+{
+  // eslint-disable-next-line no-inner-declarations
+  async function loadJson(url) { // (1)
+    let response = await fetch(url); // (2)
+  
+    if (response.status == 200) {
+      let json = await response.json(); // (3)
+      return json;
+    }
+  
+    throw new Error(response.status);
+  }
+  
+  loadJson('https://javascript.info/no-such-user.json')
+    .catch(alert); // Error: 404 (4)
+}
+/* Notes:
+  1. The function loadJson becomes async
+  2. All .then inside are replaced with awai
+  3. We can return response.json() instead of awaiting for it, like this:
 
+  if (response.status == 200) {
+    return response.json();     // (3)
+  }
+
+  Then the outer code would have to await for that promise to resolve. In our
+  case it doesn't matter.
+
+  4. The error thrown from loadJson is handled by .catch
+  We can't use await loadJson(...) there, because we're not in an async function
+
+*/
 
 
 /* Rewrite "rethrow" with async/await */
@@ -67,11 +99,13 @@ function demoGithubUser() {
 
 demoGithubUser();
 
+/* Answer:  */
+
 
 
 /* Call async from non-async */
 /* We have a “regular” function called f. How can you call the async function 
-wait() and use its result inside of f? */
+wait() and use its result inside of f? 
 async function wait() {
   await new Promise(resolve => setTimeout(resolve, 1000));
 
@@ -82,7 +116,27 @@ function f() {
   // ...what should you write here?
   // we need to call async wait() and wait to get 10
   // remember, we can't use "await"
+} 
+
+P.S. The task is technically very simple, but the question is quite common 
+for developers new to async/await. */
+
+
+/* Answer: Treat async call as promise and attach .then to it.
+
+That’s the case when knowing how it works inside is helpful.
+
+Just treat async call as promise and attach .then to it: */
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
 }
 
-/* P.S. The task is technically very simple, but the question is quite common 
-for developers new to async/await. */
+function f(){
+  // shows 10 after 1 second
+  wait().then(result => console.log(result));
+}
+
+f();
