ed13e7dc039c0fa3d7302fba9e81c7893941f95f
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-21 20:03:24 -0800

    Update asyncAwaitTasks.js

--- JavaScript/11. Promises async await/11.8 Async and Await/asyncAwaitTasks.js
+++ JavaScript/11. Promises async await/11.8 Async and Await/asyncAwaitTasks.js
@@ -58,7 +58,7 @@ loadJson('https://javascript.info/no-such-user.json')
 instead of .then/catch.
 
 And get rid of the recursion in favour of a loop in demoGithubUser: with 
-async/await that becomes easy to do. */
+async/await that becomes easy to do. 
 class HttpError extends Error {
   constructor(response) {
     super(`${response.status} for ${response.url}`);
@@ -98,9 +98,55 @@ function demoGithubUser() {
 }
 
 demoGithubUser();
+*/
 
-/* Answer:  */
+/* Answer: Just replace .catch with try..catch inside demoGithubUser 
+and add async/await where needed: */
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
 
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+// Ask for a user name until github returns a valid user
+async function demoGithubUser() {
+
+  let user;
+  // eslint-disable-next-line no-constant-condition
+  while(true) {
+    let name = prompt("Enter a name?", "iliakan");
+
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break; // no error, exit loop
+    } catch(err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        // loop continues after the alert
+        alert("No such user, please reenter.");
+      } else {
+        // unknown error, rethrow
+        throw err;
+      }
+    }
+  }
+
+
+  alert(`Full name: ${user.name}.`);
+  return user;
+}
+
+demoGithubUser();
 
 
 /* Call async from non-async */
