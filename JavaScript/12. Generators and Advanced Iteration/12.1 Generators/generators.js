8319cf1e61a5ed989ff072aa66fab5f565a4a9a7
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-22 19:55:30 -0800

    Update generators.js

--- JavaScript/12. Generators and Advanced Iteration/12.1 Generators/generators.js
+++ JavaScript/12. Generators and Advanced Iteration/12.1 Generators/generators.js
@@ -304,3 +304,121 @@ The composed generator:*/
   alert(str); // 0..9A..Za..z
 
 }
+
+/* The yield* directive delegates the execution to another generator. 
+This term means that yield* gen iterates over the generator gen and 
+transparently forwards its yields outside. As if the values were yielded
+by the outer generator.
+
+The result is the same as if we inlined the code from nested generators: */
+{
+/*   function* generateSequence(start, end) {
+    for (let i = start; i <= end; i++) yield i;
+  } 
+*/
+  
+  function* generateAlphaNum() {
+  
+    // yield* generateSequence(48, 57);
+    for (let i = 48; i <= 57; i++) yield i;
+  
+    // yield* generateSequence(65, 90);
+    for (let i = 65; i <= 90; i++) yield i;
+  
+    // yield* generateSequence(97, 122);
+    for (let i = 97; i <= 122; i++) yield i;
+  
+  }
+  
+  let str = '';
+  
+  for(let code of generateAlphaNum()) {
+    str += String.fromCharCode(code);
+  }
+  
+  alert(str); // 0..9A..Za..z
+}
+
+/* A generator composition is a natural way to insert a flow of one generator 
+into another. It doesn’t use extra memory to store intermediate results. */
+
+/* “yield” is a two-way street */
+/* Until this moment, generators were similar to iterable objects, with a 
+special syntax to generate values. But in fact they are much more powerful
+and flexible.
+
+That’s because yield is a two-way street: it not only returns the result to 
+the outside, but also can pass the value inside the generator.
+
+To do so, we should call generator.next(arg), with an argument. That argument 
+becomes the result of yield.
+
+Let’s see an example: */
+{
+  function* gen() {
+    // Pass a question to the outer code and wait for an answer
+    let result = yield "2 + 2 = ?"; // (*)
+  
+    alert(result);
+  }
+  
+  let generator = gen();
+  
+  // eslint-disable-next-line no-unused-vars
+  let question = generator.next().value; // <-- yield returns the value
+  
+  generator.next(4); // --> pass the result into the generator
+
+/* Generator                              Calling Code
+
+function* gen() {
+  let result = yield "2 + 2 = ?";    -->  question = "2 + 2 = ?"
+                                          .next(4)
+  alert(result);  // 4                         
+} 
+
+
+  1. The first call generator.next() should be always made without an 
+  argument (the argument is ignored if passed). It starts the execution 
+  and returns the result of the first yield "2+2=?". At this point the 
+  generator pauses the execution, while staying on the line (*).
+
+  2. Then, as shown at the picture above, the result of yield gets into 
+  the question variable in the calling code.
+
+  3. On generator.next(4), the generator resumes, and 4 gets in as the 
+  result: let result = 4.
+
+Please note, the outer code does not have to immediately call next(4). 
+It may take time. That’s not a problem: the generator will wait.
+
+For instance:
+*/
+
+  // resume the generator after some time
+  setTimeout(() => generator.next(4), 1000);
+}
+
+/* As we can see, unlike regular functions, a generator and the calling 
+code can exchange results by passing values in next/yield.
+
+To make things more obvious, here’s another example, with more calls: */
+{
+  function* gen() {
+    let ask1 = yield "2 + 2 = ?";
+  
+    alert(ask1); // 4
+  
+    let ask2 = yield "3 * 3 = ?";
+  
+    alert(ask2); // 9
+  }
+  
+  let generator = gen();
+  
+  alert( generator.next().value ); // "2 + 2 = ?"
+  
+  alert( generator.next(4).value ); // "3 * 3 = ?"
+  
+  alert( generator.next(9).done ); // true
+}
