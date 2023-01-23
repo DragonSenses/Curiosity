8a6321ff882c6ed4d66a6860ed99b99e32b850f0
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-22 20:04:05 -0800

    Update generatorTasks.js

--- JavaScript/12. Generators and Advanced Iteration/12.1 Generators/generatorTasks.js
+++ JavaScript/12. Generators and Advanced Iteration/12.1 Generators/generatorTasks.js
@@ -5,10 +5,10 @@ One of them is testing. We may need random data: text, numbers, etc.
 to test things out well.
 
 In JavaScript, we could use Math.random(). But if something goes wrong, 
-we’d like to be able to repeat the test, using exactly the same data.
+we'd like to be able to repeat the test, using exactly the same data.
 
-For that, so called “seeded pseudo-random generators” are used. They take 
-a “seed”, the first value, and then generate the next ones using a formula 
+For that, so called "seeded pseudo-random generators" are used. They take 
+a "seed", the first value, and then generate the next ones using a formula 
 so that the same seed yields the same sequence, and hence the whole flow is 
 easily reproducible. We only need to remember the seed to repeat it.
 
@@ -30,12 +30,40 @@ and creates the generator with this formula.
 Usage example: */
 
 function* pseudoRandom(seed){
+  let value = seed;
 
+  while(true) {
+    value = value * 16807 % 2147483647;
+    yield value;
+  }
 }
 
 
 let generator = pseudoRandom(1);
 
-alert(generator.next().value); // 16807
-alert(generator.next().value); // 282475249
-alert(generator.next().value); // 1622650073
+console.log(generator.next().value); // 16807
+console.log(generator.next().value); // 282475249
+console.log(generator.next().value); // 1622650073
+
+/* Please note, the same can be done with a regular function, like this: */
+{
+  // eslint-disable-next-line no-inner-declarations
+  function pseudoRandom(seed) {
+    let value = seed;
+  
+    return function() {
+      value = value * 16807 % 2147483647;
+      return value;
+    };
+  }
+  
+  let generator = pseudoRandom(1);
+  
+  console.log(generator()); // 16807
+  console.log(generator()); // 282475249
+  console.log(generator()); // 1622650073
+
+}
+
+/* That also works. But then we lose ability to iterate with for..of and 
+to use generator composition, that may be useful elsewhere. */
