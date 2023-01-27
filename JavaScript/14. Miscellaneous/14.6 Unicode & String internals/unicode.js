899dc221d25a098eb32fd4041cd39a4d1e5337ef
DragonSenses <kendrickfrancisco@gmail.com>
2023-01-27 00:05:27 -0800

    Create unicode.js

---
+++ JavaScript/14. Miscellaneous/14.6 Unicode & String internals/unicode.js
@@ -0,0 +1,5 @@
+/* Unicode, String internals */
+/* Advanced knowledge */
+/* The section goes deeper into string internals. This knowledge will be 
+useful for you if you plan to deal with emoji, rare mathematical or 
+hieroglyphic characters, or other rare symbols. */
