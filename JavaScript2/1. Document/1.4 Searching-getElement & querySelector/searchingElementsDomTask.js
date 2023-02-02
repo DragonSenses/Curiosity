7a17fbce4714ee7e61cdcb19a852dab8f2319954
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-01 18:10:24 -0800

    Create searchingElementsDomTask.js

---
+++ JavaScript2/1. Document/1.4 Searching-getElement & querySelector/searchingElementsDomTask.js
@@ -0,0 +1,12 @@
+/* Search for elements */
+/* Given "table.html" - a document with the table and form.
+
+How to find?…
+
+  1. The table with id="age-table".
+  2. All label elements inside that table (there should be 3 of them).
+  3. The first td in that table (with the word “Age”).
+  4. The form with name="search".
+  5. The first input in that form.
+  6. The last input in that form.
+*/
