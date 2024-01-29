3bc117821871878b1b4049b9102f04e09662f818
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-29 08:00:43 -0800

    Add login route to authenticate user

--- Authentication/app/app.js
+++ Authentication/app/app.js
@@ -55,12 +55,41 @@ app.post("/register", (req, res) => {
       // Do something with user document, such as sending a response or redirecting
       // res.status(201).send("User created");
     })
-    .catch(err => { 
+    .catch(err => {
       // Log the error to the console or a file
       console.error(err);
       // Send an error response or redirect to an error page
       res.status(500).send("Something went wrong");
     });
+});
+
+// Route handler for /login path
+app.post("/login", (req, res) => {
+  // Extract username and password from the request body
+  const { username, password } = req.body;
+  
+  // Find a user document in the database that matches the email
+  User.findOne({ email: username }, function (error, foundUser) {
+    if (error) {
+      console.log(error);
+    } else {
+      // If the user exists, compare the password with the stored password
+      if (foundUser) {
+        // If the passwords match, render the secrets page
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+        // If the passwords do not match, you can send an error message or redirect to another page
+        // else {
+        //   res.send("Wrong password");
+        // }
+      }
+      // If the user does not exist, you can send an error message or redirect to another page
+      // else {
+      //   res.send("User not found");
+      // }
+    }
+  });
 });
 
 /* Starts the server & listens for requests on the specified port */
