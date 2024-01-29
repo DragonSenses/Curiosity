5e83cbcf5e6447404667451e41da909fafd33127
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-28 22:33:56 -0800

    Set up express, ejs, and bodyParser in node project
    This commit sets up the express, ejs, and bodyParser packages in the node project. Express is used to create a web server and handle routing. Ejs is used to render dynamic HTML views. BodyParser is used to parse incoming request bodies. The commit also sets up the port, the static folder, and the view engine for the app. The app listens for requests and logs the port number or any errors to the console.

--- Authentication/app/app.js
+++ Authentication/app/app.js
@@ -1,1 +1,18 @@
 //jshint esversion:6
+import express from 'express';
+import bodyParser from 'body-parser';
+import ejs from 'ejs';
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}.`);
+});
