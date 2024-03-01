ebf20d5fab464032f76f40eb6c90f2a248b9f426
DragonSenses <kendrickfrancisco@gmail.com>
2024-03-01 13:35:33 -0800

    Update downloadImages.js

--- Web_Scraper/image-scraper/downloadImages.js
+++ Web_Scraper/image-scraper/downloadImages.js
@@ -1,16 +1,30 @@
 // Import the modules
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
+import http from 'http';
+import fs from 'fs';
 
 /* Pathname for downloaded images */
 // relative: './downloads/'
 // Windows: 'C:/Users/YourUsername/Desktop/MyDownloads/'
 // 'D:/temp/images/'
-const outputPath = './downloads/';
+// const outputPath = './downloads/';
+
+/*  
+const createFilePath = (url) => {
+  const filename = url.split('/').pop(); // Extract filename from URL
+  return `${outputPath}${filename}`;
+};
+*/
+
+function createFilePath(url, outputPath) {
+  const filename = url.split('/').pop(); // Extract filename from URL
+  return `${outputPath}${filename}`;
+}
 
 /**
  * Checks if the output path exists. If it does not exist then create
- * the directory (also create nested directories).
+ * the directory (including nested directories).
  * @param {string} path the output path name
  */
 function checkOutputPath(path){
@@ -21,16 +35,16 @@ function checkOutputPath(path){
   return;
 }
 
-const createFilePath = (url) => {
-  const filename = url.split('/').pop(); // Extract filename from URL
-  return `${outputPath}${filename}`;
-};
-
 /**
  * Given an array of URLs to images, downloads the images to the file system.
  * @param {URL[]} hrefs an array of Images
+ * @param {string} outputPath the path where images will be downloaded to
  */
-export default async function downloadImages(hrefs){
+export default async function downloadImages(hrefs, outputPath){
+  checkOutputPath(outputPath);
+
+  console.log(typeof hrefs);
+  console.log(hrefs.length === 0);
   // Loop over the array and make a request for each href
   hrefs.forEach((href) => {
     // Define the file name and path using the index and the extension
@@ -38,7 +52,7 @@ export default async function downloadImages(hrefs){
     // const fileName = "image" + index + ext;
     // const filePath = path.join("./", fileName);
   
-    const filePath = createFilePath(href);
+    const filePath = createFilePath(href, outputPath);
     // const fileName = filePath.split('/').pop();
     // const ext = filePath.extname(href);
 
