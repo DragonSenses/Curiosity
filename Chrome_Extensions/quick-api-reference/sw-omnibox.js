92058089c058294e053670dba3b648b9201346ea
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-28 23:29:00 -0700

    feat: Initialize extension state on installation

--- Chrome_Extensions/quick-api-reference/sw-omnibox.js
+++ Chrome_Extensions/quick-api-reference/sw-omnibox.js
@@ -1,1 +1,10 @@
 console.log("sw-omnibox.js");
+
+// Save default API suggestions
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === 'install') {
+    chrome.storage.local.set({
+      apiSuggestions: ['tabs', 'storage', 'scripting']
+    });
+  }
+});
