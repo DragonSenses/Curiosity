73d83d2b47c3a62a69a277b0ee1cfefa650c4d4b
DragonSenses <kendrickfrancisco@gmail.com>
2024-04-29 23:40:51 -0700

    feat: Implement omnibox suggestions in extension
    In the `sw-omnibox.js` file, set up an event listener for the `chrome.omnibox.onInputChanged` event. When the user starts typing, display suggestions based on previously saved API searches. The default suggestion prompts the user to enter a Chrome API or choose from past searches.
    
    For each API suggestion, create a description that allows opening the corresponding Chrome API reference page.

--- Chrome_Extensions/quick-api-reference/sw-omnibox.js
+++ Chrome_Extensions/quick-api-reference/sw-omnibox.js
@@ -8,3 +8,19 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
     });
   }
 });
+
+const URL_CHROME_EXTENSIONS_DOC =
+  'https://developer.chrome.com/docs/extensions/reference/';
+const NUMBER_OF_PREVIOUS_SEARCHES = 4;
+
+// Display the suggestions after user starts typing
+chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
+  await chrome.omnibox.setDefaultSuggestion({
+    description: 'Enter a Chrome API or choose from past searches'
+  });
+  const { apiSuggestions } = await chrome.storage.local.get('apiSuggestions');
+  const suggestions = apiSuggestions.map((api) => {
+    return { content: api, description: `Open chrome.${api} API` };
+  });
+  suggest(suggestions);
+});
