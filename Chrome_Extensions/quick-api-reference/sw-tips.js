5cea7353d6fbd8038ec62f2029a79fc042b53119
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-05 22:14:38 -0700

    feat: Add tip messaging to content script

--- Chrome_Extensions/quick-api-reference/sw-tips.js
+++ Chrome_Extensions/quick-api-reference/sw-tips.js
@@ -27,3 +27,11 @@ createAlarm();
 
 // Update tip once a day
 chrome.alarms.onAlarm.addListener(updateTip);
+
+// Send tip to content script via messaging
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.greeting === 'tip') {
+    chrome.storage.local.get('tip').then(sendResponse);
+    return true;
+  }
+});
