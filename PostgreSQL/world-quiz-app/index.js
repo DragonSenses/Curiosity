fa477d3887dce9fe8a7d06edd9a9bd50bd871974
DragonSenses <kendrickfrancisco@gmail.com>
2024-05-12 22:26:11 -0700

    feat: Set up PostgreSQL client connection

--- PostgreSQL/world-quiz-app/index.js
+++ PostgreSQL/world-quiz-app/index.js
@@ -2,10 +2,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+// Import the 'dotenv' package to load environment variables from a .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
+/* Create an Express app */
 const app = express();
 const port = 3000;
 
-// Middleware
+// Set up a PostgreSQL client connection
+const db = new pg.Client({
+  user: "postgres", 
+  host: "localhost",
+  database: "world", 
+  password: process.env.DB_PASSWORD, 
+  port: 5432,
+});
+
+// Connect to the PostgreSQL database
+db.connect();
+
+
+/* Middleware */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
