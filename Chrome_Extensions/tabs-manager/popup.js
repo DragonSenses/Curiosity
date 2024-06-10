2824dc50bbe9d0a26665b181cbe7915fb3f709a7
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-09 22:23:49 -0700

    feat: Implement tab sorting and display logic

--- Chrome_Extensions/tabs-manager/popup.js
+++ Chrome_Extensions/tabs-manager/popup.js
@@ -4,3 +4,26 @@ const tabs = await chrome.tabs.query({
     "https://developer.chrome.com/docs/extensions/*",
   ]
 });
+
+const collator = new Intl.Collator();
+tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+const template = document.getElementById("li_template");
+const elements = new Set();
+for (const tab of tabs) {
+  const element = template.content.firstElementChild.cloneNode(true);
+
+  const title = tab.title.split("-")[0].trim();
+  const pathname = new URL(tab.url).pathname.slice("/docs".length);
+
+  element.querySelector(".title").textContent = title;
+  element.querySelector(".pathname").textContent = pathname;
+  element.querySelector("a").addEventListener("click", async () => {
+    // need to focus window as well as the active tab
+    await chrome.tabs.update(tab.id, { active: true });
+    await chrome.windows.update(tab.windowId, { focused: true });
+  });
+
+  elements.add(element);
+}
+document.querySelector("ul").append(...elements);
