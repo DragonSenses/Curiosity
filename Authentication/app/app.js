8e07cfbed373ff9644d9069ad782ec13aa305ea2
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-10 22:26:11 -0700

    fix: Handle new userinfo endpoint in strategy
    Add `userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'` to the GoogleStrategy options to use the OAuth userinfo endpoint instead of G+.

--- Authentication/app/app.js
+++ Authentication/app/app.js
@@ -52,10 +52,12 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Fix based on PR #51: https://github.com/jaredhanson/passport-google-oauth2/pull/51
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
   function (accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
