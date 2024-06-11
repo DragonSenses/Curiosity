189aa1f6589e563660b5ee2511314679f1a6949b
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-10 22:07:21 -0700

    feat: Configure GoogleStrategy with env variables
    - Interpolate the relevant variables (clientID and clientSecret) within the object passed into GoogleStrategy
    - The callbackURL is set to "http://localhost:3000/auth/google/secrets"

--- Authentication/app/app.js
+++ Authentication/app/app.js
@@ -53,8 +53,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/secrets"
 },
   function (accessToken, refreshToken, profile, cb) {
