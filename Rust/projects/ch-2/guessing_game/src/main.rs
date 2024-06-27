cccb4358fdd3a931cdfd94dfa2215b393e51d42d
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-26 18:42:20 -0700

    feat: Generate a random number to guess

--- Rust/projects/ch-2/guessing_game/src/main.rs
+++ Rust/projects/ch-2/guessing_game/src/main.rs
@@ -1,15 +1,20 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
     io::stdin()
-      .read_line(&mut guess)
-      .expect("Failed to read line");
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("You guessed: {guess}");
 }
