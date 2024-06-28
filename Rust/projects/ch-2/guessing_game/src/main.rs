9d18275e3c0d6cccb40382d9a32ab36f4a14ef07
DragonSenses <kendrickfrancisco@gmail.com>
2024-06-27 22:31:00 -0700

    feat: Add input line parsing for user guess

--- Rust/projects/ch-2/guessing_game/src/main.rs
+++ Rust/projects/ch-2/guessing_game/src/main.rs
@@ -17,6 +17,8 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
