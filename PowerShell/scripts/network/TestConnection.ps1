2a3a91d13876d6ccf73d7a9d0dbbe0d9d6a8e657
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-17 15:45:07 -0800

    feat: Create TestConnection.ps1

---
+++ PowerShell/scripts/network/TestConnection.ps1
@@ -0,0 +1,4 @@
+#  Sends ICMP Echo Requests to "websiteName.com" and shows whether it’s reachable, along with the round-trip time in milliseconds
+# URL can be replaced with IP address
+
+Test-Connection -ComputerName Github.com
