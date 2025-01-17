902c515eac1d1fce01c83d7e1c634ceaf81b44fa
DragonSenses <kendrickfrancisco@gmail.com>
2025-01-17 15:28:13 -0800

    feat: Parameterize input & output paths

--- PowerShell/scripts/files/RecursiveFileList.ps1
+++ PowerShell/scripts/files/RecursiveFileList.ps1
@@ -1,2 +1,7 @@
-# List all files recursively in PowerShell and save the output to a CSV file
-Get-ChildItem -Recurse | Export-Csv -Path "C:\output\logs\fileList.csv" -NoTypeInformation
+# List all files recursively, print file to output and save output to CSV file
+# Set input and output paths
+$inputPath = "C:\input"
+$outputPath = "C:\output\logs\fileList.csv"
+
+# Get files recursively
+Get-ChildItem -Path $inputPath -Recurse | Export-Csv -Path $outputPath -NoTypeInformation
