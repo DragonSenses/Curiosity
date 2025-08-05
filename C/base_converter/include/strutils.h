447f0e4dcefc4c84b80803854173e0f48fc154d4
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-05 11:58:17 -0700

    feat(header): Add strutils.h for string utility declarations
    - Declared reverseString with documentation
    - Added C++ compatibility via extern "C"
    - Scoped header for modular reuse across conversion modules

--- C/base_converter/include/strutils.h
+++ C/base_converter/include/strutils.h
@@ -1,5 +1,19 @@
 #ifndef INCLUDE_STRUTILS_H
 #define INCLUDE_STRUTILS_H
 
-// Function declarationsvoid strrev();
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Reverses a null-terminated string in place.
+ *
+ * @param str The string to reverse
+ */
+void reverseString(char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif // INCLUDE_STRUTILS_H
