e662255e19c1bf43caf99c9599bfafbe4b039c87
DragonSenses <kendrickfrancisco@gmail.com>
2025-08-16 15:27:14 -0700

    feat(octal): Add decimal-to-octal and octal-to-decimal conversion functions

--- C/base_converter/src/octal.c
+++ C/base_converter/src/octal.c
@@ -1,4 +1,26 @@
 // octal.c
-#include \"../include/octal.h\"
+#include "../include/octal.h"
 
-// Function definitionsvoid octalToDecimal() {\n    // TODO: Implement octalToDecimal\n}\n void decimalToOctal() {\n    // TODO: Implement decimalToOctal\n}\n
+// Converts a decimal string to its octal string representation.
+char* decimalToOctal(int decimal)
+{
+  // Allocate space for an octal string
+  char* octal = (char*)malloc(12);
+  if (octal == NULL) {
+      printf("Memory allocation failed.\n");
+      exit(1);
+  }
+  // Convert decimal to octal
+  sprintf(octal, "%o", decimal);
+  return octal;
+}
+// Converts a octal string to its decimal string representation.
+int octalToDecimal(char octal[])
+{
+  int decimal = 0;
+  int length = strlen(octal);
+  for (int i = 0; i < length; i++) {
+      decimal = decimal * 8 + (octal[i] - '0');
+  }
+  return decimal;
+}
