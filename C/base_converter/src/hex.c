32935123f1c2253260cab857907d4817fad3c19b
DragonSenses <kendrickfrancisco@gmail.com>
2025-09-05 19:31:07 -0700

    feat(hex): Implement hexadecimalToBinary

--- C/base_converter/src/hex.c
+++ C/base_converter/src/hex.c
@@ -1,4 +1,26 @@
 // hex.c
-#include \"../include/hex.h\"
+#include "../include/hex.h"
+#include "../include/strutils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Function definitionsvoid hexadecimalToBinary() {\n    // TODO: Implement hexadecimalToBinary\n}\n
+// Function to convert hexadecimal to binary and return as a string
+char* hexadecimalToBinary(char hex[])
+{
+    // converting hexadecimal string to integer
+    unsigned int hexNum;
+    sscanf(hex, "%x", &hexNum);
+    // string to store binary number
+    char binary[33] = "";
+    // converting to hexadecimal
+    int i = 0;
+    while (hexNum) {
+        binary[i++] = '0' + hexNum % 2;
+        hexNum /= 2;
+    }
+    binary[i] = '\0';
+    reverseString(binary);
+
+    return strdup(binary);
+}
