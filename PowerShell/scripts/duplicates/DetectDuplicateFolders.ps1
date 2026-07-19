5ef1ab1565cf5b205be93043dbaa77dfa4e9ad90
DragonSenses <kendrickfrancisco@gmail.com>
2026-07-19 14:47:40 -0700

    doc: Add rule for detect duplicate script

--- PowerShell/scripts/duplicates/DetectDuplicateFolders.ps1
+++ PowerShell/scripts/duplicates/DetectDuplicateFolders.ps1
@@ -1,4 +1,6 @@
 # Regex for folders like:  New Folder (2)
+# Rule: "Any folder ending in (number) is a duplicate."
+
 $DuplicatePattern = '^(.*)\s\(\d+\)$'
 
 $folders = Get-ChildItem -Directory -Path $_.FullName
